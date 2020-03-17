7b17f50a888364b22c4921f2016c055a3291cc14
Jules de Smit <julesdesmit@gmail.com>
2020-03-17 15:40:00 +0100

    Add print statement

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use std::path::{Path, PathBuf};
 
-use phoenix::{rpc, db, Transaction};
+use phoenix::{db, rpc, Transaction};
 use tracing::trace;
 
-pub struct RuskServer {
+pub struct Rusk {
     db_path: PathBuf,
 }
 
-impl RuskServer {
+impl Rusk {
     pub fn new<P: AsRef<Path>>(path: P) -> Self {
         Self {
             db_path: path.as_ref().to_path_buf(),
@@ -16,7 +16,7 @@ impl RuskServer {
 }
 
 #[tonic::async_trait]
-impl rpc::rusk_server::Rusk for RuskServer {
+impl rpc::rusk_server::Rusk for Rusk {
     async fn echo(
         &self,
         _request: tonic::Request<rpc::EchoRequest>,
@@ -28,17 +28,24 @@ impl rpc::rusk_server::Rusk for RuskServer {
     async fn validate_state_transition(
         &self,
         request: tonic::Request<rpc::ValidateStateTransitionRequest>,
-    ) -> Result<tonic::Response<rpc::ValidateStateTransitionResponse>, tonic::Status> {
+    ) -> Result<
+        tonic::Response<rpc::ValidateStateTransitionResponse>,
+        tonic::Status,
+    > {
         let mut txs = vec![];
         for t in request.into_inner().txs {
             txs.push(
                 Transaction::try_from_rpc_transaction(&self.db_path, t)
-                    .map_err(|e| tonic::Status::invalid_argument(e.to_string()))?,
+                    .map_err(|e| {
+                        tonic::Status::invalid_argument(e.to_string())
+                    })?,
             );
         }
 
         db::store_bulk_transactions(&self.db_path, &txs)
             .map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        println!("it happened");
 
         Ok(tonic::Response::new(rpc::ValidateStateTransitionResponse {
             success: true,
