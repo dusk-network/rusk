7cda7bf79515bc285ffb815732da9b0930a6aaf3
Jules de Smit <julesdesmit@gmail.com>
2020-04-15 15:07:27 +0200

    Do mandatory Phoenix setup when starting the server
    Also, set the env var for the phoenix db on startup

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@ mod server;
 
 use tonic::transport::Server;
 
-use phoenix::rpc::rusk_server::RuskServer;
+use phoenix::{rpc::rusk_server::RuskServer, utils, zk};
 use server::Rusk;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Set DB_PATH
+    // TODO: this should not be a temp dir
+    let mut db_path = std::env::temp_dir();
+    db_path.push("phoenix-db");
+    std::env::set_var("PHOENIX_DB", db_path.into_os_string());
+
+    // Mandatory Phoenix setup
+    utils::init();
+    zk::init();
+
     let addr = "127.0.0.1:8080".parse().unwrap();
     println!("listening on {}...", addr);
     Server::builder()
