4db380f76da6d7b3dca94dd9bf13109849399870
CPerezz <carlos@dusk.network>
2020-08-21 10:41:27 +0200

    Implement MVI server for rusk
    This cleans up the implementation that we previously had in rusk
    to just leave there a simple echo service with a server implementation.
    
    - Added the server instance in `main.rs`
    - Impl `Echoer` service for `RuskServer`

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
-mod client;
 mod server;
 
-use tonic::transport::Server;
-
-use phoenix::{rpc::rusk_server::RuskServer, utils, zk};
+use server::basic_proto::echoer_server::EchoerServer;
 use server::Rusk;
+use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Set DB_PATH
-    // TODO: this should not be a temp dir
-    let mut db_path = std::env::temp_dir();
-    db_path.push("phoenix-db");
-    std::env::set_var("PHOENIX_DB", db_path.into_os_string());
+    let addr = "[::1]:50051".parse()?;
+    let rusk = Rusk::default();
 
-    // Mandatory Phoenix setup
-    utils::init();
-    zk::init();
-
-    let addr = "127.0.0.1:8080".parse().unwrap();
-    println!("listening on {}...", addr);
     Server::builder()
-        .add_service(RuskServer::new(Rusk::default()))
+        .add_service(EchoerServer::new(rusk))
         .serve(addr)
         .await?;
 
