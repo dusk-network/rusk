0835a79c9c3686ab05425f0b1b62e90332e400c4
Victor Lopez <victor@dusk.network>
2020-08-27 15:09:12 +0200

    Add copyright headers

--- tests/echo_service_tests.rs
+++ tests/echo_service_tests.rs
@@ -1,3 +1,6 @@
+// Copyright (c) DUSK NETWORK. All rights reserved.
+// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.
+
 use rusk::services::echoer::{EchoRequest, EchoerClient, EchoerServer};
 use rusk::Rusk;
 use tonic::transport::Server;
