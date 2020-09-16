8b43e7b47d2e81419594fc39202f8ee9f867614f
Luke Pearson <luke@dusk.network>
2020-09-16 14:45:07 +0200

    Add preimage gadget
    This gadget is used for
    procing the preimage of a
    note, as a circuit gate.

---
+++ contracts/transfer/circuits/src/gadgets/preimage.rs
@@ -0,0 +1,46 @@
+// Copyright (c) DUSK NETWORK. All rights reserved.
+// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.
+
+use phoenix_core::note::Note;
+use dusk_plonk::constraint_system::ecc::scalar_mul::fixed_base::scalar_mul;
+use dusk_plonk::jubjub::{
+    Fr, AffinePoint, GENERATOR_EXTENDED, GENERATOR_NUMS_EXTENDED,
+};
+use dusk_plonk::prelude::*;
+use poseidon252::sponge::sponge::sponge_hash_gadget;
+use dusk_bls12_381::Scalar;
+
+pub fn input_preimage(composer: &mut StandardComposer, note: &Note) {
+    
+    let value_commitment_x = composer.add_input(note.value_commitment().get_x());
+    let value_commitment_y = composer.add_input(note.value_commitment().get_y());
+
+    let idx =  composer.add_input(Scalar::from(note.pos()));
+
+    let pk_r_x = composer.add_input(note.stealth_address().pk_r().get_x());
+    let pk_r_y = composer.add_input(note.stealth_address().pk_r().get_y());
+
+    let output = sponge_hash_gadget(composer, 
+        &[value_commitment_x, 
+        value_commitment_y, 
+        idx, 
+        pk_r_x, 
+        pk_r_y,
+        ], 
+    );
+
+    let note_hash = composer.add_input(note.hash());
+    
+    composer.add_gate(
+        output,
+        note_hash,
+        composer.zero_var, 
+        -BlsScalar::one(),
+        BlsScalar::one(),
+        BlsScalar::one(),
+
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    
+    );
+}
