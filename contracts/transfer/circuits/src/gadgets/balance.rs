695ad63987e6114718aec69d290c16ba48a0579a
Luke Pearson <luke@dusk.network>
2020-09-17 19:33:54 +0200

    Add Balance gadget test
    The balance gadget is tested
    and passing. It uses PLONK to
    make the proofs.

--- contracts/transfer/circuits/src/gadgets/balance.rs
+++ contracts/transfer/circuits/src/gadgets/balance.rs
@@ -17,3 +17,35 @@ pub fn balance(composer: &mut StandardComposer, v_in: u64, v_out: u64) {
 
 
 
+#[cfg(test)]
+mod commitment_tests {
+    use super::*;
+    use dusk_plonk::commitment_scheme::kzg10::PublicParameters;
+    use dusk_plonk::proof_system::{Prover, Verifier};
+
+    #[test]
+    fn  balance_gadget() {
+        let v_in = 100 as u64;
+        let v_out = 100 as u64;
+
+
+        // Generate Composer & Public Parameters
+        let pub_params = PublicParameters::setup(1 << 17, &mut rand::thread_rng()).unwrap();
+        let (ck, vk) = pub_params.trim(1 << 16).unwrap();
+        let mut prover = Prover::new(b"test");
+
+        balance(prover.mut_cs(), v_in, v_out);
+        prover.mut_cs().add_dummy_constraints();
+
+        let circuit = prover.preprocess(&ck).unwrap();
+        let proof = prover.prove(&ck).unwrap();
+
+        let mut verifier = Verifier::new(b"test");
+        balance(verifier.mut_cs(), v_in, v_out);
+        verifier.mut_cs().add_dummy_constraints();
+        verifier.preprocess(&ck).unwrap();
+        
+        let pi = verifier.mut_cs().public_inputs.clone();
+        verifier.verify(&proof, &vk, &pi).unwrap();
+    }
+}
