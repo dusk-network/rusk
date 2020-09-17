df61630c8723704aec52f3c730334f14ceb33510
Luke Pearson <luke@dusk.network>
2020-09-17 19:36:03 +0200

    Add merkle gadget test
    The merkle gadget is tested
    and passing. It uses PLONK to
    make the proofs.

--- contracts/transfer/circuits/src/gadgets/commitment.rs
+++ contracts/transfer/circuits/src/gadgets/commitment.rs
@@ -5,7 +5,7 @@
 
 use dusk_plonk::constraint_system::ecc::scalar_mul::fixed_base::scalar_mul;
 use dusk_plonk::jubjub::{
-    Fr, AffinePoint, GENERATOR_EXTENDED, GENERATOR_NUMS_EXTENDED,
+    Fr, AffinePoint, GENERATOR_EXTENDED, GENERATOR_NUMS_EXTENDED, ExtendedPoint,
 };
 use dusk_plonk::prelude::*;
 use rand::*;
@@ -20,7 +20,6 @@ use dusk_bls12_381::Scalar;
 /// Prove knowledge of the value and blinding factor, which make up the value commitment.
 /// This commitment gadget is using the pedersen commitments.
 pub fn commitment(composer: &mut StandardComposer, value: Fr, blinder: Fr, pub_commit: AffinePoint) {
-    let zero = composer.add_witness_to_circuit_description(Scalar::zero());
     let bid_value = AllocatedScalar::allocate(composer, value.into());
     let blinder = composer.add_input(blinder.into());
 
@@ -42,24 +41,18 @@ mod commitment_tests {
     fn commitment_gadget() {
         let value = Fr::from(100 as u64);
         let blinder = Fr::from(20000 as u64);
-        let p1 = GENERATOR_EXTENDED * value;
-        let p2 = GENERATOR_NUMS_EXTENDED * blinder;
 
-        let pc_commitment = p1 + p2;
-
-        let mut prover = Prover::new(b"test");
-
-        commitment(prover.mut_cs(), value, blinder, AffinePoint::from(pc_commitment));
-        // prover.mut_cs().add_dummy_constraints();
-        // NOTE: this is here to make the test pass, as one set of dummy constraints
-        // isn't enough when no extra gates are added. It should be removed once the
-        // commitment gadget is properly implemented.
-        // prover.mut_cs().add_dummy_constraints();
-        ////////////////////////////////////////////////////////////////////////////
+        let pc_commitment = AffinePoint::from(
+            &(GENERATOR_EXTENDED * value) 
+                + &(GENERATOR_NUMS_EXTENDED * blinder)
+        );
 
         // Generate Composer & Public Parameters
         let pub_params = PublicParameters::setup(1 << 17, &mut rand::thread_rng()).unwrap();
         let (ck, vk) = pub_params.trim(1 << 16).unwrap();
+        let mut prover = Prover::new(b"test");
+
+        commitment(prover.mut_cs(), value, blinder, pc_commitment);
 
         let circuit = prover.preprocess(&ck).unwrap();
         let proof = prover.prove(&ck).unwrap();
@@ -67,7 +60,8 @@ mod commitment_tests {
         let mut verifier = Verifier::new(b"test");
         commitment(verifier.mut_cs(), value, blinder, AffinePoint::from(pc_commitment));
         verifier.preprocess(&ck).unwrap();
-        prover.mut_cs().check_circuit_satisfied();
-        verifier.verify(&proof, &vk, &prover.mut_cs().public_inputs.clone()).unwrap();
+        
+        let pi = verifier.mut_cs().public_inputs.clone();
+        verifier.verify(&proof, &vk, &pi).unwrap();
     }
 }
--- contracts/transfer/circuits/src/gadgets/merkle.rs
+++ contracts/transfer/circuits/src/gadgets/merkle.rs
@@ -12,3 +12,44 @@ pub fn merkle(composer: &mut StandardComposer, branch: PoseidonBranch, note: &No
 
     merkle_opening_gadget(composer, branch, leaf, root);
 }
+
+#[cfg(test)]
+mod commitment_tests {
+    use super::*;
+    use phoenix_core::note::NoteType;
+    use dusk_plonk::commitment_scheme::kzg10::PublicParameters;
+    use dusk_plonk::proof_system::{Prover, Verifier};
+    use poseidon252::{PoseidonTree, StorageScalar};
+    use kelvin::Blake2b;
+    use dusk_pki::PublicSpendKey;
+    use rand::Rng;
+    use dusk_plonk::jubjub::{GENERATOR_EXTENDED, Fr};
+
+    #[test]
+    fn merkle_gadget() {
+        let mut tree: PoseidonTree<_, Blake2b> = PoseidonTree::new(17usize);
+
+        let a = GENERATOR_EXTENDED * Fr::random(&mut rand::thread_rng());
+        let b = GENERATOR_EXTENDED * Fr::random(&mut rand::thread_rng());
+        let psk = PublicSpendKey::new(a, b);
+        let note = Note::new(NoteType::Transparent, &psk, 100);
+        tree.push(StorageScalar{0: note.hash()}).unwrap();
+
+        // Generate Composer & Public Parameters
+        let pub_params = PublicParameters::setup(1 << 17, &mut rand::thread_rng()).unwrap();
+        let (ck, vk) = pub_params.trim(1 << 16).unwrap();
+        let mut prover = Prover::new(b"test");
+
+        merkle(prover.mut_cs(), tree.poseidon_branch(0u64).unwrap().unwrap(), &note);
+
+        let circuit = prover.preprocess(&ck).unwrap();
+        let proof = prover.prove(&ck).unwrap();
+
+        let mut verifier = Verifier::new(b"test");
+        merkle(verifier.mut_cs(), tree.poseidon_branch(0u64).unwrap().unwrap(), &note);
+        verifier.preprocess(&ck).unwrap();
+        
+        let pi = verifier.mut_cs().public_inputs.clone();
+        verifier.verify(&proof, &vk, &pi).unwrap();
+    }
+}
