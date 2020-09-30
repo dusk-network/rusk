638b83a0b2b6e03447dca5e41eaf6002e2caf761
Jules de Smit <julesdesmit@gmail.com>
2020-09-30 15:23:42 +0200

    Fix license headers
    The last line did not match with the expected
    header.

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.
+// Copyright (c) DUSK NETWORK. All rights reserved.
 
 /// Buildfile for the rusk crate.
 ///
