638c5d7aec8dda2a268851da9aeec18b4f39be5d
Jules de Smit <julesdesmit@gmail.com>
2020-10-05 13:23:09 +0200

    Address review comments
    This entails:
    - Substitute usage of `copy_from_slice` for a `try_from`
    - Remove an obsolete `.into()` when decoding the ViewKey
    - Remove usage of `encode_optional_request_param` in favor of shorter code
    - Remove unused functions `encode_optional_request_param` and `decode_optional_request_param`

--- src/lib/encoding.rs
+++ src/lib/encoding.rs
@@ -12,32 +12,7 @@ use dusk_pki::{
 };
 use dusk_plonk::jubjub::AffinePoint as JubJubAffine;
 use dusk_plonk::prelude::*;
-use tonic::{Code, Status};
-
-/// Generic function used to retrieve parameters that are optional from a
-/// GRPC request.
-pub(crate) fn decode_optional_request_param<T, U>(
-    possible_param: Option<&T>,
-) -> Result<U, Status>
-where
-    T: Clone,
-    U: TryFrom<T, Error = Status>,
-{
-    Ok(U::try_from(
-        possible_param
-            .ok_or(Status::new(Code::Unknown, "Missing required fields."))?
-            .clone(),
-    )?)
-}
-
-/// Generic function used to encore parameters that are optional in a
-/// GRPC response.
-pub(crate) fn encode_optional_request_param<T, U>(param: T) -> Option<U>
-where
-    U: From<T>,
-{
-    Some(U::from(param))
-}
+use tonic::Status;
 
 /// Wrapper over `jubjub_decode` fn
 pub(crate) fn decode_affine(bytes: &[u8]) -> Result<JubJubAffine, Status> {
@@ -62,9 +37,13 @@ pub(crate) fn decode_bls_scalar(bytes: &[u8]) -> Result<BlsScalar, Status> {
             "Not enough bytes to decode a BlsScalar",
         ))
     } else {
-        let mut buff = [0u8; 32];
-        buff.copy_from_slice(&bytes[0..32]);
-        Option::from(BlsScalar::from_bytes(&buff)).ok_or_else(|| {
+        let bytes = <&[u8; 32]>::try_from(bytes).map_err(|_| {
+            Status::failed_precondition(
+                "Expecting 32 bytes to decode a BlsScalar",
+            )
+        })?;
+
+        Option::from(BlsScalar::from_bytes(&bytes)).ok_or_else(|| {
             Status::failed_precondition("Point was improperly encoded")
         })
     }
@@ -131,7 +110,7 @@ impl TryFrom<&rusk_proto::ViewKey> for ViewKey {
 
     fn try_from(value: &rusk_proto::ViewKey) -> Result<ViewKey, Status> {
         Ok(ViewKey::new(
-            decode_jubjub_scalar(&value.a)?.into(),
+            decode_jubjub_scalar(&value.a)?,
             decode_affine(&value.b_g)?.into(),
         ))
     }
