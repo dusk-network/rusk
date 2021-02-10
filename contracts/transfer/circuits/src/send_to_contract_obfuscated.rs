0ef8a35a8195c85724a666a57f13d37ef24eaabe
Victor Lopez <victor@dusk.network>
2021-02-10 22:26:34 +0100

    Implements #183 - Unsafe default transfer circuits

--- contracts/transfer/circuits/src/send_to_contract_obfuscated.rs
+++ contracts/transfer/circuits/src/send_to_contract_obfuscated.rs
@@ -41,6 +41,42 @@ pub struct SendToContractObfuscatedCircuit {
     message_pk: JubJubExtended,
 }
 
+// TODO
+// This unsafe implementation is done that way because the rusk structure
+// combined with plonk requires an instance of this struct to be able to verify
+// proofs.
+//
+// That should be different since only the keys, the proof and the public inputs
+// should provide the required data to run the verification
+//
+// The `schnorr::Signature` doesn't implement `Default`, and shouldn't. This
+// unsafe usage is a workaround until the following issue is solved:
+// https://github.com/dusk-network/plonk/issues/396
+//
+// After that, this `Default` implementation can be removed.
+// https://github.com/dusk-network/rusk/issues/183
+impl Default for SendToContractObfuscatedCircuit {
+    fn default() -> Self {
+        use std::mem;
+
+        Self {
+            pi_positions: Default::default(),
+            signature: unsafe { mem::zeroed() },
+            value: Default::default(),
+            blinding_factor: Default::default(),
+            message_value: Default::default(),
+            message_blinding_factor: Default::default(),
+            message_r: Default::default(),
+            value_commitment: Default::default(),
+            message_value_commitment: Default::default(),
+            message_nonce: Default::default(),
+            message_cipher: Default::default(),
+            pk: Default::default(),
+            message_pk: Default::default(),
+        }
+    }
+}
+
 impl SendToContractObfuscatedCircuit {
     pub const fn rusk_keys_id() -> &'static str {
         "transfer-send-to-contract-obfuscated"
