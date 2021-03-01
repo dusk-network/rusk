586f85c940b42e7cdaab0423e84d70c11cb862c6
zer0 <matteo@dusk.network>
2021-03-01 23:19:17 +0100

    rusk-abi: Update Build Status shield

--- rusk-abi/src/lib.rs
+++ rusk-abi/src/lib.rs
@@ -4,7 +4,7 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-//! ![Build Status](https://github.com/dusk-network/rusk-abi/workflows/Continuous%20integration/badge.svg)
+//! ![Build Status](https://github.com/dusk-network/rusk/workflows/Continuous%20integration/badge.svg)
 //! [![Repository](https://img.shields.io/badge/github-rusk--abi-blueviolet?logo=github)](https://github.com/dusk-network/rusk-abi)
 //! [![Documentation](https://img.shields.io/badge/docs-rusk--abi-blue?logo=rust)](https://docs.rs/rusk-abi/)
 
