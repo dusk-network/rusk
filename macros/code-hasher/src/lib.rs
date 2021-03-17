db6e3ba4459dc36823a855242cf7bb6c2c0eab25
CPerezz <carlos@dusk.network>
2021-03-17 12:58:52 +0100

    code-hasher: Add code_hasher attribute macro impl
    We need a way to version our circuits. A first step into
    that direction was basically the fact of introducing the CIRCUIT_ID
    associated const in the Circuit trait. See: dusk-network/plonk#454
    
    This is the second step which hashes the contents of a code block and
    toghether with a provided version and an `IDENT` generates an unique
    identifier for the whole block adding the const declaration to it.
    
    - Added test cases suggested by @zer0
    - Went for the generic approach suggested by @zer0 in #249 review which
    simplifies the implementation in order to be able to complicate less the
    things for now.
    - The hash includes the versions:
    1. Passed by attribute in the macro (is optional).
    2. The version of the `code_hasher` crate.
    
    Resolves: #240

---
+++ macros/code-hasher/src/lib.rs
@@ -0,0 +1,58 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+//! Tiny proc macro library designed to hash a code block generating a unique
+//! identifier for it which will get written into a `const` inside of the code
+//! block.
+//!
+//! # Example
+//! extern crate code_hahser;
+//! ```
+//! #[code_hasher::hash(SOME_CONST_NAME, version = "0.1.0")]
+//! pub mod testing_module {
+//!
+//!     pub fn this_does_something() -> [u8; 32] {
+//!         SOME_CONST_NAME
+//!     }
+//! }
+//! ```
+//! Here, `SOME_CONST_NAME` has assigned as value the resulting hash of:
+//! - The code contained inside `testing_module`.
+//! - The version of the crate (code_hasher) version.
+//! - The version passed by the user (is optional). Not adding it will basically
+//!   not hash this attribute and **WILL NOT** use any default alternatives.
+
+use blake3::Hasher;
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn hash(_attr: TokenStream, _input: TokenStream) -> TokenStream {
+    let mut hasher = Hasher::new();
+    // Add to the hasher the version of the crate used to create the code block
+    // hash.
+    hasher.update(env!("CARGO_PKG_VERSION_MINOR").as_bytes());
+
+    // We need to `let` this otherways it gets freed while borrowed.
+    let attrs_string = format!("{}", _attr.to_string());
+    let attrs_split: Vec<&str> = attrs_string.split(",").collect();
+
+    // Add the code version (passed as attribute) to the hasher.
+    hasher.update(attrs_split.get(1).expect("Missing code version").as_bytes());
+    hasher.update(_input.to_string().as_bytes());
+
+    let id = hasher.finalize().as_bytes().clone();
+    let mut token_stream = format!("{}", _input.to_string());
+    token_stream.pop();
+    token_stream.push_str(&format!(
+        "    const {}: [u8; 32] = {:?};",
+        attrs_split.get(0).expect("Missing const name"),
+        id
+    ));
+    token_stream.push_str(" }");
+    token_stream.parse().expect(
+        "Error parsing the output of the code-hasher macro as TokenStream",
+    )
+}
---
+++ macros/code-hasher/tests/main.rs
@@ -0,0 +1,69 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use dusk_plonk::prelude::*;
+
+#[derive(Debug, Default)]
+pub struct TestCircuit1 {}
+
+#[code_hasher::hash(CIRCUIT_ID, version = "0.1.0")]
+impl Circuit for TestCircuit1 {
+    fn gadget(&mut self, composer: &mut StandardComposer) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    fn padded_circuit_size(&self) -> usize {
+        1 << 11
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct TestCircuit2 {}
+
+#[code_hasher::hash(CIRCUIT_ID, version = "0.1.0")]
+impl Circuit for TestCircuit2 {
+    fn gadget(&mut self, composer: &mut StandardComposer) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    fn padded_circuit_size(&self) -> usize {
+        1 << 11
+    }
+}
+
+#[code_hasher::hash(SOME_CONST_NAME, version = "0.1.0")]
+pub mod testing_module {
+
+    pub fn this_does_something() -> [u8; 32] {
+        SOME_CONST_NAME
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn plonk_testcase_works() {
+        assert_eq!(
+            &TestCircuit1::CIRCUIT_ID,
+            &[
+                75, 161, 53, 125, 63, 238, 118, 194, 248, 174, 128, 90, 214,
+                72, 157, 0, 184, 26, 251, 147, 220, 253, 98, 165, 250, 188, 87,
+                9, 194, 33, 108, 91
+            ]
+        );
+    }
+
+    #[test]
+    fn diff_version_is_diff_hash() {
+        assert_ne!(&TestCircuit1::CIRCUIT_ID, &TestCircuit2::CIRCUIT_ID,);
+    }
+
+    #[test]
+    fn custom_mods_and_names_work() {
+        assert_ne!(testing_module::this_does_something(), [0u8; 32]);
+    }
+}
