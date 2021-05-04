b16b4b49797ad7ec15ce0a29c1aaaefc1938ffb3
CPerezz <carlos@dusk.network>
2021-05-04 12:45:35 +0200

    code-hasher: Add README.md for the crate

--- macros/code-hasher/src/lib.rs
+++ macros/code-hasher/src/lib.rs
@@ -4,13 +4,17 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+//! ![Build Status](https://github.com/dusk-network/rusk/workflows/Continuous%20integration/badge.svg)
+//! [![Repository](https://img.shields.io/badge/github-code--hasher-blueviolet?logo=github)](https://github.com/dusk-network/code-hasher)
+//! [![Documentation](https://img.shields.io/badge/docs-code--hasher-blue?logo=rust)](https://docs.rs/code-hasher/)
+//! # code-hasher
+//!
 //! Tiny proc macro library designed to hash a code block generating a unique
 //! identifier for it which will get written into a `const` inside of the code
 //! block.
 //!
-//! # Example
-//! extern crate code_hahser;
-//! ```
+//! ## Example
+//! ```rust
 //! #[code_hasher::hash(SOME_CONST_NAME, version = "0.1.0")]
 //! pub mod testing_module {
 //!
@@ -19,10 +23,15 @@
 //!     }
 //! }
 //! ```
+//!
 //! Here, `SOME_CONST_NAME` has assigned as value the resulting hash of:
 //! - The code contained inside `testing_module`.
 //! - The version passed by the user (is optional). Not adding it will basically
 //!   not hash this attribute and **WILL NOT** use any default alternatives.
+//!
+//! ## Licensing
+//! This code is licensed under Mozilla Public License Version 2.0 (MPL-2.0).
+//! Please see [LICENSE](https://github.com/dusk-network/rusk/tree/master/macros/code-hasher/LICENSE) for further info.
 
 use blake3::Hasher;
 use proc_macro::TokenStream;
