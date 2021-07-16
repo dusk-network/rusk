641ae5b425fac2970f4d3a87f1f242fedb733a8d
Victor Lopez <victor@dusk.network>
2021-07-16 14:56:06 +0200

    test-utils: Add transfer wrapper

--- test-utils/transfer-wrapper/src/lib.rs
+++ test-utils/transfer-wrapper/src/lib.rs
@@ -15,12 +15,13 @@ use rusk_vm::{Contract, GasMeter, NetworkState, VMError};
 use transfer_circuits::ExecuteCircuit;
 use transfer_contract::{Call, Error as TransferError, TransferContract};
 
-use std::convert::{TryFrom, TryInto};
+use std::convert::TryFrom;
+use std::io;
 
 use dusk_plonk::prelude::*;
 
-const TRANSFER: &'static [u8] = include_bytes!(
-    "../../../../target/wasm32-unknown-unknown/release/transfer_contract.wasm"
+static TRANSFER: &'static [u8] = include_bytes!(
+    "../../../target/wasm32-unknown-unknown/release/transfer_contract.wasm"
 );
 
 lazy_static! {
@@ -31,14 +32,8 @@ lazy_static! {
     };
 }
 
-pub fn transfer_contract() -> ContractId {
-    ContractId::from([
-        0xd3, 0xf8, 0x7f, 0xfc, 0x1b, 0xc7, 0x43, 0x1d, 0xde, 0x81, 0x5f, 0xb1,
-        0xe1, 0x1b, 0xd0, 0xfe, 0x88, 0x37, 0x1a, 0x15, 0x4a, 0xec, 0x27, 0x5d,
-        0xed, 0x2, 0x4d, 0x8c, 0xc0, 0xf7, 0x99, 0x5f,
-    ])
-}
-
+/// Create a new network state with the provided block height with an initial
+/// unspent transparent note with the value specified in `balance`
 pub fn genesis<R>(
     rng: &mut R,
     block_height: u64,
@@ -66,61 +61,56 @@ where
     let transfer = Contract::new(transfer, TRANSFER.to_vec());
     let transfer = network
         .deploy(transfer)
-        .map_err(|_| TransferError::ContractNotFound)?;
+        .or(Err(TransferError::ContractNotFound))?;
 
-    assert_eq!(transfer, transfer_contract());
+    assert_eq!(transfer, rusk_abi::transfer_contract());
 
     Ok((network, ssk))
 }
 
+/// Fetch an owned state of the transfer contract from a given network state
 pub fn transfer_state(
     network: &NetworkState,
-) -> Result<TransferContract, VMError> {
-    network.get_contract_cast_state(&transfer_contract())
+) -> Result<TransferContract, TransferError> {
+    network
+        .get_contract_cast_state(&rusk_abi::transfer_contract())
+        .or(Err(TransferError::ContractNotFound))
 }
 
-fn circuit_key(circuit_id: &[u8; 32]) -> Result<ProverKey, VMError> {
+/// Helper private function to return the ProverKey from a given circuit id
+fn circuit_key(circuit_id: &[u8; 32]) -> Result<ProverKey, TransferError> {
     let pk = rusk_profile::keys_for(circuit_id)
         .and_then(|keys| keys.get_prover())
-        .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+        .or(Err(TransferError::ProofVerificationError))?;
+
     let pk = ProverKey::from_slice(pk.as_slice())
-        .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+        .or(Err(TransferError::ProofVerificationError))?;
 
     Ok(pk)
 }
 
+/// Helper function to generate a valid execute call with its ZK proof of
+/// validity.
 fn prepare_execute<'a, R, I>(
     rng: &mut R,
     network: &NetworkState,
     inputs: I,
     output: &PublicSpendKey,
     transparent_output: bool,
-    gas_limit: u64,
-    gas_price: u64,
     gas_refund: &ViewKey,
-    crossover: u64,
+    fee: &Fee,
+    crossover: Option<&Crossover>,
+    crossover_value: u64,
 ) -> Result<
-    (
-        GasMeter,
-        BlsScalar,
-        Vec<BlsScalar>,
-        Fee,
-        Crossover,
-        Vec<Note>,
-        Vec<u8>,
-    ),
-    VMError,
+    (GasMeter, BlsScalar, Vec<BlsScalar>, Vec<Note>, Vec<u8>),
+    TransferError,
 >
 where
     R: RngCore + CryptoRng,
     I: Iterator<Item = &'a (SecretSpendKey, Note)>,
 {
-    let meter = GasMeter::with_limit(gas_limit);
-    let anchor = transfer_state(network)?
-        .notes()
-        .inner()
-        .root()
-        .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+    let meter = GasMeter::with_limit(fee.gas_limit);
+    let anchor = transfer_state(network)?.notes().inner().root()?;
 
     let mut execute_proof = ExecuteCircuit::default();
     let mut input = 0;
@@ -128,31 +118,26 @@ where
     let nullifiers: Vec<BlsScalar> = inputs
         .map(|(ssk, note)| {
             let vk = ssk.view_key();
-            let value = note
-                .value(Some(&vk))
-                .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+            let value = note.value(Some(&vk))?;
 
             input += value;
 
             let opening = transfer_state(network)?
                 .notes()
-                .opening(*note.pos())
-                .map_err(|e| VMError::ContractPanic(e.to_string()))?
-                .ok_or(VMError::ContractPanic(
-                    "Tree opening failed".to_owned(),
-                ))?;
+                .opening(*note.pos())?
+                .ok_or(TransferError::NoteNotFound)?;
 
             let signature = ExecuteCircuit::sign(rng, &ssk, note);
             execute_proof
                 .add_input(&ssk, *note, opening, signature)
-                .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+                .or(Err(TransferError::ProofVerificationError))?;
 
             Ok(note.gen_nullifier(ssk))
         })
-        .collect::<Result<_, VMError>>()?;
+        .collect::<Result<_, TransferError>>()?;
 
     let mut outputs = vec![];
-    let output_value = input - gas_limit - crossover;
+    let output_value = input - fee.gas_limit - crossover_value;
 
     if output_value == 0 {
     } else if transparent_output {
@@ -161,7 +146,7 @@ where
 
         execute_proof
             .add_output_with_data(note, output_value, blinding_factor)
-            .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+            .or(Err(TransferError::ProofVerificationError))?;
 
         outputs.push(note);
     } else {
@@ -170,83 +155,93 @@ where
 
         execute_proof
             .add_output_with_data(note, output_value, blinding_factor)
-            .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+            .or(Err(TransferError::ProofVerificationError))?;
 
         outputs.push(note);
     }
 
-    let gas_refund_psk = gas_refund.public_spend_key();
-    let blinding_factor = JubJubScalar::random(rng);
-    let note =
-        Note::obfuscated(rng, &gas_refund_psk, crossover, blinding_factor);
-
-    let (mut fee, crossover) =
-        note.try_into().map_err(|e: phoenix_core::Error| {
-            VMError::ContractPanic(e.to_string())
-        })?;
-
-    fee.gas_limit = gas_limit;
-    fee.gas_price = gas_price;
-
-    execute_proof
-        .set_fee_crossover(&fee, &crossover, gas_refund)
-        .map_err(|e| VMError::ContractPanic(e.to_string()))?;
+    match crossover {
+        Some(crossover) => {
+            execute_proof.set_fee_crossover(fee, crossover, gas_refund)
+        }
+        None => execute_proof.set_fee(fee),
+    }
+    .or(Err(TransferError::ProofVerificationError))?;
 
     let id = execute_proof.circuit_id();
     let pk = circuit_key(id)?;
 
     let proof = execute_proof
         .gen_proof(&*PP, &pk, b"dusk-network")
-        .map_err(|e| VMError::ContractPanic(e.to_string()))?
+        .or(Err(TransferError::ProofVerificationError))?
         .to_bytes()
         .to_vec();
 
-    Ok((meter, anchor, nullifiers, fee, crossover, outputs, proof))
+    Ok((meter, anchor, nullifiers, outputs, proof))
 }
 
+/// Execute a transaction in the network.
+///
+/// Every transaction must go through the transfer execute method so the gas can
+/// be properly handled.
+///
+/// The `inputs` is an iterator of a pair containing the secret spend key and
+/// its note. After the execution, the provided notes will be spent.
+///
+/// An output note will be created with the difference between the spent value
+/// and the inputs sum. This new note will be owned by the `output` key. This
+/// note will be transparent if the `transparent_output` flag is true;
+/// otherwise, it will be obfuscated.
+///
+/// The `fee` is the amount of gas reserved from the input that will be consumed
+/// to execute the transaction. The unused gas will be refund to the key
+/// `gas_refund` as a transparent note.
+///
+/// The `crossover` will be available in the context to be used with obfuscated
+/// methods of the transfer contract. The `ViewKey` is used to open its value
+/// commitment
+///
+/// The `call` is an encoded transaction to be executed in the network. If no
+/// transaction is specified, no call is performed but the gas is still consumed
+/// and the operation is valid.
 pub fn execute<'a, R, I>(
     rng: &mut R,
     network: &mut NetworkState,
     inputs: I,
     output: &PublicSpendKey,
     transparent_output: bool,
-    gas_limit: u64,
-    gas_price: u64,
-    gas_refund: &SecretSpendKey,
-    crossover: u64,
+    gas_refund: &ViewKey,
+    fee: Fee,
+    crossover: Option<(&ViewKey, Crossover)>,
     call: Option<(ContractId, Transaction)>,
 ) -> Result<(), VMError>
 where
     R: RngCore + CryptoRng,
     I: Iterator<Item = &'a (SecretSpendKey, Note)>,
 {
-    let gas_refund_vk = gas_refund.view_key();
+    let crossover_value = crossover
+        .map(|(vk, crossover)| {
+            Note::from((fee, crossover))
+                .value(Some(vk))
+                .or(Err(VMError::InvalidArguments))
+        })
+        .transpose()?
+        .unwrap_or(0);
 
-    let (
-        mut meter,
-        anchor,
-        nullifiers,
-        fee,
-        crossover_instance,
-        outputs,
-        spend_proof_execute,
-    ) = prepare_execute(
-        rng,
-        network,
-        inputs,
-        output,
-        transparent_output,
-        gas_limit,
-        gas_price,
-        &gas_refund_vk,
-        crossover,
-    )?;
-
-    let crossover = if crossover > 0 {
-        Some(crossover_instance)
-    } else {
-        None
-    };
+    let crossover = crossover.map(|(_, crossover)| crossover);
+    let (mut meter, anchor, nullifiers, outputs, spend_proof_execute) =
+        prepare_execute(
+            rng,
+            network,
+            inputs,
+            output,
+            transparent_output,
+            &gas_refund,
+            &fee,
+            crossover.as_ref(),
+            crossover_value,
+        )
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
 
     let call = Call::execute(
         anchor,
@@ -258,5 +253,5 @@ where
         call,
     );
 
-    network.transact::<_, ()>(transfer_contract(), call, &mut meter)
+    network.transact::<_, ()>(rusk_abi::transfer_contract(), call, &mut meter)
 }
