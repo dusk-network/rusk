18727cb60500ded128250700bb1f4328e155c680
CPerezz <c.perezbaro@gmail.com>
2021-07-21 15:34:02 +0200

    tmp

--- contracts/bid/tests/hosted_fns.rs
+++ contracts/bid/tests/hosted_fns.rs
@@ -6,7 +6,8 @@
 
 use bid_circuits::BidCorrectnessCircuit;
 use bid_contract::{contract_constants::*, Contract as BidContract};
-use dusk_blindbid::{Bid, V_RAW_MAX, V_RAW_MIN};
+use dusk_abi::{ContractId, Transaction};
+use dusk_blindbid::{V_RAW_MAX, V_RAW_MIN};
 use dusk_bls12_381::BlsScalar;
 use dusk_bytes::Serializable;
 use dusk_jubjub::{
@@ -14,15 +15,24 @@ use dusk_jubjub::{
 };
 use dusk_pki::{PublicKey, PublicSpendKey, SecretKey, SecretSpendKey};
 use dusk_plonk::prelude::*;
-use dusk_poseidon::{cipher::PoseidonCipher, sponge};
-use dusk_schnorr::{PublicKeyPair, Signature};
+use dusk_poseidon::sponge;
+use dusk_schnorr::Signature;
 use lazy_static::lazy_static;
 use phoenix_core::{Message, Note};
 use rand::Rng;
 use rusk_abi::RuskModule;
-use rusk_vm::{Contract, ContractId, GasMeter, NetworkState};
-use std::{convert::TryFrom, net};
+use rusk_vm::{Contract, GasMeter, NetworkState};
+use std::convert::{TryFrom, TryInto};
+use transfer_circuits::SendToContractObfuscatedCircuit;
 use transfer_contract::TransferContract;
+
+lazy_static! {
+    static ref PP: PublicParameters = unsafe {
+        let pp = rusk_profile::get_common_reference_string().unwrap();
+
+        PublicParameters::from_slice_unchecked(pp.as_slice())
+    };
+}
 
 const BID_CONTRACT_BYTECODE: &'static [u8] = include_bytes!(
     "../../../target/wasm32-unknown-unknown/release/bid_contract.wasm"
@@ -87,37 +97,90 @@ fn bid_contract_workflow_works() {
     let proof = create_proof(JubJubScalar::from(value), blinder);
 
     // Generate env
-    let mut block_height = 0u64;
+    let block_height = 0u64;
     let mut network = NetworkState::with_block_height(block_height);
     let rusk_mod = RuskModule::new(&*PUB_PARAMS);
     network.register_host_module(rusk_mod);
     // Deploy Transfer Contract
-    let (bidder_ssk, transfer_contract) = initialize_transfer_contract();
-    network.deploy(transfer_contract).expect("Deploy failure");
+    let (bidder_ssk, genesis_note, transfer_contract) =
+        initialize_transfer_contract();
+    let transfer_contract_id =
+        network.deploy(transfer_contract).expect("Deploy failure");
+
+    assert_eq!(
+        transfer_contract_id,
+        ContractId::from(rusk_abi::transfer_address())
+    );
     // Deploy contract
     let contract_id = network.deploy(contract).expect("Deploy failure");
     let mut gas = GasMeter::with_limit(1_000_000_000_000);
 
+    // 1. Call execute (contains the bid call encoded??)
+    // 2. Call Bid (called by execute)
+
     // Transfer Dusk to the Bid Contract (STCO)
+    // Transfer call (Calls execute fn)
 
-    // Add leaf to the Contract's tree and get it's pos index back
-    let call_result = network
-        .transact::<_, bool>(
-            contract_id,
-            (
-                bid_contract::ops::BID,
-                message,
-                hashed_secret,
-                stealth_addr,
-                proof.to_bytes().to_vec(),
-                proof.to_bytes().to_vec(),
-            ),
-            &mut gas,
-        )
-        .expect("Bid Transaction error");
+    // Get fee and crossover from from inital Note.
+    let note = Note::obfuscated(&mut rand::thread_rng(), &psk, value, blinder);
+    let (fee, crossover) = note
+        .try_into()
+        .expect("Error on generating Fee and Crossover");
+    // STCO Proof
+    let signature = SendToContractObfuscatedCircuit::sign(
+        &mut rand::thread_rng(),
+        &bidder_ssk,
+        &fee,
+        &crossover,
+        &message,
+        &TransferContract::contract_to_scalar(&contract_id),
+    );
+    let mut circ = SendToContractObfuscatedCircuit::new(
+        fee,
+        crossover,
+        &secret_spend_key.view_key(),
+        signature,
+        false,
+        message,
+        &psk,
+        secret,
+        TransferContract::contract_to_scalar(&contract_id),
+    )
+    .expect("Error on circuit generation");
 
-    assert!(call_result);
+    let stco_keys =
+        rusk_profile::keys_for(&SendToContractObfuscatedCircuit::CIRCUIT_ID)
+            .unwrap();
+    let stco_pk = stco_keys.get_prover().unwrap();
+    let stco_pk = ProverKey::from_slice(stco_pk.as_slice()).unwrap();
 
+    let stco_proof = circ.gen_proof(&*PP, &stco_pk, b"dusk-network").unwrap();
+    let bid_tx = Transaction::from_canon(&(
+        bid_contract::ops::BID,
+        message,
+        hashed_secret,
+        stealth_addr,
+        proof.to_bytes().to_vec(),
+        stco_proof.to_bytes().to_vec(),
+    ));
+
+    transfer_wrapper::execute(
+        &mut rand::thread_rng(),
+        &mut network,
+        [(bidder_ssk, genesis_note)].iter(),
+        &psk,
+        true,
+        &bidder_ssk.view_key(),
+        fee,
+        Some((&secret_spend_key.view_key(), crossover)),
+        Some((contract_id, bid_tx)),
+    )
+    .expect("Failed execute tx build");
+
+    // Add leaf to the Contract's tree and get it's pos index back.
+    // This is done by calling the execute method of the transfer contract.
+
+    /*
     // Sign the t_e (expiration) of the Bid.
     // TODO: Fetch the correct expiration and put it as message!!!
     let signature = Signature::new(
@@ -179,23 +242,24 @@ fn bid_contract_workflow_works() {
         .expect("Failed to call extend_bid method");
 
     assert!(call_result);
+    */
 }
 
-fn initialize_transfer_contract() -> (SecretSpendKey, Contract) {
+fn initialize_transfer_contract() -> (SecretSpendKey, Note, Contract) {
     let bidder_ssk = {
         let (a, b) = (JubJubScalar::from(2u64), JubJubScalar::from(3u64));
         SecretSpendKey::new(a, b)
     };
     let bidder_psk = PublicSpendKey::from(bidder_ssk);
-    let bidder_note = Note::transparent(
+    let gas_note = Note::transparent(
         &mut rand::thread_rng(),
         &bidder_psk,
         10_000_000_000_000,
     );
     let contract = Contract::new(
-        TransferContract::try_from(bidder_note).unwrap(),
+        TransferContract::try_from(gas_note).unwrap(),
         TRANSFER_CONTRACT_BYTECODE.to_vec(),
     );
 
-    (bidder_ssk, contract)
+    (bidder_ssk, gas_note, contract)
 }
