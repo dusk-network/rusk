2230b92157a0de4d76436a53c4543eb2960e2651
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-02-24 12:33:37 +0100

    Return early from `fetch_existing_nullifiers`
    Resolves: #49

--- wallet-core/src/ffi.rs
+++ wallet-core/src/ffi.rs
@@ -340,6 +340,11 @@ impl StateClient for FfiStateClient {
         let nullifiers_len = nullifiers.len();
         let mut nullifiers_buf = vec![0u8; BlsScalar::SIZE * nullifiers_len];
 
+        /// If no nullifiers come in, then none of them exist in the state.
+        if nullifiers_len == 0 {
+            return Ok(vec![]);
+        }
+
         let mut writer = &mut nullifiers_buf[..];
 
         for nullifier in nullifiers {
