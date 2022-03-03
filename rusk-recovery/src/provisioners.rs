e8b15a3dca952d2cbb6b5f8d8b475e73d2b8c0e9
Herr Seppia <seppia@dusk.network>
2022-03-03 16:19:39 +0100

    Add multiple provisioners in the genesis contracts

--- rusk-recovery/src/provisioners.rs
+++ rusk-recovery/src/provisioners.rs
@@ -9,18 +9,25 @@ use dusk_bytes::Serializable;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    pub static ref PROVISIONERS: [PublicKey; 1] =
-        [parse_key(include_bytes!("../provisioners/dusk.0.cpk")),];
+    pub static ref PROVISIONERS: [PublicKey; 5] = {
+        [
+            parse_key(&include_str!("../provisioners/node_0.pub")),
+            parse_key(&include_str!("../provisioners/node_1.pub")),
+            parse_key(&include_str!("../provisioners/node_2.pub")),
+            parse_key(&include_str!("../provisioners/node_3.pub")),
+            parse_key(&include_str!("../provisioners/node_4.pub")),
+        ]
+    };
 }
 
-fn parse_key(bytes: &[u8]) -> PublicKey {
+fn parse_key(bs58: &str) -> PublicKey {
+    let bytes = bs58::decode(bs58).into_vec().expect("Base58 decoding");
     // FIXME: This is only done because `BadLength` is not implemented for
     //  `dusk_bls12_381_sign::Error`. Otherwise we could use
     //  `PublicKey::from_slice`
     assert_eq!(bytes.len(), PublicKey::SIZE);
-
     let mut key_bytes = [0u8; PublicKey::SIZE];
-    key_bytes.copy_from_slice(bytes);
+    key_bytes.copy_from_slice(&bytes[..]);
 
     PublicKey::from_bytes(&key_bytes)
         .expect("Genesis consensus key to be valid")
