1f545b20d34c1d4fa373a8d62685edf16a8e623e
Herr Seppia <seppia@dusk.network>
2022-09-01 18:41:53 +0200

    rusk-recovery: add `use_prebuilt_contracts` flag
    - Allow to build the genesis state using prebuilt contracts
    - Always print the current network state root

--- rusk-recovery/src/state.rs
+++ rusk-recovery/src/state.rs
@@ -19,7 +19,9 @@ use rusk_abi::dusk::*;
 use rusk_vm::{Contract, NetworkState, NetworkStateId};
 use stake_contract::{Stake, StakeContract, MINIMUM_STAKE};
 use std::error::Error;
+use std::fs::File;
 use std::io::{Cursor, Read};
+use std::path::{Path, PathBuf};
 use std::{fs, io};
 use tracing::info;
 use tracing::log::error;
@@ -42,7 +44,11 @@ pub static FAUCET_KEY: Lazy<PublicSpendKey> = Lazy::new(|| {
     PublicSpendKey::from_bytes(bytes).expect("faucet should have a valid key")
 });
 
-fn diskbackend() -> BackendCtor<DiskBackend> {
+fn existing_diskbackend() -> BackendCtor<DiskBackend> {
+    BackendCtor::new(|| DiskBackend::new(rusk_profile::get_rusk_state_dir()?))
+}
+
+fn empty_diskbackend() -> BackendCtor<DiskBackend> {
     BackendCtor::new(|| {
         let dir = rusk_profile::get_rusk_state_dir()
             .expect("Failed to get Rusk profile directory");
@@ -129,9 +135,10 @@ fn genesis_stake(testnet: bool) -> StakeContract {
     stake_contract
 }
 
-pub fn deploy<B>(
+pub fn deploy_from_contracts<B>(
     testnet: bool,
     ctor: &BackendCtor<B>,
+    contracts_folder: Option<&PathBuf>,
 ) -> Result<NetworkStateId, Box<dyn Error>>
 where
     B: 'static + Backend,
@@ -141,19 +148,34 @@ where
     let theme = Theme::default();
     info!("{} new network state", theme.action("Generating"));
 
-    let transfer = Contract::new(
-        genesis_transfer(testnet),
-        &include_bytes!(
-      "../../target/wasm32-unknown-unknown/release/transfer_contract.wasm"
-    )[..],
-    );
+    let transfer_code = match contracts_folder {
+        Some(folder) => {
+            let mut buffer = Vec::new();
+            let mut file = File::open(folder.join("transfer_contract.wasm"))?;
+            file.read_to_end(&mut buffer)?;
+            buffer
+        }
+        None => include_bytes!(
+            "../../target/wasm32-unknown-unknown/release/transfer_contract.wasm"
+        )
+        .to_vec(),
+    };
 
-    let stake = Contract::new(
-        genesis_stake(testnet),
-        &include_bytes!(
+    let stake_code = match contracts_folder {
+        Some(folder) => {
+            let mut buffer = Vec::new();
+            let mut file = File::open(folder.join("stake_contract.wasm"))?;
+            file.read_to_end(&mut buffer)?;
+            buffer
+        }
+        None => include_bytes!(
             "../../target/wasm32-unknown-unknown/release/stake_contract.wasm"
-        )[..],
-    );
+        )
+        .to_vec(),
+    };
+
+    let transfer = Contract::new(genesis_transfer(testnet), transfer_code);
+    let stake = Contract::new(genesis_stake(testnet), stake_code);
 
     let mut network = NetworkState::default();
 
@@ -184,10 +206,21 @@ where
     Ok(state_id)
 }
 
+pub fn deploy<B>(
+    testnet: bool,
+    ctor: &BackendCtor<B>,
+) -> Result<NetworkStateId, Box<dyn Error>>
+where
+    B: 'static + Backend,
+{
+    deploy_from_contracts(testnet, ctor, None)
+}
+
 pub struct ExecConfig {
     pub build: bool,
     pub force: bool,
     pub testnet: bool,
+    pub use_prebuilt_contracts: bool,
 }
 
 pub fn exec(config: ExecConfig) -> Result<(), Box<dyn Error>> {
@@ -202,7 +235,7 @@ pub fn exec(config: ExecConfig) -> Result<(), Box<dyn Error>> {
     if !config.force && state_path.exists() && id_path.exists() {
         info!("{} existing state", theme.info("Found"));
 
-        let _ = NetworkStateId::read(&id_path)?;
+        try_network_restore()?;
 
         info!(
             "{} state id at {}",
@@ -214,8 +247,18 @@ pub fn exec(config: ExecConfig) -> Result<(), Box<dyn Error>> {
 
     if config.build {
         info!("{} new state", theme.info("Building"));
-        let state_id = deploy(config.testnet, &diskbackend())
-            .expect("Failed to deploy network state");
+
+        let contracts_folder = match config.use_prebuilt_contracts {
+            true => Some(get_contracts()?),
+            false => None,
+        };
+
+        let state_id = deploy_from_contracts(
+            config.testnet,
+            &empty_diskbackend(),
+            contracts_folder.as_ref(),
+        )
+        .expect("Failed to deploy network state");
 
         info!("{} persisted id", theme.success("Storing"));
         state_id.write(&id_path)?;
@@ -227,6 +270,7 @@ pub fn exec(config: ExecConfig) -> Result<(), Box<dyn Error>> {
             return Err(err);
         }
     }
+    try_network_restore()?;
 
     if !state_path.exists() {
         error!(
@@ -260,36 +304,76 @@ pub fn exec(config: ExecConfig) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn try_network_restore() -> Result<(), Box<dyn Error>> {
+    let theme = Theme::default();
+    Persistence::with_backend(&existing_diskbackend(), |_| Ok(()))?;
+    let network = NetworkState::new();
+    let id = NetworkStateId::read(rusk_profile::get_rusk_state_id_path()?)?;
+    let network = network.restore(id).expect("Failed to restore the state");
+    info!(
+        "{} restored {}",
+        theme.action("Root"),
+        hex::encode(network.root())
+    );
+    Ok(())
+}
+
 const STATE_URL: &str =
     "https://dusk-infra.ams3.digitaloceanspaces.com/keys/rusk-state.zip";
+const CONTRACTS_URL: &str =
+    "https://dusk-infra.ams3.digitaloceanspaces.com/keys/contracts.zip";
 
 /// Downloads the state into the rusk profile directory.
 fn download_state() -> Result<(), Box<dyn Error>> {
+    let mut profile_path = rusk_profile::get_rusk_profile_dir()?;
+    profile_path.pop();
+    download_and_unzip("state", STATE_URL, &profile_path)?;
+    Ok(())
+}
+
+fn get_contracts() -> Result<PathBuf, Box<dyn Error>> {
+    let folder = rusk_profile::get_rusk_profile_dir()?.join("contracts");
+    fs::create_dir_all(folder.as_path())
+        .expect("Unable to create contracts folder");
+
+    let transfer_missing = !folder.join("transfer_contract.wasm").is_file();
+    let stake_missing = !folder.join("stake_contract.wasm").is_file();
+
+    if transfer_missing || stake_missing {
+        download_and_unzip("contracts", CONTRACTS_URL, &folder)?;
+    }
+    Ok(folder)
+}
+
+/// Downloads a zip file and unzip it into the output directory.
+fn download_and_unzip(
+    description: &str,
+    uri: &str,
+    output: &Path,
+) -> Result<(), Box<dyn Error>> {
     let theme = Theme::default();
 
     let mut buffer = vec![];
-    let response = request::get(STATE_URL, &mut buffer)?;
+    let response = request::get(uri, &mut buffer)?;
 
     // only accept success codes.
     if !response.status_code().is_success() {
         return Err(format!(
-            "State download error: HTTP {}",
+            "{} download error: HTTP {}",
+            description,
             response.status_code()
         )
         .into());
     }
 
-    info!("{} state archive into", theme.info("Unzipping"));
+    info!("{} {} archive into", theme.info("Unzipping"), description);
 
     let reader = Cursor::new(buffer);
     let mut zip = ZipArchive::new(reader)?;
 
-    let mut profile_path = rusk_profile::get_rusk_profile_dir()?;
-    profile_path.pop();
-
     for i in 0..zip.len() {
         let mut entry = zip.by_index(i)?;
-        let entry_path = profile_path.join(entry.name());
+        let entry_path = output.join(entry.name());
 
         if entry.is_dir() {
             let _ = fs::create_dir_all(entry_path);
