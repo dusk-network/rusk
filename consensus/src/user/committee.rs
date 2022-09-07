7b57b8956f3b685470d639eede0ae0d5354ab9ed
goshawk <goshawk@dusk.network>
2022-09-07 17:25:11 +0300

    consensus: Implement quorum and bits methods in Committee

--- consensus/src/user/committee.rs
+++ consensus/src/user/committee.rs
@@ -4,15 +4,23 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+use crate::consensus::CONSENSUS_QUORUM_THRESHOLD;
 use crate::user::provisioners::{Provisioners, PublicKey};
 use crate::user::sortition;
+use crate::util::cluster::Cluster;
+use math::round;
 use std::collections::BTreeMap;
+use std::mem;
+use tracing::error;
 
 #[allow(unused)]
-// 0: BLS public key mapped to its occurrences.
-// 1: This provisioner BLS public key.
 #[derive(Default, Debug, Clone)]
-pub struct Committee(BTreeMap<PublicKey, usize>, PublicKey);
+pub struct Committee {
+    members: BTreeMap<PublicKey, usize>,
+    this_member_key: PublicKey,
+    cfg: sortition::Config,
+    total: usize,
+}
 
 #[allow(unused)]
 impl Committee {
@@ -22,40 +30,72 @@ impl Committee {
         cfg: sortition::Config,
     ) -> Self {
         // Generate committee using deterministic sortition.
-        let res = provisioners.create_committee(cfg);
+        let res = provisioners.create_committee(&cfg);
 
         // Turn the raw vector into a hashmap where we map a pubkey to its occurrences.
-        let mut committee = Self(BTreeMap::new(), pubkey_bls);
+        let mut committee = Self {
+            members: BTreeMap::new(),
+            this_member_key: pubkey_bls,
+            cfg: cfg.clone(),
+            total: 0,
+        };
+
         for member_key in res.as_slice() {
-            *committee.0.entry(*member_key).or_insert(0) += 1;
+            *committee.members.entry(*member_key).or_insert(0) += 1;
+            committee.total += 1;
         }
 
         committee
     }
 
     pub fn is_member(&self, pubkey_bls: PublicKey) -> bool {
-        self.0.contains_key(&pubkey_bls)
+        self.members.contains_key(&pubkey_bls)
     }
 
     pub fn am_member(&self) -> bool {
-        self.is_member(self.1)
+        self.is_member(self.this_member_key)
     }
 
     // get_my_pubkey returns this provisioner BLS public key.
     pub fn get_my_pubkey(&self) -> PublicKey {
-        self.1
+        self.this_member_key
     }
 
     pub fn votes_for(&self, pubkey_bls: PublicKey) -> Option<&usize> {
-        self.0.get(&pubkey_bls)
+        self.members.get(&pubkey_bls)
     }
 
     // get_occurrences returns values in a vec
     pub fn get_occurrences(&self) -> Vec<usize> {
-        self.0.clone().into_values().collect()
+        self.members.clone().into_values().collect()
     }
 
     pub fn size(&self) -> usize {
-        self.0.len()
+        self.members.len()
+    }
+
+    pub fn quorum(&self) -> usize {
+        let size = self.total as f64;
+        round::ceil(size * CONSENSUS_QUORUM_THRESHOLD, 2) as usize
+    }
+
+    pub fn bits(&self, voters: &Cluster<PublicKey>) -> u64 {
+        let mut bits: u64 = 0;
+
+        debug_assert!(self.members.len() <= mem::size_of_val(&bits) * 8);
+
+        let mut pos = 0;
+        for item in voters.0.iter() {
+            pos = 0;
+            for member in self.members.iter() {
+                pos += 1;
+                if member.0 == item.0 {
+                    bits |= 1 << pos; // flip the i-th bit to 1
+                    break;
+                }
+            }
+        }
+
+        bits
     }
 }
