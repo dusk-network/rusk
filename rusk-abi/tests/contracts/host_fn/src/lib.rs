692cf6d45f378cae9e39def2010e609ed3416aad
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-10-25 22:29:13 +0200

    Modify `rusk-abi` test contract to use `piecrust`

--- rusk-abi/tests/contracts/host_fn/src/lib.rs
+++ rusk-abi/tests/contracts/host_fn/src/lib.rs
@@ -9,254 +9,92 @@
 #![deny(clippy::all)]
 
 extern crate alloc;
+use alloc::vec::Vec;
 
-use canonical_derive::Canon;
+use dusk_bls12_381::BlsScalar;
+use dusk_bls12_381_sign::{
+    PublicKey as BlsPublicKey, Signature as BlsSignature,
+};
+use dusk_bytes::Serializable;
+use dusk_pki::PublicKey;
+use dusk_plonk::proof_system::Proof;
+use dusk_schnorr::Signature;
+use piecrust_uplink::{ModuleId, State};
+use rusk_abi::{hash::Hasher, CircuitType, PublicInput};
 
-// query ids
-pub const HASH: u8 = 0;
-pub const VERIFY: u8 = 1;
-pub const SCHNORR_SIGNATURE: u8 = 2;
-pub const BLS_SIGNATURE: u8 = 3;
-pub const GET_PAYMENT_INFO: u8 = 4;
-pub const SPONGE_HASH: u8 = 5;
+#[no_mangle]
+static SELF_ID: ModuleId = ModuleId::uninitialized();
 
-// transaction ids
-pub const SOMETHING: u8 = 0;
+static mut STATE: State<HostFnTest> = State::new(HostFnTest);
 
-#[derive(Clone, Canon, Debug, Default)]
-pub struct HostFnTest {}
+#[derive(Clone, Debug, Default)]
+pub struct HostFnTest;
 
 impl HostFnTest {
-    pub fn new() -> Self {
-        HostFnTest {}
+    pub fn hash(&self, scalars: Vec<BlsScalar>) -> BlsScalar {
+        let mut hasher = Hasher::new();
+        for scalar in scalars {
+            hasher.update(&scalar.to_bytes());
+        }
+        hasher.update(b"dusk network rocks");
+        hasher.finalize()
+    }
+
+    pub fn poseidon_hash(&self, scalars: Vec<BlsScalar>) -> BlsScalar {
+        rusk_abi::poseidon_hash(scalars)
+    }
+
+    pub fn verify_proof(
+        &self,
+        ty: CircuitType,
+        proof: Proof,
+        public_inputs: Vec<PublicInput>,
+    ) -> bool {
+        rusk_abi::verify_proof(ty, proof, public_inputs)
+    }
+
+    pub fn verify_schnorr(
+        &self,
+        msg: BlsScalar,
+        pk: PublicKey,
+        sig: Signature,
+    ) -> bool {
+        rusk_abi::verify_schnorr(msg, pk, sig)
+    }
+
+    pub fn verify_bls(
+        &self,
+        msg: Vec<u8>,
+        pk: BlsPublicKey,
+        sig: BlsSignature,
+    ) -> bool {
+        rusk_abi::verify_bls(msg, pk, sig)
     }
 }
 
-#[cfg(target_arch = "wasm32")]
-mod hosted {
-    use super::*;
-
-    use alloc::vec::Vec;
-
-    use canonical::{Canon, CanonError, Sink, Source};
-    use dusk_abi::ReturnValue;
-
-    use dusk_bls12_381::BlsScalar;
-    use dusk_bls12_381_sign::{
-        PublicKey as BlsPublicKey, Signature as BlsSignature,
-    };
-    use dusk_bytes::Serializable;
-    use dusk_pki::{PublicKey, PublicSpendKey};
-    use dusk_schnorr::Signature;
-    use rusk_abi::{hash, PaymentInfo, PublicInput};
-
-    const PAGE_SIZE: usize = 1024 * 4;
-
-    impl HostFnTest {
-        pub fn sponge_hash(&self, scalars: Vec<BlsScalar>) -> BlsScalar {
-            rusk_abi::poseidon_hash(scalars)
-        }
-
-        pub fn hash(&self, scalars: &[BlsScalar]) -> BlsScalar {
-            let mut hasher = hash::Hasher::new();
-            for scalar in scalars {
-                hasher.update(&scalar.to_bytes());
-            }
-            hasher.update(b"dusk network rocks");
-            hasher.finalize()
-        }
-
-        pub fn verify(
-            &self,
-            proof: Vec<u8>,
-            verifier_data: Vec<u8>,
-            pi_values: Vec<PublicInput>,
-        ) -> bool {
-            rusk_abi::verify_proof(proof, verifier_data, pi_values)
-        }
-
-        pub fn schnorr_signature(
-            &self,
-            sig: Signature,
-            pk: PublicKey,
-            message: BlsScalar,
-        ) -> bool {
-            rusk_abi::verify_schnorr_sign(sig, pk, message)
-        }
-
-        pub fn bls_signature(
-            &self,
-            sig: BlsSignature,
-            pk: BlsPublicKey,
-            message: Vec<u8>,
-        ) -> bool {
-            rusk_abi::verify_bls_sign(sig, pk, message)
-        }
-
-        pub fn get_payment_info(&self) -> rusk_abi::PaymentInfo {
-            rusk_abi::payment_info(dusk_abi::callee())
-        }
-    }
-
-    fn query(bytes: &mut [u8; PAGE_SIZE]) -> Result<(), CanonError> {
-        let mut source = Source::new(&bytes[..]);
-
-        // decode self.
-        let slf = HostFnTest::decode(&mut source)?;
-
-        // decode query id
-        let qid = u8::decode(&mut source)?;
-        match qid {
-            SPONGE_HASH => {
-                let arg = Vec::<BlsScalar>::decode(&mut source)?;
-
-                let ret = slf.sponge_hash(arg);
-
-                let r = {
-                    // return value
-                    let wrapped_return = ReturnValue::from_canon(&ret);
-
-                    let mut sink = Sink::new(&mut bytes[..]);
-
-                    wrapped_return.encode(&mut sink)
-                };
-
-                Ok(r)
-            }
-
-            HASH => {
-                let arg = Vec::<BlsScalar>::decode(&mut source)?;
-
-                let ret = slf.hash(&arg);
-
-                let r = {
-                    // return value
-                    let wrapped_return = ReturnValue::from_canon(&ret);
-
-                    let mut sink = Sink::new(&mut bytes[..]);
-
-                    wrapped_return.encode(&mut sink)
-                };
-
-                Ok(r)
-            }
-
-            VERIFY => {
-                let proof = Vec::<u8>::decode(&mut source)?;
-                let verifier_data = Vec::<u8>::decode(&mut source)?;
-                let pi_values =
-                    Vec::<rusk_abi::PublicInput>::decode(&mut source)?;
-
-                let ret = slf.verify(proof, verifier_data, pi_values);
-
-                let r = {
-                    // return value
-                    let wrapped_return = ReturnValue::from_canon(&ret);
-
-                    let mut sink = Sink::new(&mut bytes[..]);
-
-                    wrapped_return.encode(&mut sink)
-                };
-
-                Ok(r)
-            }
-
-            SCHNORR_SIGNATURE => {
-                let sig = Signature::decode(&mut source)?;
-                let pk = PublicKey::decode(&mut source)?;
-                let message = BlsScalar::decode(&mut source)?;
-
-                let ret = slf.schnorr_signature(sig, pk, message);
-
-                let r = {
-                    // return value
-                    let wrapped_return = ReturnValue::from_canon(&ret);
-
-                    let mut sink = Sink::new(&mut bytes[..]);
-
-                    wrapped_return.encode(&mut sink)
-                };
-
-                Ok(r)
-            }
-
-            BLS_SIGNATURE => {
-                let sig = BlsSignature::decode(&mut source)?;
-                let pk = BlsPublicKey::decode(&mut source)?;
-                let message = Vec::<u8>::decode(&mut source)?;
-
-                let ret = slf.bls_signature(sig, pk, message);
-
-                let r = {
-                    // return value
-                    let wrapped_return = ReturnValue::from_canon(&ret);
-
-                    let mut sink = Sink::new(&mut bytes[..]);
-
-                    wrapped_return.encode(&mut sink)
-                };
-
-                Ok(r)
-            }
-
-            GET_PAYMENT_INFO => {
-                let ret = slf.get_payment_info();
-
-                let r = {
-                    // return value
-                    let wrapped_return = ReturnValue::from_canon(&ret);
-
-                    let mut sink = Sink::new(&mut bytes[..]);
-
-                    wrapped_return.encode(&mut sink)
-                };
-
-                Ok(r)
-            }
-
-            rusk_abi::PAYMENT_INFO => {
-                let ret = PaymentInfo::Any(Some(PublicSpendKey::new(
-                    dusk_jubjub::JubJubExtended::default(),
-                    dusk_jubjub::JubJubExtended::default(),
-                )));
-
-                let r = {
-                    // return value
-                    let wrapped_return = ReturnValue::from_canon(&ret);
-
-                    let mut sink = Sink::new(&mut bytes[..]);
-
-                    wrapped_return.encode(&mut sink)
-                };
-
-                Ok(r)
-            }
-
-            _ => panic!(""),
-        }
-    }
-
-    #[no_mangle]
-    fn q(bytes: &mut [u8; PAGE_SIZE]) {
-        // todo, handle errors here
-        let _ = query(bytes);
-    }
-
-    fn transaction(bytes: &mut [u8; PAGE_SIZE]) -> Result<(), CanonError> {
-        let mut source = Source::new(bytes);
-
-        // decode self.
-        let mut _slf = HostFnTest::decode(&mut source)?;
-        // decode transaction id
-        let tid = u8::decode(&mut source)?;
-        match tid {
-            _ => panic!(""),
-        }
-    }
-
-    #[no_mangle]
-    fn t(bytes: &mut [u8; PAGE_SIZE]) {
-        // todo, handle errors here
-        transaction(bytes).unwrap()
-    }
+#[no_mangle]
+unsafe fn hash(arg_len: u32) -> u32 {
+    piecrust_uplink::wrap_query(arg_len, |scalars| STATE.hash(scalars))
+}
+#[no_mangle]
+unsafe fn poseidon_hash(arg_len: u32) -> u32 {
+    piecrust_uplink::wrap_query(arg_len, |scalars| STATE.poseidon_hash(scalars))
+}
+#[no_mangle]
+unsafe fn verify_proof(arg_len: u32) -> u32 {
+    piecrust_uplink::wrap_query(arg_len, |(ty, proof, public_inputs)| {
+        STATE.verify_proof(ty, proof, public_inputs)
+    })
+}
+#[no_mangle]
+unsafe fn verify_schnorr(arg_len: u32) -> u32 {
+    piecrust_uplink::wrap_query(arg_len, |(msg, pk, sig)| {
+        STATE.verify_schnorr(msg, pk, sig)
+    })
+}
+#[no_mangle]
+unsafe fn verify_bls(arg_len: u32) -> u32 {
+    piecrust_uplink::wrap_query(arg_len, |(msg, pk, sig)| {
+        STATE.verify_bls(msg, pk, sig)
+    })
 }
