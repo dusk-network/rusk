9ae32f96a60e17043e8c68187fd3d1fa1d4d60fd
Victor Lopez <victor@codx.io>
2022-10-29 01:48:39 +0200

    contracts: Add governance contract

---
+++ contracts/governance/src/governance.rs
@@ -0,0 +1,274 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use core::iter;
+
+use alloc::vec;
+use alloc::vec::Vec;
+use canonical_derive::Canon;
+use dusk_bls12_381::BlsScalar;
+use dusk_hamt::Map;
+use dusk_jubjub::GENERATOR_EXTENDED;
+use dusk_pki::PublicKey;
+use dusk_schnorr::Signature;
+
+use crate::*;
+
+#[derive(Debug, Default, Clone, Canon)]
+pub struct GovernanceContract {
+    pub(crate) seeds: Map<BlsScalar, ()>,
+    pub(crate) balances: Map<PublicKey, u64>,
+    pub(crate) whitelist: Map<PublicKey, ()>,
+    pub(crate) paused: bool,
+    pub(crate) total_supply: u64,
+}
+
+impl GovernanceContract {
+    /// Authority of the contract
+    ///
+    /// Will have to be defined in the constant space so the bytecode of the
+    /// contract will be changed as the authority does
+    pub const AUTHORITY: PublicKey =
+        PublicKey::from_raw_unchecked(GENERATOR_EXTENDED);
+
+    fn validate_seed(
+        &mut self,
+        arguments: Vec<BlsScalar>,
+        signature: Signature,
+    ) -> Result<(), Error> {
+        let seed = arguments[0];
+
+        if self.seeds.get(&seed)?.is_some() {
+            return Err(Error::SeedAlreadyUsed);
+        }
+
+        #[cfg(target_arch = "wasm32")]
+        if !rusk_abi::verify_schnorr_sign(
+            signature,
+            Self::AUTHORITY,
+            rusk_abi::poseidon_hash(arguments),
+        ) {
+            return Err(Error::InvalidSignature);
+        }
+
+        #[cfg(not(target_arch = "wasm32"))]
+        if !signature
+            .verify(&Self::AUTHORITY, dusk_poseidon::sponge::hash(&arguments))
+        {
+            return Err(Error::InvalidSignature);
+        }
+
+        self.seeds.insert(seed, ())?;
+
+        Ok(())
+    }
+
+    fn check_pause(&self) -> Result<(), Error> {
+        (!self.paused).then_some(()).ok_or(Error::ContractIsPaused)
+    }
+
+    fn is_allowed(&self, address: &PublicKey) -> Result<(), Error> {
+        self.whitelist
+            .get(address)?
+            .as_deref()
+            .copied()
+            .ok_or(Error::AddressIsNotWhitelisted)
+    }
+
+    pub fn pause(
+        &mut self,
+        seed: BlsScalar,
+        signature: Signature,
+    ) -> Result<(), Error> {
+        self.validate_seed(
+            vec![seed, BlsScalar::from(TX_PAUSE as u64)],
+            signature,
+        )?;
+
+        self.paused = true;
+
+        Ok(())
+    }
+
+    pub fn unpause(
+        &mut self,
+        seed: BlsScalar,
+        signature: Signature,
+    ) -> Result<(), Error> {
+        self.validate_seed(
+            vec![seed, BlsScalar::from(TX_UNPAUSE as u64)],
+            signature,
+        )?;
+        self.paused = false;
+
+        Ok(())
+    }
+
+    pub fn allow(
+        &mut self,
+        seed: BlsScalar,
+        signature: Signature,
+        address: PublicKey,
+    ) -> Result<(), Error> {
+        self.validate_seed(
+            iter::once([seed, BlsScalar::from(TX_ALLOW as u64)])
+                .chain(iter::once(address.as_ref().to_hash_inputs()))
+                .flatten()
+                .collect(),
+            signature,
+        )?;
+        self.whitelist.insert(address, ())?;
+
+        Ok(())
+    }
+
+    pub fn block(
+        &mut self,
+        seed: BlsScalar,
+        signature: Signature,
+        address: PublicKey,
+    ) -> Result<(), Error> {
+        self.validate_seed(
+            iter::once([seed, BlsScalar::from(TX_BLOCK as u64)])
+                .chain(iter::once(address.as_ref().to_hash_inputs()))
+                .flatten()
+                .collect(),
+            signature,
+        )?;
+        self.whitelist.remove(&address)?;
+
+        Ok(())
+    }
+
+    pub fn mint(
+        &mut self,
+        seed: BlsScalar,
+        signature: Signature,
+        address: PublicKey,
+        value: u64,
+    ) -> Result<(), Error> {
+        self.validate_seed(
+            iter::once([seed, BlsScalar::from(TX_MINT as u64)])
+                .chain(iter::once(address.as_ref().to_hash_inputs()))
+                .flatten()
+                .chain(iter::once(BlsScalar::from(value)))
+                .collect(),
+            signature,
+        )?;
+        self.check_pause()?;
+        self.is_allowed(&address)?;
+
+        self.total_supply = self
+            .total_supply
+            .checked_add(value)
+            .ok_or(Error::BalanceOverflow)?;
+
+        let value = self
+            .balances
+            .get(&address)?
+            .as_deref()
+            .copied()
+            .unwrap_or(0)
+            .checked_add(value)
+            .ok_or(Error::BalanceOverflow)?;
+
+        self.balances.insert(address, value)?;
+
+        Ok(())
+    }
+
+    pub fn burn(
+        &mut self,
+        seed: BlsScalar,
+        signature: Signature,
+        address: PublicKey,
+        value: u64,
+    ) -> Result<(), Error> {
+        self.validate_seed(
+            iter::once([seed, BlsScalar::from(TX_BURN as u64)])
+                .chain(iter::once(address.as_ref().to_hash_inputs()))
+                .flatten()
+                .chain(iter::once(BlsScalar::from(value)))
+                .collect(),
+            signature,
+        )?;
+        self.check_pause()?;
+
+        self.total_supply = self.total_supply.saturating_sub(value);
+
+        let value = self
+            .balances
+            .get(&address)?
+            .as_deref()
+            .copied()
+            .unwrap_or(0)
+            .checked_sub(value)
+            .ok_or(Error::InsufficientBalance)?;
+
+        if value == 0 {
+            self.balances.remove(&address)?;
+        } else {
+            self.balances.insert(address, value)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn transfer(
+        &mut self,
+        seed: BlsScalar,
+        signature: Signature,
+        batch: Vec<Transfer>,
+    ) -> Result<(), Error> {
+        self.validate_seed(
+            iter::once([seed, BlsScalar::from(TX_TRANSFER as u64)])
+                .flatten()
+                .chain(batch.iter().flat_map(Transfer::as_scalars))
+                .collect(),
+            signature,
+        )?;
+        self.check_pause()?;
+
+        for Transfer {
+            from, to, amount, ..
+        } in batch
+        {
+            self.is_allowed(&from)?;
+
+            let base = self
+                .balances
+                .get(&from)?
+                .as_deref()
+                .copied()
+                .unwrap_or(0)
+                .checked_sub(amount)
+                .ok_or(Error::InsufficientBalance)?;
+
+            let target = self
+                .balances
+                .get(&to)?
+                .as_deref()
+                .copied()
+                .unwrap_or(0)
+                .checked_add(amount)
+                .ok_or(Error::BalanceOverflow)?;
+
+            if base == 0 {
+                self.balances.remove(&from)?;
+            } else {
+                self.balances.insert(from, base)?;
+            }
+
+            if target == 0 {
+                self.balances.remove(&to)?;
+            } else {
+                self.balances.insert(to, target)?;
+            }
+        }
+
+        Ok(())
+    }
+}
---
+++ contracts/governance/src/wasm/bridge.rs
@@ -0,0 +1,79 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use crate::*;
+
+use canonical::{Canon, Sink, Source};
+use dusk_abi::{ContractState, ReturnValue};
+
+const PAGE_SIZE: usize = 1024 * 32;
+
+#[no_mangle]
+fn q(_bytes: &mut [u8; PAGE_SIZE]) {}
+
+#[no_mangle]
+fn t(bytes: &mut [u8; PAGE_SIZE]) {
+    let mut source = Source::new(bytes);
+
+    let mut contract =
+        GovernanceContract::decode(&mut source).expect("Failed to read state");
+    let tid = u8::decode(&mut source).expect("Failed to read tx ID");
+
+    match tid {
+        TX_PAUSE => {
+            let (seed, signature) = Canon::decode(&mut source).unwrap();
+
+            contract.pause(seed, signature).unwrap();
+        }
+
+        TX_UNPAUSE => {
+            let (seed, signature) = Canon::decode(&mut source).unwrap();
+
+            contract.unpause(seed, signature).unwrap();
+        }
+
+        TX_ALLOW => {
+            let (seed, signature, address) =
+                Canon::decode(&mut source).unwrap();
+
+            contract.allow(seed, signature, address).unwrap();
+        }
+
+        TX_BLOCK => {
+            let (seed, signature, address) =
+                Canon::decode(&mut source).unwrap();
+
+            contract.block(seed, signature, address).unwrap();
+        }
+
+        TX_MINT => {
+            let (seed, signature, address, value) =
+                Canon::decode(&mut source).unwrap();
+
+            contract.mint(seed, signature, address, value).unwrap();
+        }
+
+        TX_BURN => {
+            let (seed, signature, address, value) =
+                Canon::decode(&mut source).unwrap();
+
+            contract.burn(seed, signature, address, value).unwrap();
+        }
+
+        TX_TRANSFER => {
+            let (seed, signature, batch) = Canon::decode(&mut source).unwrap();
+
+            contract.transfer(seed, signature, batch).unwrap();
+        }
+
+        _ => panic!("Tx id not implemented"),
+    }
+
+    let mut sink = Sink::new(&mut bytes[..]);
+
+    ContractState::from_canon(&contract).encode(&mut sink);
+    ReturnValue::from_canon(&true).encode(&mut sink);
+}
