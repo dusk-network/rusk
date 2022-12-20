e9fcf0366f85578cd3d32bf22a1031b3820d98d4
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-12-20 17:57:09 +0100

    Port alice and bob contracts
    We also move their code to the contracts folder since they are
    contracts, albeit just used for test purposes.
    
    Co-authored-by: Milosz Muszynski <miloszm@users.noreply.github.com>

---
+++ contracts/bob/src/lib.rs
@@ -0,0 +1,36 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+#![no_std]
+#![feature(arbitrary_self_types)]
+
+extern crate alloc;
+
+mod state;
+use state::Bob;
+
+#[cfg(target_family = "wasm")]
+#[path = ""]
+mod wasm {
+    use super::*;
+
+    use rusk_abi::{PaymentInfo, State};
+
+    #[no_mangle]
+    static mut STATE: State<Bob> = State::new(Bob::new());
+
+    #[no_mangle]
+    unsafe fn ping(arg_len: u32) -> u32 {
+        rusk_abi::wrap_query(arg_len, |()| STATE.ping())
+    }
+
+    const PAYMENT_INFO: PaymentInfo = PaymentInfo::Transparent(None);
+
+    #[no_mangle]
+    fn payment_info(arg_len: u32) -> u32 {
+        rusk_abi::wrap_query(arg_len, |_: ()| PAYMENT_INFO)
+    }
+}
