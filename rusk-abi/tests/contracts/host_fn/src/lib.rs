6376777535fc88509d773a3cbcc18a6abbbae023
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-12-20 17:43:05 +0100

    rusk-abi: add payment info scaffold

--- rusk-abi/tests/contracts/host_fn/src/lib.rs
+++ rusk-abi/tests/contracts/host_fn/src/lib.rs
@@ -16,8 +16,7 @@ use dusk_bls12_381_sign::{Signature as BlsSignature, APK};
 use dusk_pki::PublicKey;
 use dusk_plonk::proof_system::Proof;
 use dusk_schnorr::Signature;
-use piecrust_uplink::{ModuleId, State};
-use rusk_abi::{CircuitType, PublicInput};
+use rusk_abi::{ModuleId, PaymentInfo, PublicInput, State};
 
 #[no_mangle]
 static SELF_ID: ModuleId = ModuleId::uninitialized();
@@ -38,11 +37,11 @@ impl HostFnTest {
 
     pub fn verify_proof(
         &self,
-        ty: CircuitType,
+        verifier_data: Vec<u8>,
         proof: Proof,
         public_inputs: Vec<PublicInput>,
     ) -> bool {
-        rusk_abi::verify_proof(ty, proof, public_inputs)
+        rusk_abi::verify_proof(verifier_data, proof, public_inputs)
     }
 
     pub fn verify_schnorr(
@@ -62,35 +61,51 @@ impl HostFnTest {
     ) -> bool {
         rusk_abi::verify_bls(msg, apk, sig)
     }
+
+    pub fn block_height(&self) -> u64 {
+        rusk_abi::block_height()
+    }
 }
 
 #[no_mangle]
 unsafe fn hash(arg_len: u32) -> u32 {
-    piecrust_uplink::wrap_query(arg_len, |scalars| STATE.hash(scalars))
+    rusk_abi::wrap_query(arg_len, |scalars| STATE.hash(scalars))
 }
 
 #[no_mangle]
 unsafe fn poseidon_hash(arg_len: u32) -> u32 {
-    piecrust_uplink::wrap_query(arg_len, |scalars| STATE.poseidon_hash(scalars))
+    rusk_abi::wrap_query(arg_len, |scalars| STATE.poseidon_hash(scalars))
 }
 
 #[no_mangle]
 unsafe fn verify_proof(arg_len: u32) -> u32 {
-    piecrust_uplink::wrap_query(arg_len, |(ty, proof, public_inputs)| {
-        STATE.verify_proof(ty, proof, public_inputs)
+    rusk_abi::wrap_query(arg_len, |(verifier_data, proof, public_inputs)| {
+        STATE.verify_proof(verifier_data, proof, public_inputs)
     })
 }
 
 #[no_mangle]
 unsafe fn verify_schnorr(arg_len: u32) -> u32 {
-    piecrust_uplink::wrap_query(arg_len, |(msg, pk, sig)| {
+    rusk_abi::wrap_query(arg_len, |(msg, pk, sig)| {
         STATE.verify_schnorr(msg, pk, sig)
     })
 }
 
 #[no_mangle]
 unsafe fn verify_bls(arg_len: u32) -> u32 {
-    piecrust_uplink::wrap_query(arg_len, |(msg, pk, sig)| {
+    rusk_abi::wrap_query(arg_len, |(msg, pk, sig)| {
         STATE.verify_bls(msg, pk, sig)
     })
+}
+
+#[no_mangle]
+unsafe fn block_height(arg_len: u32) -> u32 {
+    rusk_abi::wrap_query(arg_len, |_: ()| STATE.block_height())
+}
+
+const PAYMENT_INFO: PaymentInfo = PaymentInfo::Transparent(None);
+
+#[no_mangle]
+fn payment_info(arg_len: u32) -> u32 {
+    rusk_abi::wrap_query(arg_len, |_: ()| PAYMENT_INFO)
 }
