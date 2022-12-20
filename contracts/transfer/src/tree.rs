3b79af8dd9c7208d1e43af0c941b602a873e1095
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-12-20 18:04:44 +0100

    transfer-contract: upgrade to use `piecrust`
    We also introduce `transfer-contract-types` which is a crate exporting
    common functionality used to interact with the from both the host and other
    contracts.
    
    Resolves: #757

---
+++ contracts/transfer/src/tree.rs
@@ -0,0 +1,156 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use core::borrow::Borrow;
+use core::ops::Range;
+
+use dusk_bls12_381::BlsScalar;
+use dusk_poseidon::tree::{PoseidonBranch, PoseidonTree};
+use microkelvin::{Child, Compound, Step, Walk, Walker};
+use nstack::annotation::{Keyed, MaxKey};
+use ranno::Annotation;
+use transfer_contract_types::{TreeLeaf, TRANSFER_TREE_DEPTH};
+
+#[derive(Debug, Clone)]
+pub struct Tree {
+    tree: PoseidonTree<TreeLeaf, u64, TRANSFER_TREE_DEPTH>,
+}
+
+impl Tree {
+    pub const fn new() -> Self {
+        Self {
+            tree: PoseidonTree::new(),
+        }
+    }
+
+    pub fn get(&self, pos: u64) -> Option<TreeLeaf> {
+        self.tree.get(pos)
+    }
+
+    pub fn push(&mut self, leaf: TreeLeaf) -> u64 {
+        self.tree.push(leaf)
+    }
+
+    pub fn root(&self) -> BlsScalar {
+        self.tree.root()
+    }
+
+    pub fn leaves(
+        &self,
+        range: Range<u64>,
+    ) -> Option<impl Iterator<Item = &TreeLeaf>> {
+        self.tree
+            .annotated_iter_walk(HeightRangeWalker(range))
+            .map(|v| v.into_iter())
+    }
+
+    pub fn opening(
+        &self,
+        pos: u64,
+    ) -> Option<PoseidonBranch<TRANSFER_TREE_DEPTH>> {
+        self.tree.branch(pos)
+    }
+}
+
+/// Walker to find the leaves that are between two block heights.
+pub struct HeightRangeWalker(Range<u64>);
+
+impl<C, A> Walker<C, A> for HeightRangeWalker
+where
+    C: Compound<A>,
+    C::Leaf: Keyed<u64>,
+    A: Annotation<C> + Borrow<MaxKey<u64>>,
+{
+    fn walk(&mut self, walk: Walk<C, A>) -> Step {
+        for i in 0.. {
+            match walk.child(i) {
+                Child::Leaf(l) => {
+                    if self.0.contains(l.key()) {
+                        return Step::Found(i);
+                    }
+                }
+                Child::Node(n) => {
+                    let max_node_block_height = match *(*n.anno()).borrow() {
+                        MaxKey::NegativeInfinity => return Step::Abort,
+                        MaxKey::Maximum(max) => max,
+                    };
+
+                    if max_node_block_height >= self.0.start {
+                        return Step::Into(i);
+                    }
+                }
+                Child::Empty => {}
+                Child::EndOfNode => return Step::Advance,
+            }
+        }
+        unreachable!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::HeightRangeWalker;
+
+    use dusk_bls12_381::BlsScalar;
+    use dusk_poseidon::tree::{PoseidonLeaf, PoseidonTree};
+    use nstack::annotation::Keyed;
+
+    const TRANSFER_TREE_DEPTH: usize = 17;
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Leaf {
+        pub block_height: u64,
+        pub pos: u64,
+    }
+
+    impl Keyed<u64> for Leaf {
+        fn key(&self) -> &u64 {
+            &self.block_height
+        }
+    }
+
+    impl PoseidonLeaf for Leaf {
+        fn poseidon_hash(&self) -> BlsScalar {
+            BlsScalar::zero()
+        }
+
+        fn pos(&self) -> &u64 {
+            &self.pos
+        }
+
+        fn set_pos(&mut self, pos: u64) {
+            self.pos = pos;
+        }
+    }
+
+    type Tree = PoseidonTree<Leaf, u64, TRANSFER_TREE_DEPTH>;
+
+    #[test]
+    fn walk() {
+        let mut tree = Tree::new();
+
+        for i in 0..64 {
+            tree.push(Leaf {
+                block_height: i / 4,
+                pos: 0,
+            });
+        }
+
+        let (from_height, until_height) = (2, 5);
+        let range = from_height..until_height;
+
+        let walk = tree
+            .annotated_iter_walk(HeightRangeWalker(range.clone()))
+            .expect("There should be a walker");
+
+        for leaf in walk
+            .into_iter()
+            .take_while(move |item| item.block_height < until_height)
+        {
+            assert!(range.contains(&leaf.block_height));
+        }
+    }
+}
