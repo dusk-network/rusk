b8ca9b2259328861506c84279c8e387214c0e392
goshawk <goshawk@dusk.network>
2023-01-09 09:07:34 +0200

    Add node crate to rusk workspace and makefile

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,5 +1,11 @@
-use dusk_consensus::user::provisioners::Provisioners;
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
 
+use dusk_consensus::user::provisioners::Provisioners;
+#[derive(Debug)]
 pub enum Error {}
 
 /// Reads config parameters
