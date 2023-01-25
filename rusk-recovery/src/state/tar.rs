4de5a027aae590435348d6b48c5669163209e7ec
Herr Seppia <seppia@dusk.network>
2023-01-25 10:00:15 +0100

    rusk-recovery: Add support for tar archives
    Resolves #740

--- rusk-recovery/src/state/tar.rs
+++ rusk-recovery/src/state/tar.rs
@@ -4,17 +4,28 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+use flate2::{read, write, Compression};
 use std::error::Error;
+use std::fs::File;
 use std::path::Path;
+use tar::Archive;
 
 use super::zip;
 
 /// Unarchive files into a destination folder
 pub fn unarchive(buffer: &[u8], output: &Path) -> Result<(), Box<dyn Error>> {
-    zip::unzip(buffer, output)
+    let tar = read::GzDecoder::new(buffer);
+    let mut archive = Archive::new(tar);
+    archive
+        .unpack(output)
+        .or_else(|_| zip::unzip(buffer, output))
 }
 
 /// Archive a folder into a destination file.
 pub fn archive(src_dir: &Path, dst_file: &Path) -> Result<(), Box<dyn Error>> {
+    let tar_gz = File::create(dst_file)?;
+    let enc = write::GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = tar::Builder::new(enc);
+    tar.append_dir_all("", src_dir)?;
     Ok(())
 }
