3a33ea2dedae06bdf5d74c300bb728d0c8ae26a0
Herr Seppia <seppia@dusk.network>
2023-02-07 15:11:41 +0100

    rusk-recovery: Add support for governance contracts
    Resolves #800

--- rusk-recovery/src/state/snapshot/governance.rs
+++ rusk-recovery/src/state/snapshot/governance.rs
@@ -6,21 +6,24 @@
 
 use dusk_bls12_381_sign::PublicKey as BlsPublicKey;
 use dusk_bytes::Serializable;
-use rusk_abi::dusk::Dusk;
+use dusk_pki::PublicKey;
 use serde_derive::{Deserialize, Serialize};
 
 use super::wrapper::Wrapper;
 
 #[derive(Serialize, Deserialize, PartialEq, Eq)]
-pub struct GenesisStake {
-    pub(crate) address: Wrapper<BlsPublicKey, { BlsPublicKey::SIZE }>,
-    pub amount: Dusk,
-    pub eligibility: Option<u64>,
-    pub reward: Option<Dusk>,
+pub struct Governance {
+    pub contract_address: u8,
+    pub name: String,
+    pub(crate) authority: Wrapper<BlsPublicKey, { BlsPublicKey::SIZE }>,
+    pub(crate) broker: Wrapper<PublicKey, { PublicKey::SIZE }>,
 }
 
-impl GenesisStake {
-    pub fn address(&self) -> &BlsPublicKey {
-        &self.address
+impl Governance {
+    pub fn authority(&self) -> &BlsPublicKey {
+        &self.authority
+    }
+    pub fn broker(&self) -> &PublicKey {
+        &self.broker
     }
 }
