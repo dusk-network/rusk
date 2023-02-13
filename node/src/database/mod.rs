4788a34687002f87a6157f9795e76f87ac503a8e
goshawk <goshawk@dusk.network>
2023-02-13 10:09:24 +0200

    node: Define Mempool storage (trait) methods

--- node/src/database/mod.rs
+++ node/src/database/mod.rs
@@ -5,11 +5,14 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use dusk_consensus::commons::Block;
+
 pub mod rocksdb;
+
+use crate::data::ledger;
 use anyhow::Result;
 
 pub trait DB: Send + Sync + 'static {
-    type T<'a>: Tx;
+    type P<'a>: Persist;
 
     /// Creates or open a database located at this path.
     ///
@@ -19,7 +22,7 @@ pub trait DB: Send + Sync + 'static {
     /// Provides a managed execution of a read-only isolated transaction.
     fn view<F>(&self, f: F) -> Result<()>
     where
-        F: for<'a> FnOnce(Self::T<'a>) -> Result<()>;
+        F: for<'a> FnOnce(Self::P<'a>) -> Result<()>;
 
     /// Provides a managed execution of a read-write atomic transaction.
     ///
@@ -30,25 +33,44 @@ pub trait DB: Send + Sync + 'static {
     /// and no panic is raised on `fn` execution.
     fn update<F>(&self, f: F) -> Result<()>
     where
-        F: for<'a> FnOnce(&Self::T<'a>) -> Result<()>;
+        F: for<'a> FnOnce(&Self::P<'a>) -> Result<()>;
 
     fn close(&mut self);
 }
 
 /// Implements both read-write and read-only transactions to DB.
-pub trait Tx {
+
+pub trait Ledger {
     // Read-write transactions
     fn store_block(&self, b: &Block, persisted: bool) -> Result<()>;
     fn delete_block(&self, b: &Block) -> Result<()>;
     fn fetch_block(&self, hash: &[u8]) -> Result<Option<Block>>;
+}
 
-    // Candidate block functions
+pub trait Candidate {
+    // Read-write transactions
     fn store_candidate_block(&self, cm: Block) -> Result<()>;
     fn fetch_candidate_block(&self, hash: &[u8]) -> Result<Option<Block>>;
     fn clear_candidates(&self) -> Result<()>;
+}
+
+pub trait Mempool {
+    fn add_tx(&self, tx: &ledger::Transaction) -> Result<()>;
+    fn get_tx(&self, h: [u8; 32]) -> Result<Option<ledger::Transaction>>;
+    fn get_tx_exists(&self, h: [u8; 32]) -> bool;
+    fn delete_tx(&self, h: [u8; 32]) -> Result<bool>;
+
+    fn get_nullifiers_exist(&self, nullifiers: Vec<[u8; 32]>) -> bool;
+    fn get_txs_sorted_by_fee(
+        &self,
+        max_gas_limit: u64,
+    ) -> Result<Vec<Option<ledger::Transaction>>>;
+}
+
+pub trait Persist: Ledger + Candidate {
+    // Candidate block functions
 
     fn clear_database(&self) -> Result<()>;
-
     fn commit(self) -> Result<()>;
 }
 
