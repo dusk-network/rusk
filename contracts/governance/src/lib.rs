b2d1a969da31ce87cc7c465cbaf42b03d4b57131
zer0 <matteo@dusk.network>
2023-02-18 18:28:01 +0100

    governance-contract: simplify signature verification
    - Add contract's `verify` public method without hashing
    - Change `assert_seed` to only check the `seed`
    - Remove `assert_seed()` calls from contract's methods
    - Change `pause` to do not return value
    - Change `unpause` to do not return value
    - Remove contract's `mint` old method
    - Rename contract's  `mint_internal` to `mint` and made it public
    - Add `assert_running()` call to the new `mint`
    - Remove from all contract's methods `seed` and `signature` as arguments
    - Remove `as_scalars` from `Tansfer`
    - Update `expect` messages
    - Add signature and seed verification upfront, before calling any contract's method
    
    Resolves #844

--- contracts/governance/src/lib.rs
+++ contracts/governance/src/lib.rs
@@ -15,10 +15,7 @@ mod collection;
 mod error;
 mod governance;
 
-use core::iter;
-
 use canonical_derive::Canon;
-use dusk_bls12_381::BlsScalar;
 use dusk_pki::PublicKey;
 
 pub use error::Error;
@@ -36,18 +33,4 @@ pub struct Transfer {
     pub to: PublicKey,
     pub amount: u64,
     pub timestamp: u64,
-}
-
-impl Transfer {
-    pub fn as_scalars(&self) -> impl Iterator<Item = BlsScalar> {
-        let from = self.from.as_ref().to_hash_inputs();
-        let to = self.from.as_ref().to_hash_inputs();
-        let amount = BlsScalar::from(self.amount);
-        let timestamp = BlsScalar::from(self.timestamp);
-
-        iter::once(from)
-            .chain(iter::once(to))
-            .chain(iter::once([amount, timestamp]))
-            .flatten()
-    }
 }
--- contracts/governance/src/wasm/bridge.rs
+++ contracts/governance/src/wasm/bridge.rs
@@ -8,6 +8,8 @@ use crate::*;
 
 use canonical::{Canon, Sink, Source};
 use dusk_abi::{ContractState, ReturnValue};
+use dusk_bls12_381::BlsScalar;
+use dusk_bls12_381_sign::Signature;
 
 const PAGE_SIZE: usize = 1024 * 32;
 
@@ -18,44 +20,50 @@ fn q(_bytes: &mut [u8; PAGE_SIZE]) {}
 fn t(bytes: &mut [u8; PAGE_SIZE]) {
     let mut source = Source::new(bytes);
 
-    let mut contract =
-        GovernanceContract::decode(&mut source).expect("Failed to read state");
-    let tid = u8::decode(&mut source).expect("Failed to read tx ID");
+    let mut contract = GovernanceContract::decode(&mut source)
+        .expect("transact's data should have a GovernanceContract state");
+
+    let signature = Signature::decode(&mut source)
+        .expect("transact's data should have a Signature");
+
+    let len_u32 = u32::decode(&mut source)
+        .expect("transact's data should have the payload's length");
+
+    let offset = contract.encoded_len() + signature.encoded_len();
+    let len = offset + len_u32 as usize;
+
+    let seed = BlsScalar::decode(&mut source)
+        .expect("transact's data should have a seed");
+
+    contract
+        .verify(seed, signature, &bytes[offset..len])
+        .expect("contract's authority should match the signed transact's data");
+
+    let tid = u8::decode(&mut source)
+        .expect("transact's data should have a method's ID");
 
     match tid {
-        TX_PAUSE => {
-            let (seed, signature) = Canon::decode(&mut source)
-                .expect("[TX_PAUSE] Arguments parsing failed");
-
-            contract.pause(seed, signature).unwrap();
-        }
-
-        TX_UNPAUSE => {
-            let (seed, signature) = Canon::decode(&mut source)
-                .expect("[TX_UNPAUSE] Arguments parsing failed");
-
-            contract.unpause(seed, signature).unwrap();
-        }
-
+        TX_PAUSE => contract.pause(),
+        TX_UNPAUSE => contract.unpause(),
         TX_MINT => {
-            let (seed, signature, address, value) = Canon::decode(&mut source)
-                .expect("[TX_MINT] Arguments parsing failed");
+            let (address, value) = Canon::decode(&mut source)
+                .expect("[TX_MINT] arguments should be decoded");
 
-            contract.mint(seed, signature, address, value).unwrap();
+            contract.mint(address, value).unwrap();
         }
 
         TX_BURN => {
-            let (seed, signature, address, value) = Canon::decode(&mut source)
-                .expect("[TX_BURN] Arguments parsing failed");
+            let (address, value) = Canon::decode(&mut source)
+                .expect("[TX_BURN] arguments should be decoded");
 
-            contract.burn(seed, signature, address, value).unwrap();
+            contract.burn(address, value).unwrap();
         }
 
         TX_TRANSFER => {
-            let (seed, signature, batch) = Canon::decode(&mut source)
-                .expect("[TX_TRANSFER] Arguments parsing failed");
+            let batch = Canon::decode(&mut source)
+                .expect("[TX_TRANSFER] arguments should be decoded");
 
-            contract.transfer(seed, signature, batch).unwrap();
+            contract.transfer(batch).unwrap();
         }
 
         _ => panic!("Tx id not implemented"),
