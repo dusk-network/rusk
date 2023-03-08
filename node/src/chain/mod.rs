020c1433b7be24f2d565e74163f9c1d6733fcf3e
goshawk <goshawk@dusk.network>
2023-03-08 16:56:32 +0200

    node crate: Implement accept block in ledger on consensus achieved event

--- node/src/chain/mod.rs
+++ node/src/chain/mod.rs
@@ -4,50 +4,56 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use crate::data::Topics;
-use crate::utils::PendingQueue;
-use crate::{data, database, Network};
+mod consensus;
+mod genesis;
+
+use crate::database::{Candidate, Ledger};
+use crate::{database, Network};
 use crate::{LongLivedService, Message};
 use async_trait::async_trait;
-use dusk_consensus::commons::{ConsensusError, RoundUpdate};
+use dusk_consensus::commons::{ConsensusError, Database, RoundUpdate};
 use dusk_consensus::consensus::Consensus;
 use dusk_consensus::contract_state::{
     CallParams, Error, Operations, Output, StateRoot,
 };
 use dusk_consensus::user::provisioners::Provisioners;
-use dusk_consensus::util::pending_queue::PendingQueue as ConsensusPendingQueue; /* TODO: rename it or */
 use node_data::ledger::{Block, Hash};
+use node_data::message::AsyncQueue;
+use node_data::message::{Payload, Topics};
+use node_data::Serializable;
 use tokio::sync::{oneshot, Mutex, RwLock};
 use tokio::task::JoinHandle;
 
 use std::any;
 use std::sync::Arc;
 
+use self::consensus::Task;
+
 const TOPICS: &[u8] = &[
-    data::Topics::Block as u8,
-    data::Topics::NewBlock as u8,
-    data::Topics::Reduction as u8,
-    data::Topics::AggrAgreement as u8,
-    data::Topics::Agreement as u8,
+    Topics::Block as u8,
+    Topics::NewBlock as u8,
+    Topics::Reduction as u8,
+    Topics::AggrAgreement as u8,
+    Topics::Agreement as u8,
 ];
 
 pub struct ChainSrv {
-    inbound: PendingQueue<Message>,
+    /// Inbound wire messages queue
+    inbound: AsyncQueue<Message>,
 
-    upper: ConsensusLayer,
+    /// Most recently accepted block
+    most_recent_block: Block,
+
+    /// List of eligible provisioners of actual round
+    eligible_provisioners: Provisioners,
+
+    /// Upper layer consensus task
+    upper: consensus::Task,
 }
 
-impl Default for ChainSrv {
-    fn default() -> Self {
-        Self {
-            inbound: Default::default(),
-            upper: ConsensusLayer {
-                agreement_inbound: ConsensusPendingQueue::new("1"),
-                main_inbound: ConsensusPendingQueue::new("2"),
-                outbound: ConsensusPendingQueue::new("3"),
-                task_handle: None,
-            },
-        }
+impl Drop for ChainSrv {
+    fn drop(&mut self) {
+        self.upper.abort();
     }
 }
 
@@ -67,47 +73,58 @@ impl<N: Network, DB: database::DB> LongLivedService<N, DB> for ChainSrv {
         )
         .await?;
 
-        self.init::<N, DB>(&network).await?;
+        self.init::<N, DB>(&network, &db).await?;
 
         loop {
             tokio::select! {
+                // Receives results from the upper layer
+                recv = &mut self.upper.result.recv() => {
+                    if let Ok(res) = recv {
+                        match res {
+                            Ok(blk) => {
+                                if let Err(e) = self.accept_block::<DB>( &db, &blk).await {
+                                    tracing::error!("failed to accept block: {}", e);
+                                } else {
+                                    network.read().await.
+                                        broadcast(&Message::new_with_block(Box::new(blk))).await;
+                                }
+                            }
+                            Err(e) => {
+                                tracing::error!("consensus halted due to: {:?}", e);
+                            }
+                        }
+                    }
+                },
                 // Receives inbound wire messages.
                 // Component should either process it or re-route it to the next upper layer
                 recv = &mut self.inbound.recv() => {
-                    if let Ok(msg) = recv {
-                        match msg.topic {
-                            Topics::Block => {
-                                // Try to validate message
-                                if self.is_valid(&msg).is_ok() {
-                                    network.read().await.repropagate(&msg, 0).await;
-                                    self.handle_block_msg(&msg);
-
-                                    // Reset Consensus
-                                    self.upper.abort();
+                    if let Ok(mut msg) = recv {
+                        match &msg.payload {
+                            Payload::Block(b) => {
+                                if let Err(e) = self.accept_block::<DB>(&db, b).await {
+                                    tracing::error!("failed to accept block: {}", e);
+                                } else {
+                                    network.read().await.broadcast(&msg).await;
                                 }
                             }
 
                             // Re-route message to upper layer (in this case it is the Consensus layer)
-                            Topics::NewBlock | Topics::Reduction => {
+                            Payload::NewBlock(_) |  Payload::Reduction(_) => {
                                 self.upper.main_inbound.try_send(msg);
                             }
-                            Topics::Agreement | Topics::AggrAgreement => {
+                            Payload::Agreement(_) | Payload::AggrAgreement(_) => {
                                 self.upper.agreement_inbound.try_send(msg);
                             }
                             _ => tracing::warn!("invalid inbound message"),
                         }
                     }
                 },
-
                 // Re-routes messages originated from Consensus (upper) layer to the network layer.
                 recv = &mut self.upper.outbound.recv() => {
                     if let Ok(msg) = recv {
-                        network.read().await.repropagate(&msg, 0).await;
+                        network.read().await.broadcast(&msg).await;
                     }
                 },
-
-
-
             }
         }
     }
@@ -119,123 +136,53 @@ impl<N: Network, DB: database::DB> LongLivedService<N, DB> for ChainSrv {
 }
 
 impl ChainSrv {
+    pub fn new(keys_path: String) -> Self {
+        Self {
+            inbound: Default::default(),
+            upper: Task::new_with_keys(keys_path),
+            most_recent_block: Block::default(),
+            eligible_provisioners: Provisioners::default(),
+        }
+    }
+
     async fn init<N: Network, DB: database::DB>(
         &mut self,
         network: &Arc<RwLock<N>>,
+        db: &Arc<RwLock<DB>>,
     ) -> anyhow::Result<usize> {
-        self.upper.spawn();
+        (self.most_recent_block, self.eligible_provisioners) =
+            genesis::generate_state();
+
+        self.upper.spawn(
+            &self.most_recent_block.header,
+            &self.eligible_provisioners,
+            db,
+        );
 
         anyhow::Ok(0)
     }
 
-    fn is_valid(&self, msg: &Message) -> anyhow::Result<()> {
-        // TODO:
-        Ok(())
-    }
+    async fn accept_block<DB: database::DB>(
+        &mut self,
+        db: &Arc<RwLock<DB>>,
+        blk: &Block,
+    ) -> anyhow::Result<()> {
+        // Reset Consensus
+        self.upper.abort();
 
-    fn handle_block_msg(&mut self, msg: &Message) {
-        // TODO:
-    }
-}
+        // Persist block
+        db.read().await.update(|t| t.store_block(blk, true))?;
+        self.most_recent_block = blk.clone();
 
-struct ConsensusLayer {
-    agreement_inbound: ConsensusPendingQueue,
-    main_inbound: ConsensusPendingQueue,
-
-    outbound: ConsensusPendingQueue,
-
-    /// Keeps a  join_handle of a running consensus tokio task.
-    ///
-    /// None If no consensus is running,
-    task_handle: Option<JoinHandle<Result<Block, ConsensusError>>>,
-}
-
-impl ConsensusLayer {
-    fn abort(&mut self) {
-        let task = self.task_handle;
-        self.task_handle = None;
-        if let Some(v) = task {
-            v.abort();
-        }
-    }
-
-    fn spawn(&mut self) {
-        let mut c = Consensus::new(
-            self.main_inbound.clone(),
-            self.outbound.clone(),
-            self.agreement_inbound.clone(),
-            self.outbound.clone(),
-            Arc::new(Mutex::new(Executor {})),
-            Arc::new(Mutex::new(SimpleDB::default())),
+        // Restart Consensus.
+        // NB. This will be moved out of accept_block when Synchronizer is
+        // implemented.
+        self.upper.spawn(
+            &self.most_recent_block.header,
+            &self.eligible_provisioners,
+            db,
         );
 
-        let round_update = RoundUpdate::default(); // TODO:
-        let provisioners = Provisioners::default(); // TODO:
-
-        let layer_handle = tokio::spawn(async move {
-            let (_cancel_tx, cancel_rx) = oneshot::channel::<i32>();
-            c.spin(round_update, provisioners, cancel_rx).await
-        });
-
-        self.task_handle = Some(layer_handle);
-    }
-}
-
-/// Implements Executor trait to mock Contract Storage calls.
-pub struct Executor {}
-impl Operations for Executor {
-    fn verify_state_transition(
-        &self,
-        _params: CallParams,
-    ) -> Result<StateRoot, dusk_consensus::contract_state::Error> {
-        Ok([0; 32])
-    }
-
-    fn execute_state_transition(
-        &self,
-        _params: CallParams,
-    ) -> Result<Output, Error> {
-        Ok(Output::default())
-    }
-
-    fn accept(&self, _params: CallParams) -> Result<Output, Error> {
-        Ok(Output::default())
-    }
-
-    fn finalize(&self, _params: CallParams) -> Result<Output, Error> {
-        Ok(Output::default())
-    }
-
-    fn get_state_root(&self) -> Result<StateRoot, Error> {
-        Ok([0; 32])
-    }
-}
-
-#[derive(Debug, Default)]
-/// Implements Database trait to store candidates blocks in heap memory.
-pub struct SimpleDB {
-    // TODO: SimpleDB, database
-    candidates: std::collections::BTreeMap<Hash, Block>,
-}
-
-impl dusk_consensus::commons::Database for SimpleDB {
-    fn store_candidate_block(&mut self, b: Block) {
-        if b.header.hash == Hash::default() {
-            tracing::error!("candidate block with empty hash");
-            return;
-        }
-
-        self.candidates.entry(b.header.hash).or_insert(b);
-    }
-
-    fn get_candidate_block_by_hash(&self, h: &Hash) -> Option<(Hash, Block)> {
-        if let Some(v) = self.candidates.get_key_value(h) {
-            return Some((*v.0, v.1.clone()));
-        }
-        None
-    }
-
-    fn delete_candidate_blocks(&mut self) {
-        self.candidates.clear();
+        Ok(())
     }
 }
