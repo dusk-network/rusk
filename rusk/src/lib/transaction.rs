107aab27b8206352923054d004c25e0b028aa913
Herr Seppia <seppia@dusk.network>
2023-06-23 15:14:26 +0200

    rusk: remove `rusk-schema` dependency
    Co-authored-by: zer0 <matteo@dusk.network>
    Co-authored-by: Eduardo Leegwater Simões <eduardo@dusk.network>

--- rusk/src/lib/transaction.rs
+++ rusk/src/lib/transaction.rs
@@ -4,60 +4,14 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use std::ops::Deref;
-
-use dusk_bytes::{Error as BytesError, Serializable};
 use phoenix_core::Transaction;
-use rusk_abi::{ContractError, TRANSFER_CONTRACT};
-
-use rusk_schema::executed_transaction::error::Code;
-use rusk_schema::executed_transaction::Error;
-use rusk_schema::{TX_TYPE_TRANSFER, TX_VERSION};
+use rusk_abi::ContractError;
 
 /// The payload for a transfer transaction.
 ///
 /// Transfer transactions are the main type of transaction in the network.
 /// They can be used to transfer funds, call contracts, and even both at the
 /// same time.
-#[derive(Debug, Clone)]
-pub struct TransferPayload(Transaction);
-
-impl Deref for TransferPayload {
-    type Target = Transaction;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl TransferPayload {
-    pub fn into_inner(self) -> Transaction {
-        self.0
-    }
-
-    pub fn from_slice(buf: &[u8]) -> Result<Self, BytesError> {
-        Ok(Self(Transaction::from_slice(buf)?))
-    }
-}
-
-impl From<Transaction> for TransferPayload {
-    fn from(tx: Transaction) -> Self {
-        Self(tx)
-    }
-}
-
-impl From<TransferPayload> for rusk_schema::Transaction {
-    fn from(tx: TransferPayload) -> Self {
-        let payload = tx.to_var_bytes();
-
-        rusk_schema::Transaction {
-            version: TX_VERSION,
-            r#type: TX_TYPE_TRANSFER,
-            payload,
-        }
-    }
-}
-
 pub struct SpentTransaction(
     pub Transaction,
     pub u64,
@@ -67,39 +21,5 @@ pub struct SpentTransaction(
 impl SpentTransaction {
     pub fn into_inner(self) -> (Transaction, u64, Option<ContractError>) {
         (self.0, self.1, self.2)
-    }
-}
-
-impl From<SpentTransaction> for rusk_schema::ExecutedTransaction {
-    fn from(spent_tx: SpentTransaction) -> Self {
-        let (transaction, gas_spent, error) = spent_tx.into_inner();
-
-        let tx_hash_input_bytes = transaction.to_hash_input_bytes();
-        let tx_hash = rusk_abi::hash(tx_hash_input_bytes);
-
-        let error = error.map(|e| match e {
-            ContractError::PANIC => Error {
-                code: Code::ContractPanic.into(),
-                contract_id: TRANSFER_CONTRACT.to_bytes().to_vec(),
-                data: String::from("PANIC"),
-            },
-            ContractError::OUTOFGAS => Error {
-                code: Code::OutOfGas.into(),
-                contract_id: TRANSFER_CONTRACT.to_bytes().to_vec(),
-                data: String::from("OUT_OF_GAS"),
-            },
-            ContractError::OTHER(code) => Error {
-                code: Code::Other.into(),
-                contract_id: TRANSFER_CONTRACT.to_bytes().to_vec(),
-                data: format!("OTHER: {code}"),
-            },
-        });
-
-        rusk_schema::ExecutedTransaction {
-            error,
-            tx: Some(transaction.into()),
-            tx_hash: tx_hash.to_bytes().to_vec(),
-            gas_spent,
-        }
     }
 }
