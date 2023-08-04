fe54bab7cb625f9b34a9df137e8c18307ae4d0a4
Herr Seppia <seppia@dusk.network>
2023-08-04 13:29:29 +0200

    rusk: Add missing graphql handlers
    - Add blocks by range
    - Add transactions by block range

--- rusk/src/lib/http/chain/graphql.rs
+++ rusk/src/lib/http/chain/graphql.rs
@@ -13,7 +13,7 @@ use tx::*;
 use async_graphql::{Context, FieldError, FieldResult, Object};
 use node::database::rocksdb::Backend;
 use node::database::{Ledger, Register, DB};
-use node_data::ledger::{Block, SpentTransaction};
+use node_data::ledger::{Block, SpentTransaction, Transaction};
 
 use std::sync::Arc;
 use tokio::sync::RwLock;
@@ -39,32 +39,57 @@ impl Query {
             _ => Err(FieldError::new("Specify heigth or hash")),
         }
     }
+
     async fn tx(
         &self,
         ctx: &Context<'_>,
         hash: String,
     ) -> OptResult<SpentTransaction> {
         let ctx = ctx.data::<Ctx>()?;
-
         tx_by_hash(ctx, hash).await
+    }
+
+    async fn block_txs(
+        &self,
+        ctx: &Context<'_>,
+        last: Option<i32>,
+        range: Option<[i32; 2]>,
+        contract: Option<String>,
+    ) -> FieldResult<Vec<Transaction>> {
+        let blocks = self.blocks(ctx, last, range).await?;
+        let txs = blocks.into_iter().flat_map(|Block { txs, .. }| txs);
+
+        let txs =
+            match contract {
+                None => txs.collect(),
+                Some(contract) => {
+                    let contract = hex::decode(contract)?;
+                    txs.filter(|t| {
+                        let tx_contract =
+                            t.inner.call.as_ref().map(|(c, ..)| *c).unwrap_or(
+                                rusk_abi::TRANSFER_CONTRACT.to_bytes(),
+                            );
+
+                        tx_contract == contract[..]
+                    })
+                    .collect()
+                }
+            };
+
+        Ok(txs)
     }
 
     async fn blocks(
         &self,
         ctx: &Context<'_>,
         last: Option<i32>,
-        range: Option<Vec<i32>>,
+        range: Option<[i32; 2]>,
     ) -> FieldResult<Vec<Block>> {
         let ctx = ctx.data::<Ctx>()?;
 
         match (last, range) {
             (Some(count), None) => last_blocks(ctx, count).await,
-            (None, Some(range)) => {
-                let range: [i32; 2] = range.try_into().map_err(|_| {
-                    FieldError::new("You have to specify a range")
-                })?;
-                blocks_range(ctx, range[0], range[1]).await
-            }
+            (None, Some([from, to])) => blocks_range(ctx, from, to).await,
             _ => Err(FieldError::new("")),
         }
     }
