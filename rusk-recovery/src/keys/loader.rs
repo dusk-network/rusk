d52b489edeb7a342e6fec989912c3b9133b3e1ff
moana <moana@dusk.network>
2023-08-22 11:58:46 +0200

    rusk-recovery: Adapt to new exec circuit layout

--- rusk-recovery/src/keys/loader.rs
+++ rusk-recovery/src/keys/loader.rs
@@ -4,10 +4,12 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+use dusk_plonk::prelude::*;
+use license_circuits::LicenseCircuit;
+use transfer_circuits::*;
+
 use crate::keys::PUB_PARAMS;
 use crate::keys::{CircuitLoader, TRANSCRIPT_LABEL};
-use dusk_plonk::prelude::*;
-use license_circuits::*;
 
 macro_rules! loader_impl {
     ($loader:ident, $circuit:ty, $circuit_name:expr) => {
@@ -21,7 +23,7 @@ macro_rules! loader_impl {
                 $circuit_name
             }
 
-            fn compile_circuit(
+            fn compile_to_bytes(
                 &self,
             ) -> Result<(Vec<u8>, Vec<u8>), Box<dyn std::error::Error>> {
                 let (prover, verifier) = Compiler::compile::<$circuit>(
@@ -35,4 +37,44 @@ macro_rules! loader_impl {
     };
 }
 
+loader_impl!(
+    StctCircuitLoader,
+    SendToContractTransparentCircuit,
+    "SendToContractTransparent"
+);
+loader_impl!(
+    WfctCircuitLoader,
+    WithdrawFromTransparentCircuit,
+    "WithdrawFromContractTransparent"
+);
+loader_impl!(
+    StcoCircuitLoader,
+    SendToContractObfuscatedCircuit,
+    "SendToContractObfuscated"
+);
+loader_impl!(
+    WfcoCircuitLoader,
+    WithdrawFromObfuscatedCircuit,
+    "WithdrawFromContractObfuscated"
+);
+loader_impl!(
+    ExecOneTwoCircuitLoader,
+    ExecuteCircuitOneTwo,
+    "ExecuteOneTwo"
+);
+loader_impl!(
+    ExecTwoTwoCircuitLoader,
+    ExecuteCircuitTwoTwo,
+    "ExecuteTwoTwo"
+);
+loader_impl!(
+    ExecThreeTwoCircuitLoader,
+    ExecuteCircuitThreeTwo,
+    "ExecuteThreeTwo"
+);
+loader_impl!(
+    ExecFourTwoCircuitLoader,
+    ExecuteCircuitFourTwo,
+    "ExecuteFourTwo"
+);
 loader_impl!(LicenseCircuitLoader, LicenseCircuit, "LicenseCircuit");
