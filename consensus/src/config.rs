5fb969d27388a1b43cd5c12caa792eaa876dd3ff
goshawk <goshawk@dusk.network>
2023-10-23 11:38:50 +0300

    consensus: Decrease CONSENSUS_TIMEOUT_MS from 20s to 5s

--- consensus/src/config.rs
+++ consensus/src/config.rs
@@ -6,10 +6,13 @@
 
 /// Maximum number of steps Consensus runs per a single round.
 pub const CONSENSUS_MAX_STEP: u8 = 213;
+/// Maximum number of iterations Consensus runs per a single round.
+pub const CONSENSUS_MAX_ITER: u8 = CONSENSUS_MAX_STEP / 3;
+
 /// Percentage number that determines a quorum.
 pub const CONSENSUS_QUORUM_THRESHOLD: f64 = 0.67;
 /// Initial step timeout in milliseconds.
-pub const CONSENSUS_TIMEOUT_MS: u64 = 20 * 1000;
+pub const CONSENSUS_TIMEOUT_MS: u64 = 5 * 1000;
 
 /// Maximum step timeout.
 pub const CONSENSUS_MAX_TIMEOUT_MS: u64 = 60 * 1000;
@@ -21,6 +24,7 @@ pub const SECOND_REDUCTION_COMMITTEE_SIZE: usize = 64;
 
 /// Artifical delay on each selection step.
 pub const CONSENSUS_DELAY_MS: u64 = 1000;
+
 /// Default number of workers to process agreements.
 pub const ACCUMULATOR_WORKERS_AMOUNT: usize = 6;
 pub const ACCUMULATOR_QUEUE_CAP: usize = 100;
