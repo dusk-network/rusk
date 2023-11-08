fa0baec575b03858477c3901e6ce9287ec7bd3c3
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-11-08 16:30:21 +0100

    rusk: use `spend_and_execute` to handle TXs
    This also entails the removal of the "fixed fee" per transfer.

--- rusk/tests/services/gas_behavior.rs
+++ rusk/tests/services/gas_behavior.rs
@@ -11,7 +11,7 @@ use std::sync::{Arc, RwLock};
 use dusk_wallet_core::{self as wallet};
 use rand::prelude::*;
 use rand::rngs::StdRng;
-use rusk::{Result, Rusk, GAS_PER_INPUT};
+use rusk::{Result, Rusk};
 use rusk_abi::TRANSFER_CONTRACT;
 use tempfile::tempdir;
 use tracing::info;
@@ -21,11 +21,11 @@ use crate::common::state::{generator_procedure, new_state};
 use crate::common::wallet::{TestProverClient, TestStateClient, TestStore};
 
 const BLOCK_HEIGHT: u64 = 1;
-const BLOCK_GAS_LIMIT: u64 = 1_000_000_000;
+const BLOCK_GAS_LIMIT: u64 = 1_000_000_000_000;
 const INITIAL_BALANCE: u64 = 10_000_000_000;
 
-const GAS_LIMIT_0: u64 = 2_000_000;
-const GAS_LIMIT_1: u64 = 100_000_000;
+const GAS_LIMIT_0: u64 = 7_000_000;
+const GAS_LIMIT_1: u64 = 200_000_000;
 
 // Creates the Rusk initial state for the tests below
 fn initial_state<P: AsRef<Path>>(dir: P) -> Result<Rusk> {
@@ -73,14 +73,14 @@ fn make_transactions(
 
     let mut rng = StdRng::seed_from_u64(0xdead);
 
-    // The first transaction will be a `wallet.execute` to a contract that is
-    // not deployed. This will produce an error in call execution and should
-    // consume all the gas provided.
+    // The first transaction will be a `wallet.execute` to the transfer
+    // contract, querying for the root of the tree. This will be given too
+    // little gas to execute correctly and error, consuming all gas provided.
     let tx_0 = wallet
         .execute(
             &mut rng,
-            [0x42; 32].into(),
-            String::from("nonsense"),
+            TRANSFER_CONTRACT.to_bytes().into(),
+            String::from("root"),
             (),
             SENDER_INDEX_0,
             &refund_0,
@@ -89,9 +89,9 @@ fn make_transactions(
         )
         .expect("Making the transaction should succeed");
 
-    // The second transaction will also be a `wallet.execute`, but this time to
-    // the transfer contract, querying for the root of the tree. This will be
-    // tested for gas cost.
+    // The second transaction will also be a `wallet.execute` to the transfer
+    // contract, querying for the root of the tree. This will be tested for
+    // gas cost.
     let tx_1 = wallet
         .execute(
             &mut rng,
@@ -105,7 +105,7 @@ fn make_transactions(
         )
         .expect("Making the transaction should succeed");
 
-    generator_procedure(
+    let spent_transactions = generator_procedure(
         rusk,
         &[tx_0, tx_1],
         BLOCK_HEIGHT,
@@ -114,31 +114,24 @@ fn make_transactions(
     )
     .expect("generator procedure should succeed");
 
-    let final_balance_0 = wallet
-        .get_balance(SENDER_INDEX_0)
-        .expect("Getting final balance should succeed")
-        .value;
+    let mut spent_transactions = spent_transactions.into_iter();
+    let tx_0 = spent_transactions
+        .next()
+        .expect("There should be two spent transactions");
+    let tx_1 = spent_transactions
+        .next()
+        .expect("There should be two spent transactions");
 
-    let final_balance_1 = wallet
-        .get_balance(SENDER_INDEX_1)
-        .expect("Getting final balance should succeed")
-        .value;
+    assert!(tx_0.err.is_some(), "The first transaction should error");
+    assert!(tx_1.err.is_none(), "The second transaction should succeed");
 
-    // The first transaction should consume all gas given, while the second one
-    // should consume a little more due to the root query.
     assert_eq!(
-        final_balance_0,
-        initial_balance_0 - GAS_LIMIT_0,
-        "Transaction should consume all the gas"
-    );
-
-    assert!(
-        final_balance_1 < initial_balance_1 - GAS_PER_INPUT,
-        "Transaction should consume more gas than just for one input"
+        tx_0.gas_spent, GAS_LIMIT_0,
+        "Erroring transaction should consume all gas"
     );
     assert!(
-        final_balance_1 > GAS_LIMIT_1,
-        "Transaction should consume less gas than all given"
+        tx_1.gas_spent < GAS_LIMIT_1,
+        "Successful transaction should consume less than provided"
     );
 }
 
