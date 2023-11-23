be24fbea40134a046071de481cf36389e253cebc
goshawk <goshawk@dusk.network>
2023-11-23 10:52:57 +0200

    consensus: Add flags to mark  quorum_reached state

--- consensus/src/step_votes_reg.rs
+++ consensus/src/step_votes_reg.rs
@@ -24,6 +24,9 @@ struct AgreementInfo {
     /// represents candidate block hash
     hash: Option<[u8; 32]>,
     cert: Certificate,
+
+    quorum_reached_first_reduction: bool,
+    quorum_reached_sec_reduction: bool,
 }
 
 impl fmt::Display for AgreementInfo {
@@ -32,10 +35,12 @@ impl fmt::Display for AgreementInfo {
 
         write!(
             f,
-            "agreement_info: hash: {}, 1st_red: {:?}, 2nd_red: {:?}",
+            "agreement_info: hash: {}, 1st_sv: {:?}, 2nd_sv: {:?}, 1st_quorum: {}, 2nd_quorum: {}",
             to_str(&hash),
             self.cert.first_reduction,
             self.cert.second_reduction,
+            self.quorum_reached_first_reduction,
+            self.quorum_reached_sec_reduction
         )
     }
 }
@@ -47,6 +52,7 @@ impl AgreementInfo {
         hash: [u8; 32],
         sv: StepVotes,
         svt: SvType,
+        quorum_reached: bool,
     ) -> bool {
         if let Some(h) = self.hash {
             if h != hash {
@@ -57,28 +63,46 @@ impl AgreementInfo {
         }
 
         match svt {
-            SvType::FirstReduction => self.cert.first_reduction = sv,
-            SvType::SecondReduction => self.cert.second_reduction = sv,
+            SvType::FirstReduction => {
+                self.cert.first_reduction = sv;
+
+                if quorum_reached {
+                    self.quorum_reached_first_reduction = quorum_reached;
+                }
+            }
+            SvType::SecondReduction => {
+                self.cert.second_reduction = sv;
+
+                if quorum_reached {
+                    self.quorum_reached_sec_reduction = quorum_reached;
+                }
+            }
         }
 
-        debug!(event = "add_sv", iter, data = format!("{}", self));
+        debug!(
+            event = "add_sv",
+            iter,
+            data = format!("{}", self),
+            quorum_reached
+        );
+
         self.is_ready()
     }
 
-    /// Returns `true` if all fields are non-empty
+    /// Returns `true` if all fields are non-empty and quorum is reached for
+    /// both reductions
     fn is_ready(&self) -> bool {
         !self.cert.first_reduction.is_empty()
             && !self.cert.second_reduction.is_empty()
             && self.hash.is_some()
+            && self.quorum_reached_first_reduction
+            && self.quorum_reached_sec_reduction
     }
 
-    /// Returns `true` if a nil_agreement is ready
-    /// NB: Nil agreement is an agreement for NIL hash
+    /// Returns `true` if it's a anreement for NIL hash
     fn is_nil(&self) -> bool {
         if let Some(hash) = self.hash {
-            if hash == [0u8; 32] {
-                return true;
-            }
+            return hash == [0u8; 32];
         }
 
         false
@@ -112,6 +136,7 @@ impl AgreementInfoRegistry {
         hash: [u8; 32],
         sv: StepVotes,
         svt: SvType,
+        quorum_reached: bool,
     ) -> Option<Message> {
         let iter_num = u8::from_step(step);
         if iter_num as usize >= self.agreement_reg.len() {
@@ -119,7 +144,7 @@ impl AgreementInfoRegistry {
         }
 
         let r = &mut self.agreement_reg[iter_num as usize];
-        if r.add_sv(iter_num, hash, sv, svt) {
+        if r.add_sv(iter_num, hash, sv, svt, quorum_reached) {
             return Some(Self::build_agreement_msg(
                 self.ru.clone(),
                 iter_num,
