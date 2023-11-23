beb5c6a192e3ccefeaa9e9b3e764a7688f8182c3
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-11-23 15:37:02 +0100

    rusk: add criterion dependency for benchmarks

---
+++ rusk/benches/block_ingestion.rs
@@ -0,0 +1,14 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+pub fn accept_benchmark(c: &mut Criterion) {
+    c.bench_function("AST", |b| b.iter(|| {}));
+}
+
+criterion_group!(benches, accept_benchmark);
+criterion_main!(benches);
