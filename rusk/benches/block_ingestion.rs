dd15cb49ea26d44491899ef076224f40e0579533
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-11-23 23:53:45 +0100

    rusk: add AST benchmarks
    Resolves #361

--- rusk/benches/block_ingestion.rs
+++ rusk/benches/block_ingestion.rs
@@ -6,13 +6,107 @@
 
 #![feature(lazy_cell)]
 
-use criterion::{criterion_group, criterion_main, Criterion};
-
 #[path = "../tests/common/mod.rs"]
 mod common;
 
+use std::io::{BufRead, BufReader};
+use std::sync::Arc;
+use std::time;
+
+use criterion::measurement::WallTime;
+use criterion::{
+    criterion_group, criterion_main, BenchmarkGroup, BenchmarkId, Criterion,
+};
+use dusk_bls12_381_sign::{
+    PublicKey as BlsPublicKey, SecretKey as BlsSecretKey,
+};
+use node_data::ledger::Transaction;
+use phoenix_core::Transaction as PhoenixTransaction;
+use rand::prelude::StdRng;
+use rand::SeedableRng;
+use tempfile::tempdir;
+
+use common::state::new_state;
+
+fn load_txs() -> Vec<Transaction> {
+    const TXS_BYTES: &[u8] = include_bytes!("block");
+
+    let mut txs = Vec::new();
+
+    for line in BufReader::new(TXS_BYTES).lines() {
+        let line = line.unwrap();
+        let tx_bytes = hex::decode(line).unwrap();
+        let tx = PhoenixTransaction::from_slice(&tx_bytes).unwrap();
+        txs.push(Transaction {
+            version: 1,
+            r#type: 0,
+            inner: tx,
+        });
+    }
+
+    txs
+}
+
+pub fn with_group<T, F>(name: &str, c: &mut Criterion, closure: F) -> T
+where
+    F: FnOnce(&mut BenchmarkGroup<WallTime>) -> T,
+{
+    let mut group = c.benchmark_group(name);
+    // group.measurement_time(time::Duration::from_secs(200));
+    let r = closure(&mut group);
+    group.finish();
+    r
+}
+
 pub fn accept_benchmark(c: &mut Criterion) {
-    c.bench_function("AST", |b| b.iter(|| {}));
+    with_group("State Transitions", c, |group| {
+        let tmp = tempdir().expect("Creating a temp dir should work");
+        let snapshot =
+            toml::from_str(include_str!("../tests/config/bench.toml"))
+                .expect("Cannot deserialize config");
+
+        let rusk =
+            new_state(&tmp, &snapshot).expect("Creating state should work");
+        let txs = Arc::new(load_txs());
+
+        let generator = {
+            let mut rng = StdRng::seed_from_u64(0xbeef);
+            let sk = BlsSecretKey::random(&mut rng);
+            BlsPublicKey::from(&sk)
+        };
+
+        const BLOCK_HEIGHT: u64 = 1;
+        const BLOCK_GAS_LIMIT: u64 = 1_000_000_000_000;
+
+        const N_TXS: &[usize] =
+            &[1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+        for n_txs in N_TXS {
+            let rusk = rusk.clone();
+            let txs = txs.clone();
+
+            group.bench_with_input(
+                BenchmarkId::new("AST", format!("{n_txs} TXs")),
+                n_txs,
+                move |b, n_txs| {
+                    b.iter(|| {
+                        let txs = txs.as_ref()[..*n_txs].to_vec();
+
+                        rusk.accept_transactions(
+                            BLOCK_HEIGHT,
+                            BLOCK_GAS_LIMIT,
+                            generator,
+                            txs,
+                            None,
+                        )
+                        .expect("Accepting transactions should succeed");
+
+                        rusk.revert().expect("Reverting should succeed");
+                    })
+                },
+            );
+        }
+    });
 }
 
 criterion_group!(benches, accept_benchmark);
--- rusk/tests/rusk-state.rs
+++ rusk/tests/rusk-state.rs
@@ -6,23 +6,31 @@
 
 #![feature(lazy_cell)]
 pub mod common;
-use crate::common::*;
 
+use crate::common::*;
+use std::collections::HashMap;
+
+use dusk_bls12_381::BlsScalar;
 use std::path::Path;
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, RwLock};
 
 use dusk_pki::SecretSpendKey;
+use dusk_wallet_core::{self as wallet};
+use ff::Field;
 use parking_lot::MutexGuard;
 use phoenix_core::transaction::TreeLeaf;
 use phoenix_core::Note;
 use rand::prelude::*;
 use rand::rngs::StdRng;
 use rusk::{Result, Rusk, RuskInner};
+use rusk_abi::dusk::LUX;
 use rusk_abi::TRANSFER_CONTRACT;
 use tempfile::tempdir;
+use tokio::task;
 use tracing::info;
 
 use crate::common::state::new_state;
+use crate::common::wallet::{TestProverClient, TestStateClient, TestStore};
 
 const BLOCK_HEIGHT: u64 = 1;
 const INITIAL_BALANCE: u64 = 10_000_000_000;
@@ -134,6 +142,83 @@ pub fn rusk_state_finalized() -> Result<()> {
         2,
         "The new note should still be there after reversion"
     );
+
+    Ok(())
+}
+
+// #[tokio::test(flavor = "multi_thread")]
+#[allow(dead_code)]
+async fn generate_bench_txs() -> Result<(), Box<dyn std::error::Error>> {
+    // Setup the logger
+    logger();
+
+    let tmp = tempdir()?;
+    let snapshot = toml::from_str(include_str!("./config/bench.toml"))
+        .expect("Cannot deserialize config");
+
+    let rusk = new_state(&tmp, &snapshot)?;
+
+    let cache = Arc::new(RwLock::new(HashMap::new()));
+
+    let wallet = wallet::Wallet::new(
+        TestStore,
+        TestStateClient { rusk, cache },
+        TestProverClient::default(),
+    );
+
+    // Generates some public spend keys for the wallet
+    // for i in 0..100 {
+    //     let psk = wallet.public_spend_key(i).unwrap();
+    //     let psk_bytes = psk.to_bytes();
+    //     let psk_string = bs58::encode(psk_bytes).into_string();
+    //     println!("{psk_string}");
+    // }
+
+    const N_ADDRESSES: usize = 100;
+
+    const TRANSFER_VALUE: u64 = 1_000_000;
+    const GAS_LIMIT: u64 = 100_000_000;
+
+    let mut tasks = Vec::with_capacity(N_ADDRESSES);
+    let mut txs = Vec::with_capacity(N_ADDRESSES);
+
+    let wallet = Arc::new(wallet);
+
+    for sender_index in 0..N_ADDRESSES as u64 {
+        let wallet = wallet.clone();
+        let mut rng = StdRng::seed_from_u64(0xdead);
+
+        let receiver_index = (sender_index + 1) % N_ADDRESSES as u64;
+        let receiver = wallet.public_spend_key(receiver_index).unwrap();
+        let refund = wallet.public_spend_key(sender_index).unwrap();
+
+        let ref_id = BlsScalar::random(&mut rng);
+
+        tasks.push(task::spawn_blocking(move || {
+            wallet
+                .transfer(
+                    &mut rng,
+                    sender_index,
+                    &refund,
+                    &receiver,
+                    TRANSFER_VALUE,
+                    GAS_LIMIT,
+                    LUX,
+                    ref_id,
+                )
+                .expect("Making a transfer TX should succeed")
+        }));
+    }
+
+    for task in tasks {
+        txs.push(task.await.expect("Joining should succeed"));
+    }
+
+    for tx in txs {
+        let tx_bytes = tx.to_var_bytes();
+        let tx_hex = hex::encode(tx_bytes);
+        println!("{tx_hex}");
+    }
 
     Ok(())
 }
