6cc6030ddfe1e42a5765a3689abff8171a74a1de
goshawk-3 <goshawk@dusk.network>
2024-02-01 12:59:32 +0200

    node: AverageElapsedTime calculates the average value of last N duration values added

--- node/src/chain/metrics.rs
+++ node/src/chain/metrics.rs
@@ -5,61 +5,46 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use node_data::Serializable;
-use std::cmp::Ordering;
 use std::collections::VecDeque;
 use std::io;
 use std::io::{Read, Write};
+use std::time::Duration;
 
 const AVG_VALUES_NUM: usize = 5;
 
-/// Implements logic of calculating the average of last N stored values
+/// AverageElapsedTime calculates the average value of last N values added
 #[derive(Debug)]
-pub struct AverageElapsedTime(VecDeque<u16>, u64);
+pub struct AverageElapsedTime(VecDeque<Duration>);
 impl AverageElapsedTime {
-    pub fn update(&mut self, round: u64, value: u16) -> Option<u16> {
-        match round.cmp(&self.1) {
-            Ordering::Equal => {
-                if let Some(v) = self.0.back_mut() {
-                    *v = value;
-                    return Some(*v);
-                }
-            }
-            Ordering::Greater => {
-                if self.0.len() == self.0.capacity() {
-                    self.0.pop_front();
-                }
-                self.0.push_back(value);
-                self.1 = round;
-
-                return Some(value);
-            }
-            Ordering::Less => {}
+    pub fn push_back(&mut self, value: Duration) {
+        if self.0.len() == self.0.capacity() {
+            self.0.pop_front();
         }
-
-        None
+        self.0.push_back(value);
     }
 
-    pub fn average(&self) -> Option<u16> {
-        let sum: u16 = self.0.iter().sum();
-        if sum == 0 {
+    pub fn average(&self) -> Option<Duration> {
+        if self.0.is_empty() {
             return None;
         }
-        Some(sum / self.0.len() as u16)
+
+        let sum: Duration = self.0.iter().sum();
+        Some(sum / self.0.len() as u32)
     }
 }
 
 impl Default for AverageElapsedTime {
     fn default() -> Self {
-        Self(VecDeque::with_capacity(AVG_VALUES_NUM), 0)
+        Self(VecDeque::with_capacity(AVG_VALUES_NUM))
     }
 }
 
 impl Serializable for AverageElapsedTime {
     fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
-        w.write_all(&self.1.to_le_bytes())?;
-
         let mut bytes = Vec::new();
-        self.0.iter().for_each(|v| bytes.extend(v.to_le_bytes()));
+        self.0
+            .iter()
+            .for_each(|v| bytes.extend((v.as_millis() as u32).to_le_bytes()));
         w.write_all(&bytes)?;
         Ok(())
     }
@@ -68,18 +53,12 @@ impl Serializable for AverageElapsedTime {
     where
         Self: Sized,
     {
-        let round = Self::read_u64_le(r)?;
-
-        let mut buf = Vec::new();
-        _ = r.read_to_end(&mut buf)?;
         let mut vec = VecDeque::with_capacity(AVG_VALUES_NUM);
-        vec.extend(
-            (0..buf.len())
-                .step_by(2)
-                .map(|i| u16::from_le_bytes([buf[i], buf[i + 1]])),
-        );
+        while let Ok(secs) = Self::read_u32_le(r) {
+            vec.push_back(Duration::from_millis(secs as u64));
+        }
 
-        Ok(Self(vec, round))
+        Ok(Self(vec))
     }
 }
 #[cfg(test)]
@@ -88,34 +67,23 @@ mod tests {
 
     #[test]
     fn test_average() {
-        let test_cases = [
-            (1, 101, true),
-            (2, 102, true),
-            (3, 103, true),
-            (4, 104, true),
-            (5, 100, true),
-            (5, 105, true),
-            (4, 105, false),
-            (6, 106, true),
-            (7, 107, true),
-        ];
-        let avg = 525 / AVG_VALUES_NUM as u16;
-
+        let expected = Duration::from_secs(108 as u64);
         let mut metric = AverageElapsedTime::default();
-        for (round, value, exp) in test_cases.iter() {
-            assert_eq!(metric.update(*round, *value).is_some(), *exp);
+        for value in 100..111 {
+            metric.push_back(Duration::from_secs(value as u64));
         }
-        assert_eq!(metric.average().expect("positive number"), avg);
+        assert_eq!(metric.average().expect("positive number"), expected);
 
         // Marshal/Unmarshal
         let mut buf = Vec::new();
         metric.write(&mut buf).expect("all written");
+
         assert_eq!(
             AverageElapsedTime::read(&mut &buf[..])
                 .expect("all read")
                 .average()
                 .unwrap(),
-            avg
+            expected
         );
     }
 }
