f9caa1df09e6c568ee0c18c7f50054eb695c4b5c
Herr Seppia <seppia@dusk.network>
2024-02-20 10:19:08 +0100

    node: fix slashed log

--- node/src/chain/acceptor.rs
+++ node/src/chain/acceptor.rs
@@ -349,8 +349,7 @@ impl<DB: database::DB, VM: vm::VMExecution, N: Network> Acceptor<N, DB, VM> {
                 header.height,
             );
 
-            for (_, slashed) in
-                header.failed_iterations.cert_list.iter().flatten()
+            for slashed in header.failed_iterations.to_missed_generators_bytes()
             {
                 info!("Slashed {}", slashed.to_base58())
             }
