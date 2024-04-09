d968ae0c5899e6cd888456cf9bd42db1e95bb8fa
Milosz Muszynski <milosz.muszynski@gmail.com>
2024-04-09 11:41:40 +0200

    charlie-contract: initial check-in

---
+++ contracts/charlie/src/lib.rs
@@ -0,0 +1,43 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+#![no_std]
+#![feature(arbitrary_self_types)]
+
+extern crate alloc;
+
+mod state;
+use state::Charlie;
+
+#[cfg(target_family = "wasm")]
+#[path = ""]
+mod wasm {
+    use super::*;
+
+    use rusk_abi::{ContractId, PaymentInfo};
+
+    #[no_mangle]
+    static SELF_ID: ContractId = ContractId::uninitialized();
+
+    static mut STATE: Charlie = Charlie;
+
+    #[no_mangle]
+    unsafe fn ping(arg_len: u32) -> u32 {
+        rusk_abi::wrap_call(arg_len, |()| STATE.ping())
+    }
+
+    #[no_mangle]
+    unsafe fn subsidize(arg_len: u32) -> u32 {
+        rusk_abi::wrap_call(arg_len, |arg| STATE.subsidize(arg))
+    }
+
+    const PAYMENT_INFO: PaymentInfo = PaymentInfo::Any(None);
+
+    #[no_mangle]
+    fn payment_info(arg_len: u32) -> u32 {
+        rusk_abi::wrap_call(arg_len, |_: ()| PAYMENT_INFO)
+    }
+}
---
+++ contracts/charlie/src/state.rs
@@ -0,0 +1,60 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use rusk_abi::TRANSFER_CONTRACT;
+use subsidy_types::*;
+use transfer_contract_types::Stct;
+
+#[derive(Debug, Clone)]
+pub struct Charlie;
+
+impl Charlie {
+    pub fn ping(&mut self) {
+        const ALLOWANCE: u64 = 10_000_000;
+        rusk_abi::debug!(
+            "CHARLIE ping - making this call free up to {} of allowance",
+            ALLOWANCE
+        );
+        rusk_abi::set_allowance(ALLOWANCE);
+    }
+
+    /// loads the contract with funds which can be used
+    /// to sponsor free uses of some methods of this contract
+    /// technically, the funds passed in this call will be used
+    /// when granting allowances
+    /// this operation is similar to staking, but the funds
+    /// are staked into this contract's "wallet" rather than
+    /// into the stake contract's wallet
+    pub fn subsidize(&mut self, subsidy: Subsidy) {
+        // verify the signature is over the correct digest
+        // note: counter is always zero - make sure that this is safe
+        let digest = subsidy_signature_message(0, subsidy.value).to_vec();
+
+        if !rusk_abi::verify_bls(digest, subsidy.public_key, subsidy.signature)
+        {
+            panic!("Invalid signature!");
+        }
+
+        // make call to transfer contract to transfer balance from the user to
+        // this contract
+        let transfer_module = TRANSFER_CONTRACT;
+
+        let stct = Stct {
+            module: rusk_abi::self_id().to_bytes(),
+            value: subsidy.value,
+            proof: subsidy.proof,
+        };
+
+        rusk_abi::debug!(
+            "CHARLIE subsidize - subsidized self ('{:X?}') with value {}",
+            stct.module[0],
+            stct.value
+        );
+
+        rusk_abi::call::<_, bool>(transfer_module, "stct", &stct)
+            .expect("Sending note to contract should succeed");
+    }
+}
