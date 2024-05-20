d619323208dc85383fdade665b95f23f8df7232a
Milosz Muszynski <milosz.muszynski@gmail.com>
2024-05-20 12:05:37 +0200

    node-data: added field in spent transaction

--- node-data/src/ledger.rs
+++ node-data/src/ledger.rs
@@ -15,6 +15,7 @@ use std::io::{self, Read, Write};
 
 #[cfg(any(feature = "faker", test))]
 use fake::{Dummy, Fake, Faker};
+use rusk_abi::EconomicMode;
 
 pub type Seed = Signature;
 pub type Hash = [u8; 32];
@@ -94,6 +95,7 @@ pub struct SpentTransaction {
     pub inner: Transaction,
     pub block_height: u64,
     pub gas_spent: u64,
+    pub economic_mode: EconomicMode,
     pub err: Option<String>,
 }
 
@@ -443,6 +445,7 @@ pub mod faker {
                 inner: tx,
                 block_height: 0,
                 gas_spent: 3,
+                economic_mode: EconomicMode::None,
                 err: Some("error".to_string()),
             }
         }
