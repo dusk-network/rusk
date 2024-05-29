b294ab8c87453abf224502aa229fa086c41b0d9d
Alex Panturu <panturu.alexandru@gmail.com>
2024-05-29 17:19:30 +0300

    explorer: add visible version commit hash and build date

--- explorer/src/lib/components/footer/Footer.svelte
+++ explorer/src/lib/components/footer/Footer.svelte
@@ -7,30 +7,38 @@
 </script>
 
 <div class="footer">
-  <div class="footer__copyright">
-    <span>© 2018 - 2024 Dusk Network B.V. All Rights Reserved.</span>
+  <div class="footer__content">
+    <div class="footer__copyright">
+      <span>© 2018 - 2024 Dusk Network B.V. All Rights Reserved.</span>
+    </div>
+    <div class="footer__links">
+      <Anchor
+        onSurface={false}
+        href="https://dusk.network/privacy-policy"
+        className="footer__links-link">Privacy policy</Anchor
+      >
+      <Anchor
+        onSurface={false}
+        href="https://dusk.network/terms-of-use"
+        className="footer__links-link">Terms of use</Anchor
+      >
+    </div>
+    <AppAnchor href="https://dusk.network" className="footer__logo">
+      <picture>
+        <AppSource media="(max-width:768px)" srcset="/dusk_logo_icon.svg" />
+        <AppImage
+          src="/dusk_logo.svg"
+          alt="Dusk Logo"
+          sizes="(max-width:768px) 20px, 86px"
+          className="footer__logo-image"
+        />
+      </picture>
+    </AppAnchor>
   </div>
-  <div class="footer__links">
-    <Anchor
-      onSurface={false}
-      href="https://dusk.network/privacy-policy"
-      className="footer__links-link">Privacy policy</Anchor
-    >
-    <Anchor
-      onSurface={false}
-      href="https://dusk.network/terms-of-use"
-      className="footer__links-link">Terms of use</Anchor
+  <div class="footer__build-info">
+    <span
+      >Explorer v{import.meta.env.APP_VERSION} ({import.meta.env
+        .APP_BUILD_INFO})</span
     >
   </div>
-  <AppAnchor href="https://dusk.network" className="footer__logo">
-    <picture>
-      <AppSource media="(max-width:768px)" srcset="/dusk_logo_icon.svg" />
-      <AppImage
-        src="/dusk_logo.svg"
-        alt="Dusk Logo"
-        sizes="(max-width:768px) 20px, 86px"
-        className="footer__logo-image"
-      />
-    </picture>
-  </AppAnchor>
 </div>
