be62e38c18b6345d978db41162799b963e561a75
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2024-05-30 15:09:08 +0200

    explorer: Replace REST API call with GraphQL for search operations
    Resolves #1815

--- explorer/src/lib/mock-data/index.js
+++ explorer/src/lib/mock-data/index.js
@@ -1,12 +1,10 @@
 export { default as apiMarketData } from "./api-market-data.json";
 export { default as apiNodeLocations } from "./api-node-locations.json";
-export { default as apiSearchBlockResult } from "./api-search-block-result.json";
-export { default as apiSearchNoResult } from "./api-search-no-result.json";
-export { default as apiSearchTransactionResult } from "./api-search-transaction-result.json";
 export { default as apiStats } from "./api-stats.json";
 export { default as gqlBlock } from "./gql-block.json";
 export { default as gqlBlocks } from "./gql-blocks.json";
 export { default as gqlLatestChainInfo } from "./gql-latest-chain-info.json";
+export { default as gqlSearchPossibleResults } from "./gql-search-possible-results.json";
 export { default as gqlTransaction } from "./gql-transaction.json";
 export { default as gqlTransactionDetails } from "./gql-transaction-details.json";
 export { default as gqlTransactions } from "./gql-transactions.json";
