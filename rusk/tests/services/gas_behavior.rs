5e955fedc6b6c1803a970dfaf215008eaa05df30
Eduardo Leegwater Simões <eduardo@dusk.network>
2024-07-01 15:40:37 +0200

    rusk: remove economic protocol features

--- rusk/tests/services/gas_behavior.rs
+++ rusk/tests/services/gas_behavior.rs
@@ -25,8 +25,8 @@ const BLOCK_HEIGHT: u64 = 1;
 const BLOCK_GAS_LIMIT: u64 = 1_000_000_000_000;
 const INITIAL_BALANCE: u64 = 10_000_000_000;
 
-const GAS_LIMIT_0: u64 = 7_000_000;
-const GAS_LIMIT_1: u64 = 200_000_000;
+const GAS_LIMIT_0: u64 = 100_000_000;
+const GAS_LIMIT_1: u64 = 300_000_000;
 
 // Creates the Rusk initial state for the tests below
 fn initial_state<P: AsRef<Path>>(dir: P) -> Result<Rusk> {
