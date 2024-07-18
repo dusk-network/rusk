6af43989b67aaeb2cae4e31ff8479ec95a0da9d1
Milosz Muszynski <milosz.muszynski@gmail.com>
2024-07-18 11:25:06 +0200

    test-wallet: review follow-up renames

--- test-wallet/src/imp.rs
+++ test-wallet/src/imp.rs
@@ -343,7 +343,7 @@ where
     pub fn execute<Rng>(
         &self,
         rng: &mut Rng,
-        call_or_deploy: ContractExec,
+        exec: ContractExec,
         sender_index: u64,
         gas_limit: u64,
         gas_price: u64,
@@ -378,7 +378,7 @@ where
             outputs,
             fee,
             0,
-            Some(call_or_deploy),
+            Some(exec),
         )
         .map_err(Error::from_state_err)?;
 
@@ -740,7 +740,7 @@ fn new_unproven_tx<Rng: RngCore + CryptoRng, SC: StateClient>(
     outputs: [(Note, u64, JubJubScalar, [JubJubScalar; 2]); OUTPUT_NOTES],
     fee: Fee,
     deposit: u64,
-    call_or_deploy: Option<ContractExec>,
+    exec: Option<ContractExec>,
 ) -> Result<UnprovenTransaction, SC::Error> {
     let nullifiers: Vec<BlsScalar> = inputs
         .iter()
@@ -766,7 +766,7 @@ fn new_unproven_tx<Rng: RngCore + CryptoRng, SC: StateClient>(
     let payload = Payload {
         tx_skeleton,
         fee,
-        call_or_deploy,
+        exec,
     };
     let payload_hash = payload.hash();
 
