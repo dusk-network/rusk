3dc62f1dd568c018c82e335318f00c07408f7b47
Milosz Muszynski <milosz.muszynski@gmail.com>
2024-07-18 14:22:55 +0200

    rusk-prover: renamed exec to contract_exec

--- rusk-prover/src/tx.rs
+++ rusk-prover/src/tx.rs
@@ -396,7 +396,7 @@ mod tests {
         let payload = Payload {
             tx_skeleton,
             fee,
-            exec: Some(ContractExec::Call(call)),
+            contract_exec: Some(ContractExec::Call(call)),
         };
 
         let sender_blinder_1 =
