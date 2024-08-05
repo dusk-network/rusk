f459f8d92029086cb0725e65a3c8f0f892cd12ca
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2024-08-05 14:36:40 +0200

    explorer: progress bar tests miss the `ariaLabel` required prop
    Resolves #2060

--- explorer/src/lib/dusk/components/__tests__/ProgressBar.spec.js
+++ explorer/src/lib/dusk/components/__tests__/ProgressBar.spec.js
@@ -4,30 +4,47 @@ import { cleanup, render } from "@testing-library/svelte";
 import { ProgressBar } from "..";
 
 describe("ProgressBar", () => {
+  const baseProps = {
+    ariaLabel: "Loading",
+    currentPercentage: 0,
+  };
+  const baseOptions = {
+    props: baseProps,
+    target: document.body,
+  };
+
   afterEach(cleanup);
 
-  it("renders the ProgressBar component with no current percentage set", () => {
-    const { container } = render(ProgressBar);
-
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders the Stepper component with current percentage set as zero", () => {
+  it("renders the `ProgressBar` component with no current percentage set", () => {
     const { container } = render(ProgressBar, {
-      props: { ariaLabel: "Loading", currentPercentage: 0 },
+      ...baseOptions,
+      props: { ariaLabel: baseProps.ariaLabel },
     });
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it("re-renders the Stepper component when the current percentage property changes", async () => {
-    const { container, rerender } = render(ProgressBar, {
-      props: { ariaLabel: "Loading", currentPercentage: 0 },
-    });
+  it("renders the `ProgressBar` component with current percentage set as zero", () => {
+    const { container } = render(ProgressBar, baseOptions);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("re-renders the `ProgressBar` component when the current percentage property changes", async () => {
+    const { container, rerender } = render(ProgressBar, baseOptions);
 
     expect(container.firstChild).toMatchSnapshot();
 
     await rerender({ currentPercentage: 50 });
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("should pass additional class names to the rendered element", () => {
+    const { container } = render(ProgressBar, {
+      ...baseOptions,
+      props: { ...baseProps, className: "foo bar" },
+    });
 
     expect(container.firstChild).toMatchSnapshot();
   });
