5e0d054c2844214629ab4977c2aa17fb8a94d36d
Kieran Hall <kieran.hall@gmail.com>
2024-08-11 23:04:26 +0200

    Add DashboardMenu component
    Resolves #2074

---
+++ web-wallet/src/routes/components-showcase/DashboardNavs.svelte
@@ -0,0 +1,41 @@
+<svelte:options immutable={true} />
+
+<script>
+  import { mdiPlusBoxOutline, mdiSwapHorizontal, mdiTimerSand } from "@mdi/js";
+  import { DashboardNav } from "$lib/components";
+
+  const items = [
+    {
+      href: "#",
+      icons: [{ path: mdiSwapHorizontal }],
+      id: "item-1",
+      label: "Something",
+    },
+    {
+      href: "#",
+      icons: [{ path: mdiPlusBoxOutline }],
+      id: "item-2",
+      label: "Send",
+    },
+    {
+      href: "#",
+      icons: [{ path: mdiTimerSand }],
+      id: "item-3",
+      label: "Receive",
+    },
+    {
+      href: "#",
+      icons: [
+        { path: mdiTimerSand },
+        { path: mdiSwapHorizontal },
+        { path: mdiPlusBoxOutline },
+      ],
+      id: "item-4",
+      label: "Stake",
+    },
+  ];
+</script>
+
+<section>
+  <DashboardNav {items} />
+</section>
