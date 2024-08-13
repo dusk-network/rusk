4a05f6614fc64f19f4c3983a68a2d8922632ee73
moana <moana@dusk.network>
2024-08-13 16:47:59 +0200

    wallet-core: Add function to sum the balance for phoenix notes
    Resolves #2117

--- wallet-core/tests/keys.rs
+++ wallet-core/tests/keys.rs
@@ -5,6 +5,7 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use dusk_bytes::Serializable;
+
 use wallet_core::keys::{
     derive_bls_sk, derive_phoenix_pk, derive_phoenix_sk, derive_phoenix_vk,
 };
