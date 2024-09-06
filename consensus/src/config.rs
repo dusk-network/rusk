f1d7d47eda218b475ecdd9e6ee053073a153121a
Federico Franzoni <8609060+fed-franz@users.noreply.github.com>
2024-09-06 12:20:35 +0200

    consensus: reduce max number of transactions

--- consensus/src/config.rs
+++ consensus/src/config.rs
@@ -15,7 +15,7 @@ pub const VALIDATION_COMMITTEE_CREDITS: usize = 64;
 pub const RATIFICATION_COMMITTEE_CREDITS: usize = 64;
 
 pub const RELAX_ITERATION_THRESHOLD: u8 = 8;
-pub const MAX_NUMBER_OF_TRANSACTIONS: usize = 30_000;
+pub const MAX_NUMBER_OF_TRANSACTIONS: usize = 1_000;
 
 /// Emergency mode is enabled after 16 iterations
 pub const EMERGENCY_MODE_ITERATION_THRESHOLD: u8 = 16;
