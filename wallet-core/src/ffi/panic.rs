be569d7f48c82f9a9d6c0871b23599ab99112df3
zer0 <matteo@dusk.network>
2024-09-09 12:19:32 +0200

    wallet-core: add debugging capabilities
    - Add `dbg!` and `eprintln!` macros implementation for WASM host
    - Add panic handling for debug builds
    
    Resolves #2315

---
+++ wallet-core/src/ffi/panic.rs
@@ -0,0 +1,18 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+mod panic_handling {
+    use core::panic::PanicInfo;
+
+    #[panic_handler]
+    #[allow(unused)]
+    fn panic(info: &PanicInfo) -> ! {
+        #[cfg(debug_assertions)]
+        eprintln!("{}", info);
+
+        loop {}
+    }
+}
