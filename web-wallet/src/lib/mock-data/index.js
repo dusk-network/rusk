e29b9a82d8792c660da8a0f49f8c1255113e12e1
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2024-09-12 11:59:20 +0200

    web-wallet: Switch to CoinGecko API for market data
    Resolves #2307

--- web-wallet/src/lib/mock-data/index.js
+++ web-wallet/src/lib/mock-data/index.js
@@ -1,3 +1,4 @@
 export { default as addresses } from "./addresses";
+export { default as apiMarketData } from "./api-market-data.json";
 export { default as stakeInfo } from "./stakeInfo";
 export { default as transactions } from "./transactions";
