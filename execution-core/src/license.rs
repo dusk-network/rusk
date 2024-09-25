9c722c5a46ce32bc2ba98b2595363ceffa31bdf1
moana <moana@dusk.network>
2024-09-25 17:58:42 +0200

    execution-core: Add merkle tree types for license contract

--- execution-core/src/license.rs
+++ execution-core/src/license.rs
@@ -10,3 +10,16 @@ use crate::{reserved, ContractId};
 
 /// ID of the genesis license contract
 pub const LICENSE_CONTRACT: ContractId = reserved(0x3);
+
+/// The depth of the merkle tree of licenses stored in the license-contract.
+pub const LICENSE_TREE_DEPTH: usize = 17;
+/// The arity of the merkle tree of licenses stored in the
+/// license-contract.
+pub use poseidon_merkle::ARITY as LICENSE_TREE_ARITY;
+/// The merkle tree of licenses stored in the license-contract.
+pub type LicenseTree = poseidon_merkle::Tree<(), LICENSE_TREE_DEPTH>;
+/// The merkle opening for a license-hash in the merkle tree of licenses.
+pub type LicenseOpening = poseidon_merkle::Opening<(), LICENSE_TREE_DEPTH>;
+/// the tree item for the merkle-tree of licenses stored in the
+/// license-contract.
+pub type LicenseTreeItem = poseidon_merkle::Item<()>;
