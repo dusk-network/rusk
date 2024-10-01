aa2abd4b493ae3bbff4d6ad055569b29bdf9542d
Herr Seppia <seppia@dusk.network>
2024-10-01 14:49:17 +0200

    rusk: change default genesis timestamp to `UNIX_TIME`

--- rusk/src/bin/config/chain.rs
+++ rusk/src/bin/config/chain.rs
@@ -14,9 +14,6 @@ use serde::{Deserialize, Serialize};
 
 pub const DEFAULT_BLOCK_GAS_LIMIT: u64 = 5 * 1_000_000_000;
 
-// Tue Sep 10 2024 20:00:00 GMT+0000
-pub const DEFAULT_GENESIS_TIMESTAMP: u64 = 1725998400;
-
 use crate::args::Args;
 
 #[derive(Serialize, Deserialize, Clone, Default)]
@@ -109,6 +106,6 @@ impl ChainConfig {
                     .map(|n| n.as_secs())
                     .expect("This is heavy.")
             })
-            .unwrap_or(DEFAULT_GENESIS_TIMESTAMP)
+            .unwrap_or_default()
     }
 }
