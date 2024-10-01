6607cfe0feff2828bd894feacde0a5e220079734
Kieran Hall <kieran.hall@gmail.com>
2024-10-01 19:47:12 +0200

    Add maintenance layout

---
+++ web-wallet/src/routes/maintenance/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { AppImage } from "$lib/components";
+  import { settingsStore } from "$lib/stores";
+
+  const { darkMode } = $settingsStore;
+</script>
+
+<header>
+  <AppImage
+    src={darkMode ? "/dusk_logo_light.svg" : "/dusk_logo.svg"}
+    alt="Dusk Logo"
+    width="129"
+    height="31"
+  />
+</header>
+
+<slot />
