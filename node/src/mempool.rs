ad3673783e59204ddc498a110821514212a6a0e5
Herr Seppia <seppia@dusk.network>
2024-10-03 10:56:33 +0200

    node: add check for deployment's fee in the mempool

--- node/src/mempool.rs
+++ node/src/mempool.rs
@@ -37,6 +37,8 @@ pub enum TxAcceptanceError {
     VerificationFailed(String),
     #[error("gas price lower than minimum {0}")]
     GasPriceTooLow(u64),
+    #[error("gas limit lower than minimum {0}")]
+    GasLimitTooLow(u64),
     #[error("Maximum count of transactions exceeded {0}")]
     MaxTxnCountExceeded(usize),
     #[error("A generic error occurred {0}")]
@@ -195,6 +197,23 @@ impl MempoolSrv {
 
         if tx.gas_price() < 1 {
             return Err(TxAcceptanceError::GasPriceTooLow(1));
+        }
+
+        if let Some(deploy) = tx.inner.deploy() {
+            let vm = vm.read().await;
+            let min_deployment_gas_price = vm.min_deployment_gas_price();
+            if tx.gas_price() < min_deployment_gas_price {
+                return Err(TxAcceptanceError::GasPriceTooLow(
+                    min_deployment_gas_price,
+                ));
+            }
+
+            let gas_per_deploy_byte = vm.gas_per_deploy_byte();
+            let min_gas_limit =
+                vm::bytecode_charge(&deploy.bytecode, gas_per_deploy_byte);
+            if tx.inner.gas_limit() < min_gas_limit {
+                return Err(TxAcceptanceError::GasLimitTooLow(min_gas_limit));
+            }
         }
 
         // Perform basic checks on the transaction
