b7bd81014c4185dc0874591403f4ee542a1741f3
Alex Panturu <panturu.alexandru@gmail.com>
2024-10-29 13:02:15 +0200

    web-wallet: resolve comments

--- web-wallet/src/lib/components/BigIntInput/BigIntInput.svelte
+++ web-wallet/src/lib/components/BigIntInput/BigIntInput.svelte
@@ -3,6 +3,8 @@
 <script>
   import { createEventDispatcher } from "svelte";
   import { Textbox } from "$lib/dusk/components";
+  import { makeClassName } from "$lib/dusk/string";
+  import "./BigIntInput.css";
 
   /** @type {string | undefined} */
   export let className = undefined;
@@ -10,22 +12,43 @@
   const dispatch = createEventDispatcher();
 
   /** @type {bigint} */
+  export let minValue = 0n;
+
+  /** @type {bigint} */
+  export let maxValue = 9007199254740991n;
+
+  /** @type {bigint} */
   export let value = 0n;
 
+  let isInvalidInput = false;
   let internalValue = value.toString();
+
+  const checkValidity = () => {
+    isInvalidInput = !!(minValue > value || value > maxValue);
+    if (isInvalidInput) {
+      dispatch("error", "Value exceeds limits");
+    }
+  };
 
   function validateInput() {
     try {
       value = BigInt(internalValue);
-      dispatch("update", value);
-    } catch {
+      checkValidity();
+      dispatch("change", value);
+    } catch (error) {
       internalValue = value.toString();
     }
   }
+
+  $: inputClass = makeClassName({
+    "invalid-input": isInvalidInput,
+    [`${className}`]: true,
+  });
 </script>
 
 <Textbox
-  {className}
+  className={inputClass}
+  inputmode="numeric"
   type="text"
   bind:value={internalValue}
   on:input={validateInput}
--- web-wallet/src/lib/components/__tests__/BigIntInput.spec.js
+++ web-wallet/src/lib/components/__tests__/BigIntInput.spec.js
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, it } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
 import { cleanup, fireEvent, render } from "@testing-library/svelte";
 import { BigIntInput } from "..";
 
@@ -12,14 +12,14 @@ describe("BigIntInput", () => {
     expect(input).toBeInTheDocument();
   });
 
-  it("updates value on valid input", async () => {
+  it("changes the value on valid input", async () => {
     const { getByDisplayValue, component } = render(BigIntInput, {
       value: 123n,
     });
     const input = getByDisplayValue("123");
 
     let updatedValue;
-    component.$on("update", (event) => {
+    component.$on("change", (event) => {
       updatedValue = event.detail;
     });
 
@@ -36,5 +36,29 @@ describe("BigIntInput", () => {
     await fireEvent.input(input, { target: { value: "a" } });
 
     expect(input).toHaveValue("123");
+  });
+
+  it("emits an error event when the input is a valid number that can be converted to a BigInt but exceed the limits", async () => {
+    const errorHandler = vi.fn();
+    const { getByDisplayValue, component } = render(BigIntInput, {
+      maxValue: 10n,
+      minValue: 1n,
+      value: 2n,
+    });
+    const input = getByDisplayValue("2");
+
+    component.$on("error", errorHandler);
+
+    await fireEvent.input(input, { target: { value: "11" } });
+
+    expect(input).toHaveValue("11");
+    expect(errorHandler).toHaveBeenCalledTimes(1);
+    expect(errorHandler).toHaveBeenCalledWith(expect.any(Event));
+
+    await fireEvent.input(input, { target: { value: "0" } });
+
+    expect(input).toHaveValue("0");
+    expect(errorHandler).toHaveBeenCalledTimes(2);
+    expect(errorHandler).toHaveBeenCalledWith(expect.any(Event));
   });
 });
