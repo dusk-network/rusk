c688a7744d8f4daf48914bc2432d49a50318d59d
Herr Seppia <seppia@dusk.network>
2024-11-04 13:16:28 +0100

    rusk: adapt to new ledger nomenclature

--- rusk/src/lib/http/chain/graphql/tx.rs
+++ rusk/src/lib/http/chain/graphql/tx.rs
@@ -14,7 +14,7 @@ pub async fn tx_by_hash(
 ) -> OptResult<SpentTransaction> {
     let (db, _) = ctx.data::<DBContext>()?;
     let hash = hex::decode(hash)?;
-    let tx = db.read().await.view(|t| t.get_ledger_tx_by_hash(&hash))?;
+    let tx = db.read().await.view(|t| t.ledger_tx(&hash))?;
     Ok(tx.map(SpentTransaction))
 }
 
@@ -31,23 +31,22 @@ pub async fn last_transactions(
         let mut txs = vec![];
         let mut current_block =
             t.op_read(MD_HASH_KEY).and_then(|res| match res {
-                Some(hash) => t.fetch_light_block(&hash),
+                Some(hash) => t.light_block(&hash),
                 None => Ok(None),
             })?;
 
         while let Some(h) = current_block {
             for txs_id in h.transactions_ids {
-                let tx =
-                    t.get_ledger_tx_by_hash(&txs_id)?.ok_or_else(|| {
-                        FieldError::new("Cannot find transaction")
-                    })?;
+                let tx = t.ledger_tx(&txs_id)?.ok_or_else(|| {
+                    FieldError::new("Cannot find transaction")
+                })?;
 
                 txs.push(SpentTransaction(tx));
                 if txs.len() >= count {
                     return Ok::<_, async_graphql::Error>(txs);
                 }
             }
-            current_block = t.fetch_light_block(&h.header.prev_block_hash)?;
+            current_block = t.light_block(&h.header.prev_block_hash)?;
         }
 
         Ok::<_, async_graphql::Error>(txs)
