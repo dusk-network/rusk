e5a00d03deb043e442e2645a57143cf30db646cb
Federico Franzoni <8609060+fed-franz@users.noreply.github.com>
2024-12-05 10:27:37 +0100

    node: refactor execute_checks

--- node/src/chain/acceptor.rs
+++ node/src/chain/acceptor.rs
@@ -1344,6 +1344,6 @@ pub(crate) async fn verify_block_header<DB: database::DB>(
         header.height,
     );
     validator
-        .execute_checks(header, &expected_generator, false)
+        .execute_checks(header, &expected_generator, true)
         .await
 }
