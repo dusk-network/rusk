9e2e0ecbd7fa3c92e69b0084d21568ee241eeb1c
moana <moana@dusk.network>
2024-12-13 16:08:18 +0100

    rusk: Move transaction execution function to rusk-abi

--- rusk/tests/services/contract_deployment.rs
+++ rusk/tests/services/contract_deployment.rs
@@ -14,9 +14,8 @@ use execution_core::{
 };
 use rand::prelude::*;
 use rand::rngs::StdRng;
-use rusk::gen_id::gen_contract_id;
 use rusk::{Result, Rusk};
-use rusk_abi::{ContractData, PiecrustError};
+use rusk_abi::{gen_contract_id, ContractData, PiecrustError};
 use rusk_recovery_tools::state;
 use tempfile::tempdir;
 use test_wallet::{self as wallet, Wallet};
