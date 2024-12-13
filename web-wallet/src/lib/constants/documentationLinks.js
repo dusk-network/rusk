6c8817567d7815989edb0aeadfc1fd770b0c3f2d
Hein Dauven <heindauven@gmail.com>
2024-12-13 12:00:22 +0100

    web-wallet: Update docs URLs

--- web-wallet/src/lib/constants/documentationLinks.js
+++ web-wallet/src/lib/constants/documentationLinks.js
@@ -1,5 +1,5 @@
 export const DOCUMENTATION_LINKS = {
   RESTORE_BLOCK_HEIGHT: "https://docs.dusk.network/learn/guides/restore-height",
-  RUN_A_PROVISIONER: "https://docs.dusk.network/operator/02-provisioner/",
-  SLASHING: "https://docs.dusk.network/operator/02-provisioner/#slashing",
+  RUN_A_PROVISIONER: "https://docs.dusk.network/operator/provisioner/",
+  SLASHING: "https://docs.dusk.network/operator/provisioner/#slashing",
 };
