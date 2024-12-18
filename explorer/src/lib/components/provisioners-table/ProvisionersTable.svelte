a24654799e387ec7637f6c6d93761a026f2959b3
Norton Andreev <norton@nortonandreev.com>
2024-12-18 15:05:49 +0200

    explorer: Update Stake Info (Provisioners page)
    Resolves #3218

--- explorer/src/lib/components/provisioners-table/ProvisionersTable.svelte
+++ explorer/src/lib/components/provisioners-table/ProvisionersTable.svelte
@@ -31,7 +31,7 @@
   <TableHead>
     <TableRow>
       <TableCell type="th">Staking Address</TableCell>
-      <TableCell type="th">Stake Amount (DUSK)</TableCell>
+      <TableCell type="th">Stake</TableCell>
       <TableCell type="th">Slashes</TableCell>
       <TableCell type="th">Accumulated Reward (DUSK)</TableCell>
     </TableRow>
@@ -43,19 +43,20 @@
           {middleEllipsis(provisioner.key, HASH_CHARS_LENGTH)}
         </TableCell>
         <TableCell>
-          <b class="provisioners-table__staked-amount-type-label"
-            >Reclaimable:</b
-          >
+          <b class="provisioners-table__stake-data-label">Active:</b>
           {numberFormatter(luxToDusk(provisioner.amount))}
           <br />
-          <b class="provisioners-table__staked-amount-type-label">Locked:</b>
+          <b class="provisioners-table__stake-data-label">Inactive:</b>
           {numberFormatter(luxToDusk(provisioner.locked_amt))}
+          <br />
+          <b class="provisioners-table__stake-data-label">Maturity At: </b>
+          #{numberFormatter(provisioner.eligibility)}
         </TableCell>
         <TableCell>
-          <b class="provisioners-table__slash-type-label">Soft:</b>
+          <b class="provisioners-table__slash-data-label">Soft:</b>
           {numberFormatter(provisioner.faults)}
           <br />
-          <b class="provisioners-table__slash-type-label">Hard:</b>
+          <b class="provisioners-table__slash-data-label">Hard:</b>
           {numberFormatter(provisioner.hard_faults)}
         </TableCell>
         <TableCell>{numberFormatter(luxToDusk(provisioner.reward))}</TableCell>
