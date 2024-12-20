cb788b7a970592e3f7b5d5814f09cb002e79935a
moana <moana@dusk.network>
2024-12-20 16:33:25 +0100

    rusk-recovery: Add Dusk's `VM`

--- rusk-recovery/src/state.rs
+++ rusk-recovery/src/state.rs
@@ -15,7 +15,7 @@ use dusk_core::stake::{StakeAmount, StakeData, StakeKeys, STAKE_CONTRACT};
 use dusk_core::transfer::phoenix::{Note, PublicKey, Sender};
 use dusk_core::transfer::TRANSFER_CONTRACT;
 use dusk_core::JubJubScalar;
-use dusk_vm::{ContractData, Session, VM};
+use dusk_vm::{new_genesis_session, new_session, ContractData, Session, VM};
 use ff::Field;
 use once_cell::sync::Lazy;
 use rand::rngs::StdRng;
@@ -176,8 +176,8 @@ fn generate_empty_state<P: AsRef<Path>>(
 
     let state_dir = state_dir.as_ref();
 
-    let vm = dusk_vm::new_vm(state_dir)?;
-    let mut session = dusk_vm::new_genesis_session(&vm, GENESIS_CHAIN_ID);
+    let vm = VM::new(state_dir)?;
+    let mut session = new_genesis_session(&vm, GENESIS_CHAIN_ID);
 
     let transfer_code = include_bytes!(
         "../../target/dusk/wasm64-unknown-unknown/release/transfer_contract.wasm"
@@ -260,7 +260,7 @@ where
         None => generate_empty_state(state_dir, snapshot),
     }?;
 
-    let mut session = dusk_vm::new_session(
+    let mut session = new_session(
         &vm,
         old_commit_id,
         GENESIS_CHAIN_ID,
@@ -312,7 +312,7 @@ pub fn restore_state<P: AsRef<Path>>(
     let mut commit_id = [0u8; 32];
     commit_id.copy_from_slice(&commit_id_bytes);
 
-    let vm = dusk_vm::new_vm(state_dir)?;
+    let vm = VM::new(state_dir)?;
     Ok((vm, commit_id))
 }
 
