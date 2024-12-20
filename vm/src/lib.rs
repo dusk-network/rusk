baa66e4a1b5e8e2c401eac3adefc0ccd7aea2611
moana <moana@dusk.network>
2024-12-20 13:01:54 +0100

    vm: Rename `rusk-abi` to `dusk-vm`

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -4,13 +4,7 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-//! ![Build Status](https://github.com/dusk-network/rusk/workflows/Continuous%20integration/badge.svg)
-//! [![Repository](https://img.shields.io/badge/github-rusk-blueviolet?logo=github)](https://github.com/dusk-network/rusk)
-//! [![Documentation](https://img.shields.io/badge/docs-rusk--abi-blue?logo=rust)](https://docs.rs/rusk-abi/)
-
-//! # Rusk ABI
-//!
-//! The ABI to develop Dusk Network smart contracts
+//![doc = include_str!("../README.md")]
 
 #![deny(missing_docs)]
 #![deny(clippy::all)]
@@ -32,8 +26,8 @@ pub use piecrust::{
 
 #[cfg(test)]
 mod tests {
-    // rust doesn't allow for optional dev-dependencies so we need to add this
-    // work-around to satisfy the `unused_crate_dependencies` lint
+    // the `unused_crate_dependencies` lint complains for dev-dependencies that
+    // are only used in integration tests, so adding this work-around here
     use ff as _;
     use once_cell as _;
     use rand as _;
