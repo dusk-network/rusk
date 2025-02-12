bba26d836eb2814c95f4b23c6e034a71b493b25d
Demilade Sonuga <demilade@dusk.network>
2025-02-12 12:48:17 +0100

    docker: fix incorrect node setup in persistent dockerfile

--- scripts/persistent-docker-setup/setup.sh
+++ scripts/persistent-docker-setup/setup.sh
@@ -18,6 +18,19 @@ detect_ips_output=$(./detect_ips.sh)
 PUBLIC_IP=$(echo "$detect_ips_output" | sed -n '1p')
 LISTEN_IP=$(echo "$detect_ips_output" | sed -n '2p')
 
+SELECTED_NETWORK=""
+case "$NETWORK" in
+    mainnet)
+        SELECTED_NETWORK="mainnet"
+        ;;
+    testnet)
+        SELECTED_NETWORK="testnet"
+        ;;
+    *)
+        echo "Unknown network $NETWORK. Defaulting to mainnet"
+        SELECTED_NETWORK="mainnet"
+esac
+
 toml_set() {
     file=$1
     property=$2
@@ -26,53 +39,11 @@ toml_set() {
     echo -e "$(toml-cli set $file $property $value)" > $file
 }
 
+# Configure your local installation based on the selected network
 configure_network() {
-    local network=$1
-    local kadcast_id
-    local bootstrapping_nodes
-    local genesis_timestamp
-    local base_state
-    local prover_url
-
-    case "$network" in
-        mainnet)
-            kadcast_id="0x1"
-            bootstrapping_nodes="['165.232.91.113:9000', '64.226.105.70:9000', '137.184.232.115:9000']"
-            genesis_timestamp="'2025-01-07T12:00:00Z'"
-            base_state="https://nodes.dusk.network/genesis-state"
-            prover_url="https://provers.dusk.network"
-            ;;
-        testnet)
-            kadcast_id="0x2"
-            bootstrapping_nodes="['134.122.62.88:9000','165.232.64.16:9000','137.184.118.43:9000']"
-            genesis_timestamp="'2024-12-23T17:00:00Z'"
-            base_state="https://testnet.nodes.dusk.network/genesis-state"
-            prover_url="https://testnet.provers.dusk.network"
-            ;;
-        devnet)
-            kadcast_id="0x3"
-            bootstrapping_nodes="['128.199.32.54', '159.223.29.22', '143.198.225.158']"
-            genesis_timestamp="'2024-12-23T12:00:00Z'"
-            base_state="https://devnet.nodes.dusk.network/genesis-state"
-            prover_url="https://devnet.provers.dusk.network"
-            ;;
-        *)
-            echo "Unknown network: $network. Defaulting to mainnet."
-            configure_network "mainnet"
-            return
-            ;;
-    esac
-
     echo "Generating configuration"
 
-    cat > "$RUSK_CONFIG_DIR/genesis.toml" <<EOF
-base_state = "$base_state"
-EOF
-
     cp "$RUSK_TEMPLATE_CONFIG_PATH" "$RUSK_CONFIG_PATH"
-    sed -i "s/^kadcast_id =.*/kadcast_id = $kadcast_id/" "$RUSK_CONFIG_PATH"
-    sed -i "s/^bootstrapping_nodes =.*/bootstrapping_nodes = $bootstrapping_nodes/" "$RUSK_CONFIG_PATH"
-    sed -i "s/^genesis_timestamp =.*/genesis_timestamp = $genesis_timestamp/" "$RUSK_CONFIG_PATH"
     toml_set "$RUSK_CONFIG_PATH" kadcast.public_address "$PUBLIC_IP:9000"
     toml_set "$RUSK_CONFIG_PATH" kadcast.listen_address "$LISTEN_IP:9000"
     if toml-cli get "$RUSK_CONFIG_PATH" http &> /dev/null; then
@@ -80,22 +51,49 @@ EOF
     fi
 }
 
-download_rusk_config() {
+download_rusk_config_template() {
     echo "Downloading default template rusk config from the dusk node installer"
-    REMOTE_LOCATION=https://raw.githubusercontent.com/dusk-network/node-installer/9cdf0be1372ca6cb52cb279bd58781a3a27bf8ae/conf/rusk.toml
+    local remote_location
+    case "$SELECTED_NETWORK" in
+        mainnet)
+            remote_location="https://raw.githubusercontent.com/dusk-network/node-installer/ac1dd78eb31be4dba1c9c0986f6d6a06b5bd4fcc/conf/mainnet.toml"
+            ;;
+        testnet)
+            remote_location="https://raw.githubusercontent.com/dusk-network/node-installer/ac1dd78eb31be4dba1c9c0986f6d6a06b5bd4fcc/conf/testnet.toml"
+            ;;
+    esac
     mkdir -p "$RUSK_CONFIG_DIR"
-    curl -o "$RUSK_TEMPLATE_CONFIG_PATH" "$REMOTE_LOCATION"
+    curl -o "$RUSK_TEMPLATE_CONFIG_PATH" "$remote_location"
     if [ "$(cat $RUSK_TEMPLATE_CONFIG_PATH)" = "404: Not Found" ]; then
         echo "Couldn't find the default rusk template config file. This is a bug, please file an issue."
         exit 1
     fi
 }
 
-if [ ! -f "$RUSK_TEMPLATE_CONFIG_PATH" ]; then
-    download_rusk_config
-fi
+download_genesis_config() {
+    echo "Downloading the genesis config from the dusk node installer"
+    local remote_location
+    case "$SELECTED_NETWORK" in
+        mainnet)
+            remote_location="https://raw.githubusercontent.com/dusk-network/node-installer/ac1dd78eb31be4dba1c9c0986f6d6a06b5bd4fcc/conf/mainnet.genesis"
+            ;;
+        testnet)
+            remote_location="https://raw.githubusercontent.com/dusk-network/node-installer/ac1dd78eb31be4dba1c9c0986f6d6a06b5bd4fcc/conf/testnet.genesis"
+            ;;
+    esac
+    mkdir -p "$RUSK_CONFIG_DIR"
+    curl -o "$RUSK_RECOVERY_INPUT" "$remote_location"
+    if [ "$(cat $RUSK_RECOVERY_INPUT)" = "404: Not Found" ]; then
+        echo "Couldn't find the genesis config file. This is a bug, please file an issue."
+        exit 1
+    fi
+}
 
-configure_network "$NETWORK"
+if [ ! -f "$RUSK_TEMPLATE_CONFIG_PATH" ]; then
+    download_rusk_config_template
+fi
+download_genesis_config
+configure_network
 
 if [ -z "$DUSK_CONSENSUS_KEYS_PASS" ]; then
     echo "DUSK_CONSENSUS_KEYS_PASS is not set"
