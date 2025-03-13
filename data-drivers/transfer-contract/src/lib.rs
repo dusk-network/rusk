0e557635ea56c512757057aabc42aaf11f7e6d54
Herr Seppia <seppia@dusk.network>
2025-03-13 17:36:52 +0100

    transfer-contract-dd: implement ConvertibleContract

--- data-drivers/transfer-contract/src/lib.rs
+++ data-drivers/transfer-contract/src/lib.rs
@@ -20,48 +20,145 @@ extern crate alloc;
 mod bindgen;
 
 use alloc::format;
-use alloc::string::String;
+use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 
+use dusk_core::abi::ContractId;
+use dusk_core::signatures::bls::PublicKey as AccountPublicKey;
+use dusk_core::transfer::moonlight::AccountData;
 use dusk_core::transfer::{
-    ContractToAccountEvent, ContractToContractEvent, ConvertEvent,
-    DepositEvent, MoonlightTransactionEvent, PhoenixTransactionEvent,
-    WithdrawEvent, CONTRACT_TO_ACCOUNT_TOPIC, CONTRACT_TO_CONTRACT_TOPIC,
-    CONVERT_TOPIC, DEPOSIT_TOPIC, MINT_CONTRACT_TOPIC, MINT_TOPIC,
-    MOONLIGHT_TOPIC, PHOENIX_TOPIC, WITHDRAW_TOPIC,
+    withdraw::Withdraw, ContractToAccount, ContractToAccountEvent,
+    ContractToContract, ContractToContractEvent, ConvertEvent, DepositEvent,
+    MoonlightTransactionEvent, PhoenixTransactionEvent, WithdrawEvent,
+    CONTRACT_TO_ACCOUNT_TOPIC, CONTRACT_TO_CONTRACT_TOPIC, CONVERT_TOPIC,
+    DEPOSIT_TOPIC, MINT_CONTRACT_TOPIC, MINT_TOPIC, MOONLIGHT_TOPIC,
+    PHOENIX_TOPIC, WITHDRAW_TOPIC,
 };
-use dusk_data_driver::{rkyv_to_json, ConvertibleContract, Error, JsonValue};
+use dusk_core::BlsScalar;
+use dusk_data_driver::{
+    from_rkyv, json_to_rkyv, json_to_rkyv_pair_u64, json_to_rkyv_u64,
+    rkyv_to_json, rkyv_to_json_pair_u64, rkyv_to_json_u64, to_json,
+    ConvertibleContract, Error, JsonValue,
+};
 
 /// The contract driver for encoding and decoding transactions.
 #[derive(Default)]
 pub struct ContractDriver;
 
+#[allow(clippy::match_same_arms)]
 impl ConvertibleContract for ContractDriver {
-    #[allow(unused_variables)]
     fn encode_input_fn(
         &self,
         fn_name: &str,
         json: &str,
     ) -> Result<Vec<u8>, Error> {
-        todo!()
+        match fn_name {
+            // Transactions
+            "convert" => json_to_rkyv::<Withdraw>(json),
+            // Protocol calls
+            "deposit" => json_to_rkyv_u64(json),
+            "mint" | "withdraw" => json_to_rkyv::<Withdraw>(json),
+            "mint_to_contract" | "contract_to_contract" => {
+                json_to_rkyv::<ContractToContract>(json)
+            }
+            "contract_to_account" => json_to_rkyv::<ContractToAccount>(json),
+            // Queries
+            "root" | "num_notes" | "chain_id" => json_to_rkyv::<()>(json),
+            "account" => json_to_rkyv::<AccountPublicKey>(json),
+            "contract_balance" => json_to_rkyv::<ContractId>(json),
+            "opening" => json_to_rkyv_u64(json),
+            "existing_nullifiers" => json_to_rkyv::<Vec<BlsScalar>>(json),
+            // Feeder Queries
+            "leaves_from_height" | "leaves_from_pos" => json_to_rkyv_u64(json),
+            "sync"
+            | "sync_nullifiers"
+            | "sync_contract_balances"
+            | "sync_accounts" => json_to_rkyv_pair_u64(json),
+
+            // Unsupported
+            name => Err(Error::Unsupported(format!("fn_name {name}"))),
+        }
     }
 
-    #[allow(unused_variables)]
     fn decode_input_fn(
         &self,
         fn_name: &str,
         rkyv: &[u8],
     ) -> Result<JsonValue, Error> {
-        todo!()
+        match fn_name {
+            // Transactions and internal calls
+            "convert" => rkyv_to_json::<Withdraw>(rkyv),
+            // Protocol calls
+            "deposit" => rkyv_to_json_u64(rkyv),
+            "mint" | "withdraw" => rkyv_to_json::<Withdraw>(rkyv),
+            "mint_to_contract" | "contract_to_contract" => {
+                rkyv_to_json::<ContractToContract>(rkyv)
+            }
+            "contract_to_account" => rkyv_to_json::<ContractToAccount>(rkyv),
+            // Queries
+            "root" | "num_notes" | "chain_id" => rkyv_to_json::<()>(rkyv),
+            "account" => rkyv_to_json::<AccountPublicKey>(rkyv),
+            "contract_balance" => rkyv_to_json::<ContractId>(rkyv),
+            "opening" => rkyv_to_json_u64(rkyv),
+            "existing_nullifiers" => rkyv_to_json::<Vec<BlsScalar>>(rkyv),
+            // Feeder Queries
+            "leaves_from_height" | "leaves_from_pos" => rkyv_to_json_u64(rkyv),
+            "sync"
+            | "sync_nullifiers"
+            | "sync_contract_balances"
+            | "sync_accounts" => rkyv_to_json_pair_u64(rkyv),
+
+            // Unsupported
+            name => Err(Error::Unsupported(format!("fn_name {name}"))),
+        }
     }
 
-    #[allow(unused_variables)]
     fn decode_output_fn(
         &self,
         fn_name: &str,
         rkyv: &[u8],
     ) -> Result<JsonValue, Error> {
-        todo!()
+        match fn_name {
+            // Transactions and internal calls
+            "convert" => Ok(JsonValue::Null),
+            // Protocol calls
+            "deposit" => Ok(JsonValue::Null),
+            "mint" | "withdraw" => Ok(JsonValue::Null),
+            "mint_to_contract" | "contract_to_contract" => Ok(JsonValue::Null),
+            "contract_to_account" => Ok(JsonValue::Null),
+            // Queries
+            "root" => rkyv_to_json::<BlsScalar>(rkyv),
+            "num_notes" => rkyv_to_json_u64(rkyv),
+            "chain_id" => rkyv_to_json::<u8>(rkyv),
+            "account" => rkyv_to_json::<AccountData>(rkyv),
+            "contract_balance" => rkyv_to_json_u64(rkyv),
+            "opening" => {
+                Err(Error::Unsupported("opening not supported".into()))
+            }
+            "existing_nullifiers" => rkyv_to_json::<Vec<BlsScalar>>(rkyv),
+            // Feeder Queries
+            "sync_accounts" => from_rkyv::<(AccountData, [u8; 193])>(rkyv)
+                .and_then(|(data, key)| unsafe {
+                    to_json((
+                        data,
+                        AccountPublicKey::from_slice_unchecked(&key),
+                    ))
+                    .map_err(Error::from)
+                }),
+            "sync_nullifiers" => rkyv_to_json::<BlsScalar>(rkyv),
+            "sync_contract_balances" => from_rkyv::<(ContractId, u64)>(rkyv)
+                .and_then(|(contract, balance)| {
+                    to_json((contract, JsonValue::String(balance.to_string())))
+                        .map_err(Error::from)
+                }),
+
+            "leaves_from_height" | "leaves_from_pos" | "sync" => {
+                Err(Error::Unsupported("unsupported".into()))
+            }
+
+            // Unsupported
+            name => Err(Error::Unsupported(format!("fn_name {name}"))),
+        }
     }
 
     fn decode_event(
