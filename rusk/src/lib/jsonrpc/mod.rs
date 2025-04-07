273cc6c4a2469a2c56758c040058a6c9ae5cf03e
Arthur Welf <arthur.welf@gmail.com>
2025-04-07 04:44:48 +0200

    Update jsonrpc::mod.rs

--- rusk/src/lib/jsonrpc/mod.rs
+++ rusk/src/lib/jsonrpc/mod.rs
@@ -1,13 +1,19 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+//! Rusk JSON-RPC Server Module
+//!
+//! This module implements the JSON-RPC 2.0 server for Rusk, providing both
+//! HTTP and WebSocket interfaces for interacting with the node.
+
 pub mod config;
 pub mod error;
-pub mod handler;
 pub mod infrastructure;
-pub mod model;
+// pub mod model; // To be added later
 pub mod service;
-pub mod transport;
+// pub mod transport; // To be added later
