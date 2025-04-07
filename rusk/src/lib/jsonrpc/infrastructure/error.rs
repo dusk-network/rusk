b674ed7ccc6da14ff611205605657bf53aa207a2
Arthur Welf <arthur.welf@gmail.com>
2025-04-07 22:40:20 +0200

    Fix dusk-analyzer errors

--- rusk/src/lib/jsonrpc/infrastructure/error.rs
+++ rusk/src/lib/jsonrpc/infrastructure/error.rs
@@ -3,11 +3,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-//
-// Copyright (c) DUSK NETWORK. All rights reserved.
 
 //! Errors specific to the JSON-RPC infrastructure layer.
 //!
