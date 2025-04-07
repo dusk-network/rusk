4655c0794caea2a40b56d74e4310c01c22ce729f
Arthur Welf <arthur.welf@gmail.com>
2025-04-07 04:40:12 +0200

    Add service layer errors

--- rusk/src/lib/jsonrpc/service/mod.rs
+++ rusk/src/lib/jsonrpc/service/mod.rs
@@ -4,10 +4,16 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-pub mod block;
-pub mod contract;
+//! JSON-RPC Service Layer
+//!
+//! This module contains the business logic for handling JSON-RPC method calls,
+//! delegating tasks to the appropriate Rusk components or infrastructure
+//! services.
+
 pub mod error;
-pub mod network;
-pub mod prover;
-pub mod subscription;
-pub mod transaction;
+// pub mod block; // TODO: Uncomment when block service is created
+// pub mod contract; // TODO: Uncomment when contract service is created
+// pub mod network; // TODO: Uncomment when network service is created
+// pub mod prover; // TODO: Uncomment when prover service is created
+// pub mod subscription; // TODO: Uncomment when subscription service is created
+// pub mod transaction; // TODO: Uncomment when transaction service is created
