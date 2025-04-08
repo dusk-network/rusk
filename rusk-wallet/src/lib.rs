3abeac0e7e07245acb401237606a9b387d961803
moana <moana@dusk.network>
2025-04-08 13:18:39 +0200

    rusk-wallet: deny clippy::must_use_candidate

--- rusk-wallet/src/lib.rs
+++ rusk-wallet/src/lib.rs
@@ -15,6 +15,7 @@
 #![deny(missing_docs)]
 #![deny(clippy::pedantic)]
 #![allow(clippy::similar_names)]
+#![allow(clippy::module_name_repetitions)]
 #![allow(clippy::unused_async)]
 #![allow(clippy::return_self_not_must_use)]
 #![allow(clippy::cast_lossless)]
@@ -25,8 +26,6 @@
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::missing_panics_doc)]
 #![allow(clippy::doc_markdown)]
-#![allow(clippy::must_use_candidate)]
-#![allow(clippy::module_name_repetitions)]
 #![allow(clippy::needless_pass_by_value)]
 
 mod cache;
