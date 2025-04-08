80a6830e38ccf6ea5831af14e23e481f624d7b2c
moana <moana@dusk.network>
2025-04-08 11:44:56 +0200

    rusk-wallet: Add clippy pedantic and allow all errors

--- rusk-wallet/src/lib.rs
+++ rusk-wallet/src/lib.rs
@@ -13,6 +13,29 @@
 //! through the network of their choice, stake and withdraw rewards, etc.
 
 #![deny(missing_docs)]
+#![deny(clippy::pedantic)]
+#![allow(clippy::similar_names)]
+#![allow(clippy::unused_async)]
+#![allow(clippy::return_self_not_must_use)]
+#![allow(clippy::cast_lossless)]
+#![allow(clippy::cast_precision_loss)]
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::uninlined_format_args)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::missing_panics_doc)]
+#![allow(clippy::doc_markdown)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::wildcard_imports)]
+#![allow(clippy::enum_glob_use)]
+#![allow(clippy::match_same_arms)]
+#![allow(clippy::match_bool)]
+#![allow(clippy::map_unwrap_or)]
+#![allow(clippy::needless_pass_by_value)]
+#![allow(clippy::ignored_unit_patterns)]
+#![allow(clippy::explicit_iter_loop)]
+#![allow(clippy::unreadable_literal)]
 
 mod cache;
 mod clients;
