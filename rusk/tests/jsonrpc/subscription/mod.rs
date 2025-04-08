2d72652ee1d3e59b60e01cfc1470c8de6c585661
Arthur Welf <arthur.welf@gmail.com>
2025-04-08 19:06:47 +0200

    Split infrastructure::subscription tests into separate modules

---
+++ rusk/tests/jsonrpc/subscription/mod.rs
@@ -0,0 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+pub mod filter;
+pub mod manager;
+pub mod parameter_types;
+pub mod session_id;
+pub mod subscription_id;
+pub mod topic;
