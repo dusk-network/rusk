9e7382d8431edb51f068017f72614f1b94d6eaf0
moana <moana@dusk.network>
2025-04-08 13:33:30 +0200

    rusk-wallet: Fix SpentTx.gas_spent to be u64 instead of f64

--- rusk-wallet/src/lib.rs
+++ rusk-wallet/src/lib.rs
@@ -23,7 +23,6 @@
 #![allow(clippy::cast_lossless)]
 #![allow(clippy::cast_precision_loss)]
 #![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::cast_sign_loss)]
 
 mod cache;
 mod clients;
