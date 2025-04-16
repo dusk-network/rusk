ab21200ff8b0db81d47d3eb3ab7f2ea96c439f49
Arthur Welf <arthur.welf@gmail.com>
2025-04-16 10:11:28 +0200

    Add SubscriptionManager placeholder

--- rusk/src/lib/jsonrpc/infrastructure/subscription/manager.rs
+++ rusk/src/lib/jsonrpc/infrastructure/subscription/manager.rs
@@ -3,3 +3,16 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
+
+// --- Placeholder Types ---
+
+/// Placeholder struct for managing WebSocket subscriptions.
+///
+/// This struct will handle subscription requests, store active subscriptions,
+/// and manage broadcasting events to subscribers.
+#[derive(Debug, Default)]
+pub struct SubscriptionManager {
+    // TODO: Add fields for managing subscriptions later.
+}
+
+// --- End Placeholder Types ---
