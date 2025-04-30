4ada8a6521b3058121c5a5b690750eca7b5e4a1d
Demilade Sonuga <demilade@dusk.network>
2025-04-30 10:51:09 +0100

    rusk-wallet: withdraw all rewards by default in interactive mode

--- rusk-wallet/src/bin/interactive/command_menu.rs
+++ rusk-wallet/src/bin/interactive/command_menu.rs
@@ -267,11 +267,12 @@ pub(crate) async fn online(
 
             ProfileOp::Run(Box::new(Command::Withdraw {
                 address: Some(addr),
-                gas_limit: prompt::request_gas_limit(gas::DEFAULT_LIMIT_CALL)?,
-                reward: Some(prompt::request_token_amt(
+                reward: Some(prompt::request_token_amt_with_default(
                     "withdraw rewards",
                     max_withdraw,
+                    max_withdraw,
                 )?),
+                gas_limit: prompt::request_gas_limit(gas::DEFAULT_LIMIT_CALL)?,
                 gas_price: prompt::request_gas_price(
                     DEFAULT_PRICE,
                     mempool_gas_prices,
