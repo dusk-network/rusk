ffd20035207a04b62cbff81c8b9513f3c9d480fe
Arthur Welf <arthur.welf@gmail.com>
2025-05-08 23:52:51 +0200

    Add NodeInfo model

--- rusk/src/lib/jsonrpc/model/network.rs
+++ rusk/src/lib/jsonrpc/model/network.rs
@@ -16,6 +16,8 @@
 
 use serde::{Deserialize, Serialize};
 
+use crate::jsonrpc::model;
+
 /// Represents basic metrics related to the node's network peers.
 ///
 /// Provides a high-level overview of the node's connectivity.
@@ -83,4 +85,33 @@ pub struct NetworkStatus {
     /// used to distinguish between different blockchain networks or
     /// environments, such as mainnet, testnet, or custom deployments.
     pub network_id: u8,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+/// General information about the node, including version, configuration, and
+/// network details.
+pub struct NodeInfo {
+    /// Semantic version of the node
+    pub version: String,
+    /// Build information of the node
+    pub version_build: String,
+    /// The unique identifier of the blockchain network the node is connected
+    /// to.
+    pub chain_id: u8,
+    /// Public `SocketAddress` of the Peer. No domain name allowed
+    ///
+    /// This is the address where other peers can contact you.
+    /// This address MUST be accessible from any peer of the network
+    pub public_address: std::net::SocketAddr,
+    /// List of known bootstrapping kadcast nodes.
+    ///
+    /// It accepts the same representation of `public_address` but with domain
+    /// names allowed
+    pub bootstrap_nodes: Vec<String>,
+    /// Represents the relevant VM configuration settings exposed via JSON-RPC.
+    ///
+    /// This provides information about gas limits, deployment costs, and other
+    /// parameters influencing transaction execution and block generation
+    /// within the VM.
+    pub vm_config: model::vm::VmConfig,
 }
