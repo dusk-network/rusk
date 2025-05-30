229a44f8e06b2c3d570e69c87de567b80c17dcd4
Norton Andreev <norton@nortonandreev.com>
2025-05-30 12:55:13 +0300

    explorer: Use supply.dusk.network for circulating supply
    Resolves #3757

--- explorer/vite.config.js
+++ explorer/vite.config.js
@@ -43,6 +43,11 @@ export default defineConfig(({ mode }) => {
           rewrite: (path) => path.replace(/^\/rusk/, ""),
           target: "http://localhost:8080/",
         },
+        "/supply": {
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/supply/, ""),
+          target: "https://supply.dusk.network/",
+        },
       },
     },
     test: {
