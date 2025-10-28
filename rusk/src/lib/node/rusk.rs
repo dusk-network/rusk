8d6100a0b91a073d6b82a72642182dbab105044a
Milosz Muszynski <milosz.muszynski@gmail.com>
2025-10-28 16:22:39 +0100

    rusk: formatting

--- rusk/src/lib/node/rusk.rs
+++ rusk/src/lib/node/rusk.rs
@@ -513,10 +513,7 @@ impl Rusk {
 
     /// Opens a session for query, setting a block height of zero since this
     /// doesn't affect the result.
-    pub fn query_session(
-        &self,
-        commit: Option<[u8; 32]>,
-    ) -> Result<Session> {
+    pub fn query_session(&self, commit: Option<[u8; 32]>) -> Result<Session> {
         self._session(0, commit)
     }
 
