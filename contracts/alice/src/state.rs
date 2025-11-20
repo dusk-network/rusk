e33b98f69fe0d82ece24265eec1ba6e522c427c3
moana <moana@dusk.network>
2025-11-20 12:52:08 +0100

    alice: Add stake_activate test function

--- contracts/alice/src/state.rs
+++ contracts/alice/src/state.rs
@@ -4,11 +4,13 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use dusk_core::abi;
+use dusk_core::abi::{self, ContractId};
+use dusk_core::stake::Stake;
 use dusk_core::transfer::{
     withdraw::Withdraw, ContractToAccount, ContractToContract,
     TRANSFER_CONTRACT,
 };
+use rkyv;
 
 /// Alice contract.
 #[derive(Debug, Clone)]
@@ -38,5 +40,30 @@ impl Alice {
     pub fn contract_to_account(&mut self, transfer: ContractToAccount) {
         abi::call::<_, ()>(TRANSFER_CONTRACT, "contract_to_account", &transfer)
             .expect("Transferring to account should succeed");
+    }
+
+    pub fn stake_activate(&mut self, stake: Stake) {
+        const SCRATCH_BUF_BYTES: usize = 256;
+        const CHARLIE_ID: ContractId = ContractId::from_bytes([4; 32]);
+
+        let data = rkyv::to_bytes::<_, SCRATCH_BUF_BYTES>(&stake)
+            .expect("Stake should be rkyv serialized correctly")
+            .to_vec();
+
+        let transfer = ContractToContract {
+            contract: CHARLIE_ID,
+            value: stake.value(),
+            fn_name: "stake_from_contract".into(),
+            data,
+        };
+
+        abi::call::<_, ()>(
+            TRANSFER_CONTRACT,
+            "contract_to_contract",
+            &transfer,
+        )
+        .expect(
+            "Staking to the stake contract via the relayer contract should succeed",
+        );
     }
 }
