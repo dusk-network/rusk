75ed362f186915e1d4f302da8ea712e2ebaa7bc7
moana <moana@dusk.network>
2025-11-25 12:03:02 +0100

    alice: Add query to the reproduce the wasm trap

--- contracts/alice/src/state.rs
+++ contracts/alice/src/state.rs
@@ -5,7 +5,8 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use dusk_core::abi::{self, ContractId};
-use dusk_core::stake::Stake;
+use dusk_core::signatures::bls::PublicKey as BlsPublicKey;
+use dusk_core::stake::{Stake, StakeData, STAKE_CONTRACT};
 use dusk_core::transfer::{
     withdraw::Withdraw, ContractToAccount, ContractToContract,
     TRANSFER_CONTRACT,
@@ -46,6 +47,22 @@ impl Alice {
         const SCRATCH_BUF_BYTES: usize = 256;
         const CHARLIE_ID: ContractId = ContractId::from_bytes([4; 32]);
 
+        // adding a query to the transfer contract reproduces the wasm trap
+        abi::call::<_, u64>(TRANSFER_CONTRACT, "root", &())
+            .expect("quering the transfer contract should succeed");
+
+        // adding a query to the stake contract doesn't reproduce the wasm trap
+        // let provisioner = include_bytes!("../../../rusk/src/assets/dusk.cpk");
+        // use dusk_bytes::Serializable;
+        // let provisioner = BlsPublicKey::from_bytes(&provisioner)
+        //     .expect("The pk should be a valid point");
+        // abi::call::<_, Option<StakeData>>(
+        //     STAKE_CONTRACT,
+        //     "get_stake",
+        //     &provisioner,
+        // )
+        // .expect("calling get_stake should succeed");
+
         let data = rkyv::to_bytes::<_, SCRATCH_BUF_BYTES>(&stake)
             .expect("Stake should be rkyv serialized correctly")
             .to_vec();
@@ -67,3 +84,15 @@ impl Alice {
         );
     }
 }
+
+// fn ds_address(ds_str: &str) -> BlsPublicKey {
+//     // let ds_pk_bytes = bs58::decode(ds_str)
+//     //     .into_vec()
+//     //     .expect("address string should be bs58 encoded");
+//     let ds_pk_bytes = hex::decode(ds_str).expect("decoding hex should work");
+//     let ds_pk_bytes: [u8; 96] = ds_pk_bytes
+//         .try_into()
+//         .expect("the pk should be exactly 96 bytes");
+//     BlsPublicKey::from_bytes(&ds_pk_bytes)
+//         .expect("The pk should be a valid point")
+// }
