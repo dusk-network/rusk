9f3a1100f20feaff1ce078370539641cd756d7a0
Neotamandua <107320179+Neotamandua@users.noreply.github.com>
2026-02-03 11:29:03 +0100

    rusk: change test imports

--- rusk/tests/common/block.rs
+++ rusk/tests/common/block.rs
@@ -4,17 +4,19 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+use std::future::Future;
+
 use tokio::runtime::Handle;
 use tokio::task::block_in_place;
 
 pub(crate) trait Block {
-    fn wait(self) -> <Self as futures::Future>::Output
+    fn wait(self) -> <Self as Future>::Output
     where
         Self: Sized,
-        Self: futures::Future,
+        Self: Future,
     {
         block_in_place(move || Handle::current().block_on(self))
     }
 }
 
-impl<F, T> Block for F where F: futures::Future<Output = T> {}
+impl<F, T> Block for F where F: Future<Output = T> {}
