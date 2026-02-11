4834b234e5d70844c228a3e7f3a5da01b0c04dd8
Herr Seppia <seppia@dusk.network>
2026-02-11 12:06:42 +0100

    rusk: adapt `owner_calls` tests to new TestContext

--- rusk/tests/services/owner_calls.rs
+++ rusk/tests/services/owner_calls.rs
@@ -6,32 +6,23 @@
 
 use bytecheck::CheckBytes;
 use dusk_bytes::Serializable;
+use dusk_core::transfer::data::ContractCall;
+use dusk_rusk_test::TestContext;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 use rkyv::validation::validators::DefaultValidator;
 use rkyv::{Archive, Deserialize, Infallible, Serialize};
-use rusk::node::{DriverStore, RuskVmConfig};
-use std::collections::HashMap;
-use std::path::{Path, PathBuf};
-use std::sync::{Arc, RwLock};
+use rusk::node::RuskVmConfig;
 
 use dusk_core::abi::ContractId;
 use dusk_core::signatures::bls::{
     PublicKey as BlsPublicKey, SecretKey as BlsSecretKey,
-    Signature as BlsSignature,
 };
-use dusk_vm::{gen_contract_id, CallReceipt, ContractData, Session, VM};
-use rusk::{Error, Result, Rusk};
-use rusk_recovery_tools::state;
-use tempfile::tempdir;
-use tokio::sync::broadcast;
+use dusk_vm::{gen_contract_id, ContractData};
+use rusk::Result;
 use tracing::info;
 
 use crate::common::logger;
-use crate::common::state::DEFAULT_MIN_GAS_LIMIT;
-use crate::common::wallet::{
-    test_wallet as wallet, test_wallet::Wallet, TestStateClient, TestStore,
-};
 
 const POINT_LIMIT: u64 = 0x10000000;
 
@@ -41,160 +32,77 @@ const BOB_INIT_VALUE: u8 = 5;
 
 const GUARDED_METHOD: &str = "owner_reset";
 
-const CHAIN_ID: u8 = 0xFA;
-
-async fn initial_state<P: AsRef<Path>>(
-    dir: P,
-    owner: impl AsRef<[u8]>,
-) -> Result<Rusk> {
-    let dir = dir.as_ref();
-
-    let snapshot =
-        toml::from_str(include_str!("../config/contract_deployment.toml"))
-            .expect("Cannot deserialize config");
-
-    let dusk_key = *rusk::DUSK_CONSENSUS_KEY;
-    let deploy = state::deploy(dir, &snapshot, dusk_key, |session| {
-        let bob_bytecode = include_bytes!("../../../contracts/bin/bob.wasm");
-
-        session
-            .deploy(
-                bob_bytecode,
-                ContractData::builder()
-                    .owner(owner.as_ref())
-                    .init_arg(&BOB_INIT_VALUE)
-                    .contract_id(gen_contract_id(bob_bytecode, 0u64, owner)),
-                POINT_LIMIT,
-            )
-            .expect("Deploying the bob contract should succeed");
-    })
-    .expect("Deploying initial state should succeed");
-
-    let (_vm, _commit_id) = deploy;
-
-    let (sender, _) = broadcast::channel(10);
-
-    #[cfg(feature = "archive")]
-    let archive_dir =
-        tempdir().expect("Should be able to create temporary directory");
-    #[cfg(feature = "archive")]
-    let archive =
-        node::archive::Archive::create_or_open(archive_dir.path()).await;
-
-    let rusk = Rusk::new(
-        dir,
-        CHAIN_ID,
-        RuskVmConfig::new(),
-        DEFAULT_MIN_GAS_LIMIT,
-        u64::MAX,
-        sender,
-        #[cfg(feature = "archive")]
-        archive,
-        DriverStore::new(None::<PathBuf>),
-    )
-    .expect("Instantiating rusk should succeed");
-    Ok(rusk)
-}
-
 #[allow(dead_code)]
 struct Fixture {
-    pub rusk: Rusk,
-    pub wallet: Wallet<TestStore, TestStateClient>,
-    pub bob_bytecode: Vec<u8>,
+    pub tc: TestContext,
     pub contract_id: ContractId,
-    pub path: PathBuf,
-    pub session: Option<Session>,
 }
 
 impl Fixture {
     async fn build(owner: impl AsRef<[u8]>) -> Self {
-        let tmp =
-            tempdir().expect("Should be able to create temporary directory");
-        let rusk = initial_state(&tmp, owner.as_ref())
-            .await
-            .expect("Initializing should succeed");
+        let owner = owner.as_ref();
+        let state = include_str!("../config/contract_deployment.toml");
+        let bob_bytecode = include_bytes!("../../../contracts/bin/bob.wasm");
+        let contract_id = gen_contract_id(bob_bytecode, 0u64, owner);
+        let deploy_data = ContractData::builder()
+            .owner(owner)
+            .init_arg(&BOB_INIT_VALUE)
+            .contract_id(contract_id);
 
-        let cache = Arc::new(RwLock::new(HashMap::new()));
-
-        let wallet = wallet::Wallet::new(
-            TestStore,
-            TestStateClient {
-                rusk: rusk.clone(),
-                cache,
+        let tc = TestContext::instantiate_with(
+            state,
+            RuskVmConfig::new(),
+            |session| {
+                session
+                    .deploy(bob_bytecode, deploy_data, POINT_LIMIT)
+                    .expect("Deploying the bob contract should succeed");
             },
-        );
+        )
+        .await
+        .expect("Initializing should succeed");
 
-        let original_root = rusk.state_root();
+        let original_root = tc.state_root();
 
         info!("Original Root: {:?}", hex::encode(original_root));
 
-        let bob_bytecode =
-            include_bytes!("../../../contracts/bin/bob.wasm").to_vec();
-        let contract_id = gen_contract_id(&bob_bytecode, 0u64, owner.as_ref());
-
-        let path = tmp.into_path();
-
-        Self {
-            rusk,
-            wallet,
-            bob_bytecode,
-            contract_id,
-            path,
-            session: None,
-        }
+        Self { tc, contract_id }
     }
 
     pub fn assert_bob_contract_is_deployed(&self) {
         const BOB_ECHO_VALUE: u64 = 775;
-        let commit = self.rusk.state_root();
-        let vm =
-            VM::new(self.path.as_path()).expect("VM creation should succeed");
-        let mut session = vm
-            .session(commit, CHAIN_ID, 0)
-            .expect("Session creation should succeed");
-        let result = session.call::<_, u64>(
-            self.contract_id,
-            "echo",
-            &BOB_ECHO_VALUE,
-            u64::MAX,
-        );
-        assert_eq!(
-            result.expect("Echo call should succeed").data,
-            BOB_ECHO_VALUE
-        );
-        let result =
-            session.call::<_, u8>(self.contract_id, "value", &(), u64::MAX);
-        assert_eq!(
-            result.expect("Value call should succeed").data,
-            BOB_INIT_VALUE
-        );
-    }
 
-    pub fn set_session(&mut self) {
-        let commit = self.rusk.state_root();
-        let vm =
-            VM::new(self.path.as_path()).expect("VM creation should succeed");
-        self.session = Some(
-            vm.session(commit, CHAIN_ID, 0)
-                .expect("Session creation should succeed"),
-        );
+        let result: Result<u64, _> =
+            self.tc
+                .rusk()
+                .query(self.contract_id, "echo", &BOB_ECHO_VALUE);
+        assert_eq!(result.expect("Echo call should succeed"), BOB_ECHO_VALUE);
+
+        let result: u8 = self
+            .tc
+            .rusk()
+            .query(self.contract_id, "value", &())
+            .expect("Value call should succeed");
+        assert_eq!(result, BOB_INIT_VALUE);
     }
 
     pub fn query_contract<R>(
         &mut self,
         method: impl AsRef<str>,
-    ) -> Result<CallReceipt<R>, Error>
+    ) -> anyhow::Result<R>
     where
         R: Archive,
         R::Archived: Deserialize<R, Infallible>
             + for<'b> CheckBytes<DefaultValidator<'b>>,
     {
-        assert!(self.session.is_some());
-        self.session
-            .as_mut()
-            .unwrap()
-            .call::<_, R>(self.contract_id, method.as_ref(), &(), u64::MAX)
-            .map_err(Error::Vm)
+        self.tc
+            .rusk()
+            .query(self.contract_id, method.as_ref(), &())
+            .map_err(|e| {
+                anyhow::anyhow!(
+                    "Querying contract method {} failed: {e}",
+                    method.as_ref()
+                )
+            })
     }
 }
 
@@ -211,19 +119,17 @@ pub struct OwnerMessage {
 }
 
 // performs a call to a method which may verify that it is called by the owner
-fn make_owner_only_call<R>(
+fn make_owner_only_tx<'a, E: Into<Option<&'a str>>>(
     contract_id: ContractId,
     args: u8,
     fname: impl AsRef<str>,
     nonce: u64,
     owner_sk: &BlsSecretKey,
-    session: &mut Session,
-) -> Result<CallReceipt<R>, Error>
-where
-    R: Archive,
-    R::Archived:
-        Deserialize<R, Infallible> + for<'b> CheckBytes<DefaultValidator<'b>>,
-{
+    tc: &TestContext,
+    expected_error: E,
+) {
+    const GAS_LIMIT: u64 = 1_000_000_000;
+    const GAS_PRICE: u64 = 1;
     let msg = OwnerMessage {
         contract_id,
         args,
@@ -233,43 +139,43 @@ where
     let msg_bytes = rkyv::to_bytes::<_, 4096>(&msg)
         .expect("Message should serialize correctly")
         .to_vec();
-    let sig: BlsSignature = owner_sk.sign(&msg_bytes);
-    let args = (sig, msg.clone());
-    session
-        .call::<(BlsSignature, OwnerMessage), R>(
-            contract_id,
-            fname.as_ref(),
-            &args,
-            u64::MAX,
-        )
-        .map_err(Error::Vm)
+    let sig = owner_sk.sign(&msg_bytes);
+
+    let call = ContractCall::new(contract_id, fname.as_ref())
+        .with_args(&(sig, msg))
+        .expect("call to be created successfully");
+    let tx = tc
+        .wallet()
+        .moonlight_execute(0, 0, 0, GAS_LIMIT, GAS_PRICE, Some(call))
+        .expect("tx to be created successfully");
+    let _ = tc.execute_transaction(tx, 1, expected_error);
 }
 
 #[tokio::test(flavor = "multi_thread")]
-pub async fn non_bls_owner_guarded_call() -> Result<(), Error> {
+pub async fn bls_non_owner_guarded_call() -> anyhow::Result<()> {
     logger();
     const VALUE: u8 = 244;
     let rng = &mut StdRng::seed_from_u64(0xcafe);
     let sk = BlsSecretKey::random(rng);
-    let mut f = Fixture::build(NON_BLS_OWNER).await;
-    f.assert_bob_contract_is_deployed();
-    f.set_session();
 
-    let r = make_owner_only_call::<()>(
+    let f = Fixture::build(NON_BLS_OWNER).await;
+    f.assert_bob_contract_is_deployed();
+
+    make_owner_only_tx(
         f.contract_id,
         VALUE,
         GUARDED_METHOD,
         0,
         &sk,
-        f.session.as_mut().unwrap(),
+        &f.tc,
+        "Panic: method restricted only to the owner",
     );
-    assert!(r.is_err());
     Ok(())
 }
 
 /// owner is a BLS public key, method called is guarded
 #[tokio::test(flavor = "multi_thread")]
-pub async fn bls_owner_guarded_call() -> Result<(), Error> {
+pub async fn bls_owner_guarded_call() -> anyhow::Result<()> {
     logger();
     const VALUE1: u8 = 244;
     const VALUE2: u8 = 233;
@@ -280,68 +186,68 @@ pub async fn bls_owner_guarded_call() -> Result<(), Error> {
 
     let mut f = Fixture::build(&owner).await;
     f.assert_bob_contract_is_deployed();
-    f.set_session();
 
-    let nonce = f.query_contract::<u64>("nonce")?.data;
-
-    make_owner_only_call::<()>(
+    let nonce = f.query_contract::<u64>("nonce")?;
+    make_owner_only_tx(
         f.contract_id,
         VALUE1,
         GUARDED_METHOD,
         nonce,
         &sk,
-        f.session.as_mut().unwrap(),
-    )?;
+        &f.tc,
+        None,
+    );
     let value = f.query_contract::<u8>("value")?;
-    assert_eq!(VALUE1, value.data);
+    assert_eq!(VALUE1, value);
 
     // make sure the next call will fail if we do not increase the nonce
-    let r = make_owner_only_call::<()>(
+    make_owner_only_tx(
         f.contract_id,
         VALUE2,
         GUARDED_METHOD,
         nonce,
         &sk,
-        f.session.as_mut().unwrap(),
+        &f.tc,
+        "Panic: method restricted only to the owner",
     );
-    assert!(r.is_err());
 
     // next call should work if we increase the nonce
-    make_owner_only_call::<()>(
+    make_owner_only_tx(
         f.contract_id,
         VALUE2,
         GUARDED_METHOD,
         nonce + 1,
         &sk,
-        f.session.as_mut().unwrap(),
-    )?;
+        &f.tc,
+        None,
+    );
     let value = f.query_contract::<u8>("value")?;
-    assert_eq!(VALUE2, value.data);
+    assert_eq!(VALUE2, value);
 
     // call should fail if method name is incorrect
-    let r = make_owner_only_call::<()>(
+    make_owner_only_tx(
         f.contract_id,
         VALUE2,
         "incorrect",
         nonce + 2,
         &sk,
-        f.session.as_mut().unwrap(),
+        &f.tc,
+        "Unknown",
     );
-    assert!(r.is_err());
 
     // call should fail if contract id is incorrect
     let mut contract_id_bytes = f.contract_id.to_bytes();
     contract_id_bytes[0] ^= 0xff;
     let incorrect_contract_id = ContractId::from_bytes(contract_id_bytes);
-    let r = make_owner_only_call::<()>(
+    make_owner_only_tx(
         incorrect_contract_id,
         VALUE2,
         GUARDED_METHOD,
         nonce + 2,
         &sk,
-        f.session.as_mut().unwrap(),
+        &f.tc,
+        "Contract does not exist",
     );
-    assert!(r.is_err());
 
     Ok(())
 }
